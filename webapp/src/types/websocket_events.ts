c88711c2c43191eef8ea13bc943c966ef8819b30
Christopher Poile <cpoile@gmail.com>
2020-03-30 15:09:00 -0400

    PR comments from #16

--- webapp/src/types/websocket_events.ts
+++ webapp/src/types/websocket_events.ts
@@ -7,19 +7,4 @@ import {Incident} from './incident';
 
 export const WEBSOCKET_INCIDENT_UPDATE = `custom_${pluginId}_incident_update`;
 
-export interface WebsocketEvent {
-    event: string;
-    data: {
-        payload: string;
-    };
-    broadcast: {
-        omit_users: {
-            [userId: string]: boolean;
-        };
-        user_id: string;
-        channel_id: string;
-        team_id: string;
-    };
-    seq: number;
-}
-
+export {WebSocketMessage} from 'mattermost-redux/actions/websocket';
