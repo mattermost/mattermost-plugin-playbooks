dd0295c3fb4fdfe04c2435ba5b98d41a6a3dbd3f
Christopher Poile <cpoile@gmail.com>
2020-04-01 11:54:36 -0400

    move sorting into selectors; use reselect to memoize selectors

--- webapp/src/reducer.js
+++ webapp/src/reducer.js
@@ -33,21 +33,15 @@ function rhsState(state = RHSState.List, action) {
 function incidents(state = [], action) {
     switch (action.type) {
     case RECEIVED_INCIDENTS:
-        return sortedDescending(action.incidents) || [];
+        return action.incidents || [];
     case RECEIVED_INCIDENT_UPDATE: {
         const newState = state.filter((incident) => incident.id !== action.incident.id);
         newState.push(action.incident);
-        return sortedDescending(newState);
+        return newState;
     }
     default:
         return state;
     }
-}
-
-function sortedDescending(incidentList) {
-    return incidentList.sort((a, b) => {
-        return b.created_at - a.created_at;
-    });
 }
 
 function incidentDetails(state = {}, action) {
