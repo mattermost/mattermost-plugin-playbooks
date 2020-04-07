6553c95b0f0d5122bd9518515ab3ffa1b11f08c0
Christopher Poile <cpoile@gmail.com>
2020-04-07 10:42:07 -0400

    the linter was bullying me

--- server/incident/service.go
+++ server/incident/service.go
@@ -134,7 +134,7 @@ func (s *ServiceImpl) EndIncident(incidentID string, userID string) error {
 	// Close the incident
 	incdnt.IsActive = false
 
-	if err := s.store.UpdateIncident(incdnt); err != nil {
+	if err = s.store.UpdateIncident(incdnt); err != nil {
 		return fmt.Errorf("failed to end incident: %w", err)
 	}
 
