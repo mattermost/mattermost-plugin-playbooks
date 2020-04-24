488e2f628cbfbbb88b0f449a8706f552abd2484d
Christopher Poile <cpoile@gmail.com>
2020-04-24 15:44:29 -0400

    MM-24384 - Instruct non-members of the incident response channel how to join (#54)
    * add footer and link; style
    
    * better wording; slight refactoring of footer code

--- webapp/src/components/rhs/incident_details/incident_details.tsx
+++ webapp/src/components/rhs/incident_details/incident_details.tsx
@@ -14,15 +14,20 @@ import {Checklist, ChecklistItem} from 'src/types/playbook';
 import ProfileSelector from 'src/components/profile/profile_selector/profile_selector';
 import Link from 'src/components/rhs/link';
 
+// @ts-ignore
+const WebappUtils = window.WebappUtils;
+
 import './incident_details.scss';
+import {isMobile} from 'src/utils/utils';
 
 interface Props {
     incident: Incident;
     commander: UserProfile;
     profileUri: string;
     channelDetails: ChannelWithTeamData[];
-    involvedInIncident: boolean;
     viewingIncidentChannel: boolean;
+    involvedInIncident: boolean;
+    teamName: string;
     actions: {
         endIncident: () => void;
         modifyChecklistItemState: (incidentID: string, checklistNum: number, itemNum: number, checked: boolean) => void;
@@ -30,10 +35,18 @@ interface Props {
         removeChecklistItem: (incidentID: string, checklistNum: number, itemNum: number) => void;
         renameChecklistItem: (incidentID: string, checklistNum: number, itemNum: number, newtitle: string) => void;
         reorderChecklist: (incidentID: string, checklistNum: number, itemNum: number, newPosition: number) => void;
+        toggleRHS: () => void;
     };
 }
 
 export default class IncidentDetails extends React.PureComponent<Props> {
+    private moveToDM(userName: string) {
+        WebappUtils.browserHistory.push(`/${this.props.teamName}/messages/@${userName}`);
+        if (isMobile()) {
+            this.props.actions.toggleRHS();
+        }
+    }
+
     public render(): JSX.Element {
         const incidentChannel = this.props.channelDetails?.length > 0 ? this.props.channelDetails[0] : null;
 
@@ -88,29 +101,44 @@ export default class IncidentDetails extends React.PureComponent<Props> {
                     </div>
                 }
 
-                {
-                    this.props.involvedInIncident &&
-                    <div className='footer-div'>
-                        <button
-                            className='btn btn-primary'
-                            onClick={() => this.props.actions.endIncident()}
-                            disabled={!this.props.viewingIncidentChannel}
-                        >
-                            {'End Incident'}
-                        </button>
-                        {
-                            this.props.involvedInIncident && !this.props.viewingIncidentChannel &&
-                            <div className='help-text'>
-                                {'Go to '}
-                                <Link
-                                    to={`/${this.props.channelDetails[0].team_name}/channels/${this.props.channelDetails[0].name}`}
-                                    text={'the incident channel'}
-                                />
-                                {' to make changes.'}
-                            </div>
-                        }
-                    </div>
-                }
+                <div className='footer-div'>
+                    {
+                        this.props.involvedInIncident &&
+                        <>
+                            <button
+                                className='btn btn-primary'
+                                onClick={() => this.props.actions.endIncident()}
+                                disabled={!this.props.viewingIncidentChannel}
+                            >
+                                {'End Incident'}
+                            </button>
+                            {
+                                !this.props.viewingIncidentChannel &&
+                                <div className='help-text'>
+                                    {'Go to '}
+                                    <Link
+                                        to={`/${this.props.channelDetails[0].team_name}/channels/${this.props.channelDetails[0].name}`}
+                                        text={'the incident channel'}
+                                    />
+                                    {' to make changes.'}
+                                </div>
+                            }
+                        </>
+                    }
+
+                    {
+                        !this.props.involvedInIncident &&
+                        <div className='help-text'>
+                            {'You are not a participant in the incident. Contact '}
+                            <a
+                                onClick={() => this.moveToDM(this.props.commander.username)}
+                            >
+                                {'@' + this.props.commander.username}
+                            </a>
+                            {' to request access.'}
+                        </div>
+                    }
+                </div>
             </div>
         );
     }
