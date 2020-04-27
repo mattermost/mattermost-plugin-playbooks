c6962d25412247b7eb4c0adfdd2790df3df63205
Christopher Speller <crspeller@gmail.com>
2020-04-27 06:41:39 -0700

    MM-24210 Checked times beside checklist items. (#52)
    * Some small refactors.
    
    * Checked times beside checklist items.
    
    * Use moment.js so the dates are pretty.

--- webapp/src/types/playbook.ts
+++ webapp/src/types/playbook.ts
@@ -15,6 +15,7 @@ export interface Checklist {
 export interface ChecklistItem {
     title: string;
     checked: boolean;
+    checked_modified: string;
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
