df7296353401476bf4e767f67cc57e2059c069dc
Prapti <praptishrestha@gmail.com>
2020-05-02 10:23:34 -0700

    Removing tests-e2e/node_modules contents

--- tests-e2e/node_modules/@testing-library/cypress/add-commands.js
+++
@@ -1,4 +0,0 @@
-// this file is here so it's easy to register the commands
-// `import 'cypress-testing-library/add-commands'`
-// eslint-disable-next-line
-require('./dist/add-commands')
--- tests-e2e/node_modules/cypress-wait-until/release.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  plugins: [
-    "@semantic-release/release-notes-generator",
-    "@semantic-release/github",
-    "@semantic-release/npm"
-  ]
-};
