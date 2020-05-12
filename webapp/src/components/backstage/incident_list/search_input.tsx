8d78e6dccbac515cff5fded790224f2dc822a25d
Christopher Poile <cpoile@gmail.com>
2020-05-12 15:41:26 -0400

    simplify search_input

--- webapp/src/components/backstage/incident_list/search_input.tsx
+++ webapp/src/components/backstage/incident_list/search_input.tsx
@@ -1,7 +1,7 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import React, {useState} from 'react';
+import React from 'react';
 
 import './seach_input.scss';
 
@@ -10,10 +10,7 @@ interface Props {
 }
 
 export default function SearchInput(props: Props) {
-    const [term, setTerm] = useState('');
-
     const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setTerm(event.target.value);
         props.onSearch(event.target.value);
     };
 
@@ -22,7 +19,6 @@ export default function SearchInput(props: Props) {
             className='incident-list-search-input'
             type='text'
             placeholder='Search by Incident name'
-            value={term}
             onChange={onChange}
         />
     );
