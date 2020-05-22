5087681bd6051ba44fb6184efc895c2efdb0bddc
marianunez <maria.nunez@mattermost.com>
2020-05-21 21:52:36 -0400

    More chart work

--- webapp/src/components/backstage/incidents/incident_details/checklist_timeline.tsx
+++ webapp/src/components/backstage/incidents/incident_details/checklist_timeline.tsx
@@ -5,118 +5,45 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import moment from 'moment';
 
-import Chart, {ChartOptions, ChartTooltipItem} from 'chart.js';
+import Chart, {ChartOptions, ChartTooltipItem, ChartTooltipModel} from 'chart.js';
 
+import {changeOpacity} from 'mattermost-redux/utils/theme_utils';
+
+import {ChecklistItem} from 'src/types/playbook';
 import {Incident} from 'src/types/incident';
 
+import EmptyChecklitImage from 'src/components/assets/empty_checklist';
+
 import './incident_details.scss';
-import {ChecklistItem} from 'src/types/playbook';
 
 interface Props {
     theme: Record<string, string>;
     width: number;
     height: number;
-    data?: any;
     incident: Incident;
+}
+
+interface ChartData{
+    yLabels: string[];
+    checklistItems: ChecklistItem[];
+    datasets: any[];
 }
 
 export default class ChecklistTimeline extends React.PureComponent<Props> {
     public chart: Chart | null = null;
-    public chartOptions: ChartOptions = {
-        showLines: false,
-        legend: {
-            display: false,
-        },
-        scales: {
-            xAxes: [{
-                type: 'linear',
-                display: true,
-                scaleLabel: {
-                    display: true,
-                    labelString: 'Duration',
-                },
-                gridLines: {
-                    borderDash: [8, 4],
-                },
-                ticks: {
-                    callback: (value) => {
-                        const duration = moment.duration(value);
+    public chartOptions: ChartOptions;
+    public chartData: ChartData;
 
-                        if (duration.days()) {
-                            return `${duration.days()} days ${duration.hours()} h`;
-                        }
+    constructor(props: Props) {
+        super(props);
 
-                        if (duration.hours()) {
-                            return `${duration.hours()} h ${duration.minutes()} m`;
-                        }
-
-                        if (duration.minutes()) {
-                            return `${duration.minutes()} m`;
-                        }
-
-                        return `${duration.seconds()} s`;
-                    },
-                },
-            }],
-            yAxes: [{
-                type: 'category',
-                position: 'left',
-                display: true,
-                ticks: {
-                    reverse: true,
-                },
-                gridLines: {
-                    display: false,
-                },
-            }],
-        },
-        tooltips: {
-            custom: (tooltip) => {
-                if (!tooltip) {
-                    return;
-                }
-
-                // disable displaying the color box
-                tooltip.displayColors = false;
-            },
-            callbacks: {
-                title: () => '',
-                label: this.tooltipLabel,
-            },
-        },
-    };
-
-    public tooltipLabel(tooltipItem: ChartTooltipItem, data: any) {
-        const timeUpdated = moment(data.checklistItems[tooltipItem.index].checked_modified);
-        return timeUpdated.format('MMM DD LT');
+        this.chartOptions = this.initChartOptions();
+        this.chartData = this.initData();
     }
 
     public componentDidMount(): void {
         this.initChart();
     }
-
-    /*
-    public componentDidUpdate(prevProps: Props): void {
-        const currentData = this.props.data && this.props.data.labels.length > 0;
-
-        if (!currentData && this.chart) {
-            // Clean up the rendered chart before we render and destroy its context
-            this.chart.destroy();
-            this.chart = null;
-        }
-
-        if (Utils.areObjectsEqual(prevProps.data, this.props.data)) {
-            return;
-        }
-
-        const hasData = this.props.data && this.props.data.labels.length > 0;
-        const hasChart = Boolean(this.chart);
-
-        if (hasData) {
-            // Update the rendered chart or initialize it as necessary
-            this.initChart(hasChart);
-        }
-    }*/
 
     public componentWillUnmount(): void {
         if (this.chart) {
@@ -124,19 +51,108 @@ export default class ChecklistTimeline extends React.PureComponent<Props> {
         }
     }
 
+    public initChartOptions() {
+        const chartOptions: ChartOptions = {
+            showLines: false,
+            legend: {
+                display: false,
+            },
+            scales: {
+                xAxes: [{
+                    type: 'linear',
+                    display: true,
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Duration',
+                        fontColor: changeOpacity(this.props.theme.centerChannelColor, 0.56),
+                    },
+                    gridLines: {
+                        borderDash: [8, 4],
+                        color: changeOpacity(this.props.theme.centerChannelColor, 0.16),
+                        zeroLineColor: changeOpacity(this.props.theme.centerChannelColor, 0.16),
+                    },
+                    ticks: {
+                        fontColor: changeOpacity(this.props.theme.centerChannelColor, 0.72),
+                        callback: this.durationTickLabel,
+                    },
+                }],
+                yAxes: [{
+                    type: 'category',
+                    position: 'left',
+                    display: true,
+                    ticks: {
+                        reverse: true,
+                        fontColor: changeOpacity(this.props.theme.centerChannelColor, 0.72),
+                    },
+                    gridLines: {
+                        display: false,
+                    },
+                }],
+            },
+            tooltips: {
+                custom: this.tooltipColorBox,
+                bodyAlign: 'center',
+
+                // @ts-ignore
+                yAlign: 'bottom',
+                callbacks: {
+                    title: () => '', // Empty tooltip title
+                    label: this.tooltipLabel,
+                },
+            },
+        };
+
+        return chartOptions;
+    }
+
+    public tooltipLabel(tooltipItem: ChartTooltipItem, data: any) {
+        if (tooltipItem.index == null) {
+            return '';
+        }
+
+        const timeUpdated = moment(data.checklistItems[tooltipItem.index].checked_modified);
+        return timeUpdated.format('MMM DD LT');
+    }
+
+    public tooltipColorBox(tooltip: ChartTooltipModel) {
+        if (!tooltip) {
+            return;
+        }
+
+        // disable displaying the color box
+        tooltip.displayColors = false;
+    }
+
+    public durationTickLabel(xValue: number) {
+        const duration = moment.duration(xValue);
+
+        if (duration.days()) {
+            return `${duration.days()} days ${duration.hours()} h`;
+        }
+
+        if (duration.hours()) {
+            return `${duration.hours()} h ${duration.minutes()} m`;
+        }
+
+        if (duration.minutes()) {
+            return `${duration.minutes()} m`;
+        }
+
+        return `${duration.seconds()} s`;
+    }
+
     public initData() {
         const chartData = {
-            yLabels: [] as any,
+            yLabels: [] as string[],
             checklistItems: [] as ChecklistItem[],
 
             datasets: [{
-                borderColor: 'rgba(151,187,205,1)',
                 pointBackgroundColor: this.props.theme.buttonBg,
-                pointBorderColor: '#fff',
-                pointHoverBackgroundColor: 'var(--sidebar-bg-color-16)',
-                pointHoverBorderColor: 'rgba(151,187,205,1)',
-                pointRadius: 5,
-                pointHoverRadius: 15,
+                pointBorderColor: this.props.theme.buttonBg,
+                pointHoverBackgroundColor: this.props.theme.buttonBg,
+                pointHoverBorderColor: this.props.theme.buttonBg,
+                pointRadius: 3,
+                pointHoverRadius: 10,
                 data: [] as any,
             }],
         };
@@ -164,14 +180,14 @@ export default class ChecklistTimeline extends React.PureComponent<Props> {
 
         chartData.yLabels = chartData.yLabels.reverse();
 
-        // Add an initial/last tick to scales
+        // Add an initial/last tick to scale
         chartData.yLabels.unshift('');
         chartData.yLabels.push('');
 
         return chartData;
     }
 
-    public initChart = (update?: boolean): void => {
+    public initChart = (): void => {
         if (!this.refs.canvas) {
             return;
         }
@@ -179,49 +195,69 @@ export default class ChecklistTimeline extends React.PureComponent<Props> {
         const el = ReactDOM.findDOMNode(this.refs.canvas) as HTMLCanvasElement;
         const ctx = el.getContext('2d') as CanvasRenderingContext2D;
 
-        const chartData = this.initData();
+        Chart.defaults.customLine = Chart.defaults.line;
 
-        const dataCopy = JSON.parse(JSON.stringify(chartData));
+        // Custom draw grid line on hover
+        Chart.controllers.customLine = Chart.controllers.line.extend({
+            draw(ease: any) {
+                Chart.controllers.line.prototype.draw.call(this, ease);
 
-        this.chart = new Chart(ctx, {type: 'line', data: dataCopy, options: this.chartOptions || {}});
+                // Only draw the custom line when showing the tooltip
+                if (this.chart.tooltip._active && this.chart.tooltip._active.length) {
+                    const activePoint = this.chart.tooltip._active[0];
 
-        if (update) {
-            this.chart.update();
-        }
-    }
+                    const y = activePoint.tooltipPosition().y;
+                    const x = activePoint.tooltipPosition().x;
+
+                    // draw line
+                    ctx.save();
+                    ctx.beginPath();
+                    ctx.moveTo(this.chart.chartArea.left, y);
+                    ctx.lineWidth = 0.5;
+
+                    ctx.lineTo(x, y);
+                    ctx.lineTo(x, this.chart.chartArea.bottom);
+
+                    ctx.stroke();
+                    ctx.restore();
+                }
+            },
+        });
+
+        const dataCopy = JSON.parse(JSON.stringify(this.chartData));
+        this.chart = new Chart(ctx, {type: 'customLine', data: dataCopy, options: this.chartOptions || {}});
+    };
 
     public render(): JSX.Element {
-        const chartData = this.initData();
-
         let content;
-        if (chartData == null) {
-            content = 'Loading...';
-        } else if (chartData.yLabels.length === 2) {
-            // If it only has the trailing empty labels
-            content = (
-                'Not enough data for a meaningful representation.'
+        if (this.chartData.yLabels.length === 2) {
+            // No data if it only has the two trailing empty labels
+            content = (<div className='content'>
+                <div>
+                    <EmptyChecklitImage theme={this.props.theme}/>
+                </div>
+                <div className='chart-label'>
+                    {'The incident has no checklist items yet'}
+                </div>
+            </div>
             );
         } else {
-            content = (
+            content = (<>
+                <div className='chart-title'>
+                    {'Time occurance of each Checklist item'}
+                </div>
                 <canvas
                     ref='canvas'
-
                     width={this.props.width}
                     height={this.props.height}
                 />
+            </>
             );
         }
 
         return (
             <div className='col-sm-12'>
-                <div className='total-count by-day'>
-                    <div className='title'>
-                        {'Time occurance of each Checklist item'}
-                    </div>
-                    <div className='content'>
-                        {content}
-                    </div>
-                </div>
+                {content}
             </div>
         );
     }
