0f10b5be820e662f6fa22e0e03baf695bfa85710
Christopher Speller <crspeller@gmail.com>
2020-06-25 06:59:28 -0700

    Refactoring to use hooks.

---
+++ webapp/src/components/assets/icons/incident_icon.tsx
@@ -0,0 +1,32 @@
+// Copyright (c) 2017-present Mattermost, Inc. All Rights Reserved.
+// See License for license information.
+
+import React, {useRef, FC} from 'react';
+import {useSelector} from 'react-redux';
+
+import {rhsOpen} from 'src/selectors';
+
+const IncidentIcon: FC = () => {
+    const myRef = useRef(null);
+    const isRHSOpen = useSelector(rhsOpen);
+
+    // If it has been mounted, we know our parent is always a button.
+    const parent = myRef?.current ? myRef?.current?.parentNode as HTMLButtonElement : null;
+    if (parent) {
+        if (isRHSOpen) {
+            parent.classList.add('channel-header__icon--active');
+        } else {
+            parent.classList.remove('channel-header__icon--active');
+        }
+    }
+
+    return (
+        <i
+            id='incidentIcon'
+            ref={myRef}
+            className={'icon fa fa-exclamation'}
+        />
+    );
+};
+
+export default IncidentIcon;
