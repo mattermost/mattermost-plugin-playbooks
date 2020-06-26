2a14135b18d967ce83fb6ab81f243163a005dd07
Alejandro García Montoro <alejandro.garciamontoro@gmail.com>
2020-06-26 18:10:16 +0200

    Add E2E test for URL redirection to /error

--- tests-e2e/cypress/integration/playbook_backstage/playbook_backstage_spec.js
+++ tests-e2e/cypress/integration/playbook_backstage/playbook_backstage_spec.js
@@ -13,23 +13,31 @@
 import users from '../../fixtures/users.json';
 
 describe('Playbook List View in Backstage', () => {
-	before(() => {
+	beforeEach(() => {
 		// # Login as non-admin user
 		cy.apiLogin('user-1');
 
 		// # Go to the team's town-square channel
 		cy.visit('/ad-1/channels/town-square');
+	});
 
+	it('Has "Playbooks" and team name in heading', () => {
 		// # Launch incident backstage
 		cy.openIncidentBackstage();
 
 		// # Switch to Playbooks backstage
 		cy.findByTestId('playbooksLHSButton').click();
-	});
 
-	it('Has "Playbooks" and team name in heading', () => {
 		// * Verify that the heading has "Playbooks" and the team's name -- eligendi
 		cy.findByTestId('titlePlaybook').should('be.visible').contains('Playbooks');
 		cy.findByTestId('titlePlaybook').contains('eligendi');
+	});
+
+	it('Redirects to /error if the playbook is unknown', () => {
+		// # Visit the URL of a non-existing playbook
+		cy.visit('/ad-1/com.mattermost.plugin-incident-response/playbooks/an_unknown_id');
+
+		// * Verify that the user has been redirect to /error with type=playbooks
+		cy.url().should('include', '/ad-1/com.mattermost.plugin-incident-response/error?type=playbooks');
 	});
 });
