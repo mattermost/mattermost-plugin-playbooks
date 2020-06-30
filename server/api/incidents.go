7b6659a06f48f7dd717a8b6307d2e4f32222ec18
Christopher Speller <crspeller@gmail.com>
2020-06-30 07:28:46 -0700

    Remove endpoint for get incidents by channel in favour of #171

--- server/api/incidents.go
+++ server/api/incidents.go
@@ -46,7 +46,6 @@ func NewIncidentHandler(router *mux.Router, incidentService incident.Service, pl
 	incidentsRouter.HandleFunc("/end-dialog", handler.endIncidentFromDialog).Methods(http.MethodPost)
 	incidentsRouter.HandleFunc("/commanders", handler.getCommanders).Methods(http.MethodGet)
 	incidentsRouter.HandleFunc("/channels", handler.getChannels).Methods(http.MethodGet)
-	incidentsRouter.HandleFunc("/channel/{channel_id:[A-Za-z0-9]+}", handler.getIncidentWithDetailsByChannel).Methods(http.MethodGet)
 
 	incidentRouter := incidentsRouter.PathPrefix("/{id:[A-Za-z0-9]+}").Subrouter()
 	incidentRouter.HandleFunc("", handler.getIncident).Methods(http.MethodGet)
@@ -273,50 +272,6 @@ func (h *IncidentHandler) getIncidentWithDetails(w http.ResponseWriter, r *http.
 	userID := r.Header.Get("Mattermost-User-ID")
 
 	if err := permissions.CheckHasPermissionsToIncidentChannel(userID, incidentID, h.pluginAPI, h.incidentService); err != nil {
-		if errors.Is(err, permissions.ErrNoPermissions) {
-			HandleErrorWithCode(w, http.StatusForbidden, "Not authorized",
-				errors.Errorf("userid: %s does not have permissions to view the incident details", userID))
-			return
-		}
-		HandleError(w, err)
-		return
-	}
-
-	incidentToGet, err := h.incidentService.GetIncidentWithDetails(incidentID)
-	if err != nil {
-		HandleError(w, err)
-		return
-	}
-
-	jsonBytes, err := json.Marshal(incidentToGet)
-	if err != nil {
-		HandleError(w, err)
-		return
-	}
-
-	w.WriteHeader(http.StatusOK)
-	if _, err = w.Write(jsonBytes); err != nil {
-		HandleError(w, err)
-		return
-	}
-}
-
-func (h *IncidentHandler) getIncidentWithDetailsByChannel(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	channelID := vars["channel_id"]
-	userID := r.Header.Get("Mattermost-User-ID")
-
-	incidentID, err := h.incidentService.GetIncidentIDForChannel(channelID)
-	if err != nil {
-		if errors.Is(err, incident.ErrNotFound) {
-			HandleErrorWithCode(w, http.StatusNotFound, "Not Found", err)
-		} else {
-			HandleError(w, err)
-		}
-		return
-	}
-
-	if err = permissions.CheckHasPermissionsToIncidentChannel(userID, incidentID, h.pluginAPI, h.incidentService); err != nil {
 		if errors.Is(err, permissions.ErrNoPermissions) {
 			HandleErrorWithCode(w, http.StatusForbidden, "Not authorized",
 				errors.Errorf("userid: %s does not have permissions to view the incident details", userID))
