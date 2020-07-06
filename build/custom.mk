0c3795594f94f92c89eef93042da71f2cfc4714e
Alejandro García Montoro <alejandro.garciamontoro@gmail.com>
2020-07-06 16:55:53 +0200

    MM-26687 Fix incident creation in empty databases (#179)
    * Check for nil oldValue in the SetAtomic updater
    
    * Test first incident's updateHeaders call
    
    * Check for nil oldValue in playbook index managers
    
    * Test first playbook's {addTo,removeFrom}Index call

--- build/custom.mk
+++ build/custom.mk
@@ -12,4 +12,5 @@ ifneq ($(HAS_SERVER),)
 	mockgen -destination server/incident/mocks/mock_store.go github.com/mattermost/mattermost-plugin-incident-response/server/incident Store
 	mockgen -destination server/pluginkvstore/mocks/mock_kvapi.go github.com/mattermost/mattermost-plugin-incident-response/server/pluginkvstore KVAPI
 	mockgen -destination server/pluginkvstore/mocks/mock_storeapi.go github.com/mattermost/mattermost-plugin-incident-response/server/pluginkvstore StoreAPI
+	mockgen -destination server/pluginkvstore/mocks/serverpluginapi/mock_plugin.go github.com/mattermost/mattermost-server/v5/plugin API
 endif
