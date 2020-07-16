c3bb97f1cc0b907872c02cb95af42ad663e5ff85
Maria A Nunez <maria.nunez@mattermost.com>
2020-07-16 08:35:51 -0400

    MM-26675 - Restored websocket handler for incident created (#208)
    * Restored websocket handler for create incident
    
    * Moved out of incident update hook
    
    * Linting
    
    * More linting
    
    * Sorry for the noise

--- webapp/src/websocket_events.ts
+++ webapp/src/websocket_events.ts
@@ -1,8 +1,13 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
+import {GetStateFunc} from 'mattermost-redux/types/actions';
 import {WebSocketMessage} from 'mattermost-redux/actions/websocket';
+import {getCurrentTeam} from 'mattermost-redux/selectors/entities/teams';
 
+import {navigateToUrl} from 'src/browser_routing';
+
+import {clientId} from './selectors';
 import {isIncident, Incident} from './types/incident';
 
 export const websocketSubscribers = new Set<(incident: Incident) => void>();
@@ -18,5 +23,28 @@ export function handleWebsocketIncidentUpdate() {
         }
 
         websocketSubscribers.forEach((fn) => fn(incident));
+    };
+}
+
+export function handleWebsocketIncidentCreate(getState: GetStateFunc) {
+    return (msg: WebSocketMessage): void => {
+        if (!msg.data.payload) {
+            return;
+        }
+        const payload = JSON.parse(msg.data.payload);
+        const incident = payload.incident;
+        if (!isIncident(incident)) {
+            return;
+        }
+
+        if (payload.client_id !== clientId(getState())) {
+            return;
+        }
+
+        const currentTeam = getCurrentTeam(getState());
+
+        // Navigate to the newly created channel
+        const url = `/${currentTeam.name}/channels/${incident.primary_channel_id}`;
+        navigateToUrl(url);
     };
 }
