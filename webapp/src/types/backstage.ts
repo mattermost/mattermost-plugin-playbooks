d1ffd7d00dc8d769926db52ac183cf3fc6c75b14
Christopher Poile <cpoile@gmail.com>
2020-07-28 11:00:48 -0400

    remove incorrect comment

--- webapp/src/types/backstage.ts
+++ webapp/src/types/backstage.ts
@@ -8,9 +8,6 @@ export interface CommanderInfo {
     username: string;
 }
 
-// Performs formatting of user posts including highlighting mentions and search terms and converting urls, hashtags,
-// @mentions and ~channels to links by taking a user's message and returning a string of formatted html. Also takes
-// a number of options as part of the second parameter:
 export type ChannelNamesMap = {
     [name: string]: {
         display_name: string;
