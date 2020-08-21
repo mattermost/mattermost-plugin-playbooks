722ce489121b4b15b172e5f9c57f20123ca66dce
Christopher Poile <cpoile@gmail.com>
2020-08-21 19:15:29 -0400

    MM-27999 Fix commander filter; fix autocomplete (#259)
    * fix for getting commander UserProfiles
    
    * MM-28010 - remove advance option in autocomplete text
    
    * simplify--no need for getMissingProfiles
    
    * simplify a lot.

--- webapp/src/components/profile/profile_selector.tsx
+++ webapp/src/components/profile/profile_selector.tsx
@@ -96,7 +96,7 @@ export default function ProfileSelector(props: Props) {
                     />
                 ),
                 userId: user.id,
-            });
+            } as Option);
         });
 
         if (props.selfIsFirstOption) {
