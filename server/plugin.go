c868c7c2d3a14c51cbe7e98ad7e8a1810cb0368d
Jesse Hallam <jesse.hallam@gmail.com>
2020-08-21 12:58:07 -0300

    require Mattermost E20 Enterprise License on start

--- server/plugin.go
+++ server/plugin.go
@@ -52,6 +52,10 @@ func (p *Plugin) OnActivate() error {
 	p.config = config.NewConfigService(pluginAPIClient, manifest)
 	pluginapi.ConfigureLogrus(logrus.New(), pluginAPIClient)
 
+	if !isLicensed(pluginAPIClient.Configuration.GetConfig(), pluginAPIClient.System.GetLicense()) {
+		return errors.New("A valid Mattermost Enterprise E20 license is required to use this plugin.")
+	}
+
 	botID, err := pluginAPIClient.Bot.EnsureBot(&model.Bot{
 		Username:    "incident",
 		DisplayName: "Incident Bot",
