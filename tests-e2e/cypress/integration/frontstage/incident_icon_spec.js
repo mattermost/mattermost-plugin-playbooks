d0b222f9afef3b59b52140701f1ad5ccf4d52669
Jesse Hallam <jesse.hallam@gmail.com>
2020-09-01 12:45:38 -0300

    rework e2e test organization (#263)
    * rework e2e test organization
    
    * Update tests-e2e/cypress/integration/frontstage/incident_dialog_spec.js
    
    Co-authored-by: Alejandro García Montoro <alejandro.garciamontoro@gmail.com>
    
    * simplify naming
    
    * Update tests-e2e/cypress/integration/frontstage/incident_creation_spec.js
    
    Co-authored-by: Prapti <praptishrestha@gmail.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Prapti <praptishrestha@gmail.com>
    
    Co-authored-by: Alejandro García Montoro <alejandro.garciamontoro@gmail.com>
    Co-authored-by: Prapti <praptishrestha@gmail.com>

---
+++ tests-e2e/cypress/integration/frontstage/incident_icon_spec.js
@@ -0,0 +1,40 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+// ***************************************************************
+// - [#] indicates a test step (e.g. # Go to a page)
+// - [*] indicates an assertion (e.g. * Check the title)
+// ***************************************************************
+
+describe('incident icon in channel header', () => {
+    beforeEach(() => {
+        // # Login as user-1
+        cy.apiLogin('user-1');
+    });
+
+    it('toggles the incident RHS', () => {
+        // # Size the viewport to show plugin icons even when RHS is open
+        cy.viewport('macbook-13');
+
+        // # Navigate to the application and a channel without an incident
+        cy.visit('/ad-1/channels/off-topic');
+
+        // # Click the incident icon
+        cy.get('#channel-header').within(() => {
+            cy.get('#incidentIcon').should('be.visible').click();
+        });
+
+        // * Verify the incident RHS is open.
+        cy.get('#rhsContainer').should('be.visible').within(() => {
+            cy.findByText('Incidents').should('be.visible');
+        });
+
+        // # Click the incident icon again
+        cy.get('#channel-header').within(() => {
+            cy.get('#incidentIcon').should('be.visible').click();
+        });
+
+        // * Verify the incident RHS is no longer open.
+        cy.get('#rhsContainer').should('not.be.visible');
+    });
+});
