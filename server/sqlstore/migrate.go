9724bd4d6ca8c1e2e1fc359d9a49249a7f88097f
Christopher Poile <cpoile@gmail.com>
2020-09-05 11:10:55 -0400

    don't use defer inside a loop

--- server/sqlstore/migrate.go
+++ server/sqlstore/migrate.go
@@ -1,8 +1,6 @@
 package sqlstore
 
 import (
-	"database/sql"
-
 	"github.com/blang/semver"
 	"github.com/pkg/errors"
 )
@@ -14,38 +12,48 @@ func (sqlStore *SQLStore) Migrate(pluginAPI PluginAPIClient, originalSchemaVersi
 			continue
 		}
 
-		tx, err := sqlStore.db.Beginx()
-		if err != nil {
-			return errors.Wrap(err, "could not begin transaction")
-		}
-		defer func() {
-			cerr := tx.Rollback()
-			if err == nil && cerr != sql.ErrTxDone {
-				err = cerr
-			}
-		}()
-
-		if err := migration.migrationFunc(tx); err != nil {
-			return errors.Wrapf(err, "error executing migration from version %s to version %s", migration.fromVersion.String(), migration.toVersion.String())
-		}
-
-		if err := sqlStore.SetCurrentVersion(tx, migration.toVersion); err != nil {
-			return errors.Wrapf(err, "migration succeeded, but failed to set the current version to %s. Database is now in an inconsistent state", migration.toVersion.String())
-		}
-
-		// TODO: Remove when all customers are in 0.1.0
-		if migration.toVersion.EQ(semver.MustParse("0.1.0")) {
-			if err := DataMigration(sqlStore, tx, pluginAPI.KV); err != nil {
-				return errors.Wrap(err, "failed to migrate the data from the KV store to the SQL database")
-			}
-		}
-
-		if err := tx.Commit(); err != nil {
-			return errors.Wrap(err, "could not commit transaction")
+		if err := sqlStore.migrate(pluginAPI, migration); err != nil {
+			return err
 		}
 
 		currentSchemaVersion = migration.toVersion
 	}
 
+	return nil
+}
+
+func (sqlStore *SQLStore) migrate(pluginAPI PluginAPIClient, migration Migration) (err error) {
+	tx, err := sqlStore.db.Beginx()
+	if err != nil {
+		return errors.Wrap(err, "could not begin transaction")
+	}
+	defer func() {
+		if err != nil {
+			cerr := tx.Rollback()
+			if cerr != nil {
+				err = errors.Wrap(err, "because of error, tried to rollback. Rollback returned: "+cerr.Error())
+			}
+		}
+	}()
+
+	if err := migration.migrationFunc(tx); err != nil {
+		return errors.Wrapf(err, "error executing migration from version %s to version %s", migration.fromVersion.String(), migration.toVersion.String())
+	}
+
+	if err := sqlStore.SetCurrentVersion(tx, migration.toVersion); err != nil {
+		return errors.Wrapf(err, "migration succeeded, but failed to set the current version to %s. Database is now in an inconsistent state", migration.toVersion.String())
+	}
+
+	// TODO: Remove when all customers are in 0.1.0
+	// https://mattermost.atlassian.net/browse/MM-28373
+	if migration.toVersion.EQ(semver.MustParse("0.1.0")) {
+		if err := DataMigration(sqlStore, tx, pluginAPI.KV); err != nil {
+			return errors.Wrap(err, "failed to migrate the data from the KV store to the SQL database")
+		}
+	}
+
+	if err := tx.Commit(); err != nil {
+		return errors.Wrap(err, "could not commit transaction")
+	}
 	return nil
 }
