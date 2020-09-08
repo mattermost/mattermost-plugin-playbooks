dc76a33f6f53694981d2aaf2de221762c9d9a0a8
Christopher Poile <cpoile@gmail.com>
2020-09-08 10:34:20 -0400

    add getTestElement; we can use 'data-testid' instead of class names

--- tests-e2e/cypress/integration/backstage/incident_list_spec.js
+++ tests-e2e/cypress/integration/backstage/incident_list_spec.js
@@ -6,6 +6,8 @@
 // - [*] indicates an assertion (e.g. * Check the title)
 // ***************************************************************
 
+import {getTestElement} from '../../support/ui_commands';
+
 describe('backstage incident list', () => {
     const playbookName = 'Playbook (' + Date.now() + ')';
     let teamId;
@@ -76,6 +78,6 @@ describe('backstage incident list', () => {
         });
 
         // * Verify that the header contains the incident name
-        cy.get('.details-header').contains(incidentName);
+        getTestElement('incident-title').contains(incidentName);
     });
 });
