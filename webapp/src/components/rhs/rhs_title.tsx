a7d8dd22a69140b10e048e495eae3b93ee42c69d
Jesse Hallam <jesse.hallam@gmail.com>
2020-09-29 17:07:29 -0300

    MM-28931: show incident status in RHS

--- webapp/src/components/rhs/rhs_title.tsx
+++ webapp/src/components/rhs/rhs_title.tsx
@@ -2,18 +2,39 @@
 // See LICENSE.txt for license information.
 
 import React, {FC} from 'react';
+import styled from 'styled-components';
 
 import {useCurrentIncident} from 'src/hooks';
 
-import './rhs_title.scss';
+import StatusBadge from '../backstage/incidents/status_badge';
+
+const RHSTitleContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: flex-end;
+    justify-content: space-between;
+    overflow: hidden;
+`;
+
+const RHSTitleText = styled.div`
+    overflow: hidden;
+    text-overflow: ellipsis;
+    margin-right: 8px;
+`;
 
 const RHSTitle: FC = () => {
     const [incident] = useCurrentIncident();
 
     return (
-        <div className='rhs-incident-title'>
-            {(incident && incident.name) || 'Incidents'}
-        </div>
+        <RHSTitleContainer>
+            <RHSTitleText>{incident?.name || 'Incidents'}</RHSTitleText>
+            {incident && (
+                <StatusBadge
+                    isActive={incident?.is_active}
+                    compact={true}
+                />
+            )}
+        </RHSTitleContainer>
     );
 };
 
