b108087a9210b08aa2b7112b06cf886cec352285
Alejandro García Montoro <alejandro.garciamontoro@gmail.com>
2020-10-01 16:29:12 +0200

    Add /incident info (#306)
    * Add /incident info
    
    Prefer attachments over raw message
    
    * Fix indentation
    
    * Send nothing to the server when RHS is opened
    
    * Use final copy text
    
    * Make the linters happy

--- webapp/src/slash_command.ts
+++ webapp/src/slash_command.ts
@@ -1,8 +1,10 @@
 import {Store} from 'redux';
+import {getCurrentChannel} from 'mattermost-redux/selectors/entities/channels';
 import {GlobalState} from 'mattermost-redux/types/store';
 import {generateId} from 'mattermost-redux/utils/helpers';
 
-import {setClientId} from 'src/actions';
+import {toggleRHS, setClientId} from 'src/actions';
+import {isIncidentChannel, isIncidentRHSOpen} from 'src/selectors';
 
 export function makeSlashCommandHook(store: Store<GlobalState>) {
     return (message: string, args = {}) => {
@@ -16,6 +18,20 @@ export function makeSlashCommandHook(store: Store<GlobalState>) {
             store.dispatch(setClientId(clientId));
 
             messageTrimmed = `/incident start ${clientId}`;
+
+            return Promise.resolve({message: messageTrimmed, args});
+        }
+
+        if (messageTrimmed && messageTrimmed.startsWith('/incident info')) {
+            const state = store.getState();
+            const currentChannel = getCurrentChannel(state);
+
+            if (isIncidentChannel(state, currentChannel.id) && !isIncidentRHSOpen(state)) {
+                //@ts-ignore thunk
+                store.dispatch(toggleRHS());
+
+                return Promise.resolve({});
+            }
 
             return Promise.resolve({message: messageTrimmed, args});
         }
