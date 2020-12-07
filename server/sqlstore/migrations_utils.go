25cbcd354008371d2ebd90eb55121f53ba50a00a
Christopher Poile <cpoile@gmail.com>
2020-12-07 14:54:11 -0500

    inline the database() call

--- server/sqlstore/migrations_utils.go
+++ server/sqlstore/migrations_utils.go
@@ -39,16 +39,9 @@ var addColumnToPGTable = func(e sqlx.Ext, tableName, columnName, columnType stri
 }
 
 var addColumnToMySQLTable = func(e sqlx.Ext, tableName, columnName, columnType string) error {
-	var dbName string
-	err := e.QueryRowx("SELECT DATABASE();").Scan(&dbName)
-	if err != nil {
-		return err
-	}
-
 	var result int
-	err = e.QueryRowx(
-		"SELECT 1 FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = ? AND TABLE_NAME = ? AND COLUMN_NAME = ?",
-		dbName,
+	err := e.QueryRowx(
+		"SELECT 1 FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = ? AND COLUMN_NAME = ?",
 		tableName,
 		columnName,
 	).Scan(&result)
