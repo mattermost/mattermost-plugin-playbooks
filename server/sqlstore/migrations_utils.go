607f9d449386397f14d648905c376c53510f4e13
Christopher Poile <cpoile@gmail.com>
2020-12-07 15:15:36 -0500

    Fix msql migrations (#405)
    * mysql needs table_schema set when querying if a column exists
    
    * Revert "mysql needs table_schema set when querying if a column exists"
    
    This reverts commit fd1a7031
    
    * Simpler fix
    
    * return error
    
    * inline the database() call

--- server/sqlstore/migrations_utils.go
+++ server/sqlstore/migrations_utils.go
@@ -41,7 +41,7 @@ var addColumnToPGTable = func(e sqlx.Ext, tableName, columnName, columnType stri
 var addColumnToMySQLTable = func(e sqlx.Ext, tableName, columnName, columnType string) error {
 	var result int
 	err := e.QueryRowx(
-		"SELECT 1 FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = ? AND COLUMN_NAME = ?",
+		"SELECT 1 FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = ? AND COLUMN_NAME = ?",
 		tableName,
 		columnName,
 	).Scan(&result)
