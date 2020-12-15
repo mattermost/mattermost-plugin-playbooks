9c108ef43cc9d7f495ed9b9628fbcf13556da90a
Jesse Hallam <jesse.hallam@gmail.com>
2020-12-15 08:33:41 -0400

    MM-31242: fix unusual backstage shadows (#415)
    Safari's treatment of `linear-gradient` has `transparent` meaning black
    transparent (namely `rgba(0,0,0,0)`).
    ([Reference](https://stackoverflow.com/questions/30673204/css-linear-gradient-transparency-misbehaving-only-in-safari).
    This is actually according to spec, but results in the undesirable
    behaviour below as we try to fade out the image assets in the backstage:
    
    Fix this by making a transparent version of the background colour over
    which to apply the gradient:

--- webapp/src/components/assets/left_fade.tsx
+++ webapp/src/components/assets/left_fade.tsx
@@ -6,7 +6,7 @@ const LeftFade = styled.div`
     top: 85px;
     left: 0;
     height: 100%;
-    background: linear-gradient(90deg, var(--center-channel-bg) 0%, rgba(255, 255, 255, 0) 94.89%);
+    background: linear-gradient(90deg, var(--center-channel-bg) 0%, rgba(var(--center-channel-bg-rgb), 0) 94.89%);
     pointer-events: none;
     z-index: -1;
 `;
