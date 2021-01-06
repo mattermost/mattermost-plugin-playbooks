014a27435f82dff195d4bf2e282c02452d77a571
Christopher Speller <crspeller@gmail.com>
2021-01-06 10:26:58 -0800

    Add a universal test for the MarshalJSON null issue.

--- server/incident/incident_test.go
+++ server/incident/incident_test.go
@@ -1,1 +1,17 @@
 package incident
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestIncident_MarshalJSON(t *testing.T) {
+	testIncident := &Incident{}
+	result, err := json.Marshal(testIncident)
+	require.NoError(t, err)
+	// Should not contain null. Triggering this?
+	// Add your new nullable thing to one of the MarshalJSONs in incident/incident.go
+	require.NotContains(t, string(result), "null")
+}
