9107233c1d06a618195833ea5d99f1a1d86288c0
Christopher Speller <crspeller@gmail.com>
2021-01-08 10:05:32 -0800

    More fix cypress tests.

--- tests-e2e/cypress/integration/frontstage/incident_rhs_list_spec.js
+++ tests-e2e/cypress/integration/frontstage/incident_rhs_list_spec.js
@@ -931,7 +931,8 @@ describe('rhs incident list', () => {
             });
         });
 
-        it('incident should be removed from list when the user closes incident and presses back button', () => {
+        /* TEMP disable, to be restored with alternate incident close method.
+         * it('incident should be removed from list when the user closes incident and presses back button', () => {
             // # Navigate directly to a non-incident channel
             cy.visit('/ad-1/channels/town-square');
 
@@ -985,7 +986,7 @@ describe('rhs incident list', () => {
                 // * Verify we cannot see the ended incident
                 cy.findByText(incidentName).should('not.exist');
             });
-        });
+        });*/
 
         it('incident should be removed from list when another user closes incident', () => {
             // # Navigate directly to a non-incident channel
--- tests-e2e/cypress/integration/frontstage/rhs/footer_spec.js
+++ tests-e2e/cypress/integration/frontstage/rhs/footer_spec.js
@@ -12,11 +12,6 @@ describe.only('incident rhs > footer', () => {
     let userId;
     let playbookId;
 
-    const biStagePlaybookName = playbookName + ' - two stages';
-    let biStagePlaybookId;
-    const triStagePlaybookName = playbookName + ' - three stages';
-    let triStagePlaybookId;
-
     before(() => {
         // # Login as user-1
         cy.apiLogin('user-1');
@@ -34,75 +29,18 @@ describe.only('incident rhs > footer', () => {
                 }).then((playbook) => {
                     playbookId = playbook.id;
                 });
-
-                cy.apiCreatePlaybook({
-                    teamId: team.id,
-                    title: biStagePlaybookName,
-                    checklists: [
-                        {
-                            title: 'Stage 1',
-                            items: [
-                                {title: 'Step 1'},
-                                {title: 'Step 2'},
-                            ],
-                        },
-                        {
-                            title: 'Stage 2',
-                            items: [
-                                {title: 'Step 1'},
-                                {title: 'Step 2'},
-                            ],
-                        },
-                    ],
-                    memberIDs: [userId],
-                }).then((playbook) => {
-                    biStagePlaybookId = playbook.id;
-                });
-
-                cy.apiCreatePlaybook({
-                    teamId: team.id,
-                    title: triStagePlaybookName,
-                    checklists: [
-                        {
-                            title: 'Stage 1',
-                            items: [
-                                {title: 'Step 1'},
-                                {title: 'Step 2'},
-                            ],
-                        },
-                        {
-                            title: 'Stage 2',
-                            items: [
-                                {title: 'Step 1'},
-                                {title: 'Step 2'},
-                            ],
-                        },
-                        {
-                            title: 'Stage 3',
-                            items: [
-                                {title: 'Step 1'},
-                                {title: 'Step 2'},
-                            ],
-                        },
-                    ],
-                    memberIDs: [userId],
-                }).then((playbook) => {
-                    triStagePlaybookId = playbook.id;
-                });
             });
         });
-    });
 
-    beforeEach(() => {
+        beforeEach(() => {
         // # Size the viewport to show the RHS without covering posts.
-        cy.viewport('macbook-13');
+            cy.viewport('macbook-13');
 
-        // # Login as user-1
-        cy.apiLogin('user-1');
-    });
+            // # Login as user-1
+            cy.apiLogin('user-1');
+        });
 
-    describe('has navigation button', () => {
-        describe('for incidents with one stage', () => {
+        describe('has navigation button', () => {
             beforeEach(() => {
                 const now = Date.now();
                 const incidentName = 'Incident (' + now + ')';
@@ -123,55 +61,15 @@ describe.only('incident rhs > footer', () => {
                 cy.findByTestId('tasks').click();
             });
 
-            it('with "End Incident" and secondary styled when tasks are not finished', () => {
-                // * Verify that the button contains End Incident and is secondary styled
-                cy.get('#incidentRHSFooter button').contains('End Incident').should('have.css', 'background-color', 'rgba(0, 0, 0, 0)');
+            it('should always say update status', () => {
+                // * Verify that the button contains Update Incident
+                cy.get('#incidentRHSFooter button').contains('Update Incident');
 
                 // # Click on the End Incident button
                 cy.get('#incidentRHSFooter button').click();
 
                 // * Verify that the interactive dialog is visible
                 cy.get('#interactiveDialogModalLabel').contains('Confirm End Incident');
-            });
-
-            it('with "End Incident" and primary styled when all tasks are finished', () => {
-                // # Check all checkboxes in the stage
-                cy.get('.checklist').within(() => {
-                    cy.get('.checkbox').each((checkbox) => {
-                        cy.wrap(checkbox).click();
-                    });
-                });
-
-                // # Check that the button contains End Incident and is primary styled
-                cy.get('#incidentRHSFooter button').contains('End Incident').should('have.css', 'background-color', 'rgb(22, 109, 224)');
-
-                // # Click on the End Incident button
-                cy.get('#incidentRHSFooter button').click();
-
-                // * Verify that the interactive dialog is visible
-                cy.get('#interactiveDialogModalLabel').contains('Confirm End Incident');
-            });
-
-            it('with "Restart Incident" when the incident is ended', () => {
-                // # Click on the End Incident button
-                cy.get('#incidentRHSFooter button').click();
-
-                // # Confirm the dialog
-                cy.get('#interactiveDialogSubmit').click();
-
-                // * Verify that the button contains Restart Incident
-                cy.get('#incidentRHSFooter button').contains('Restart Incident');
-
-                // # Restart the incident
-                cy.get('#incidentRHSFooter button').click();
-
-                // * Verify that the incident is ongoing
-                cy.get('#rhsContainer').within(() => {
-                    cy.get('.sidebar--right__title').contains('Ongoing');
-                });
-
-                // * Verify that the button contains End Incident again
-                cy.get('#incidentRHSFooter button').contains('End Incident');
             });
         });
     });
