60b43ff8a4b31d2d8227210d056a087e09698f51
Christopher Poile <cpoile@gmail.com>
2021-01-20 14:47:18 -0500

    create an Event[] from post and user data

--- webapp/src/components/rhs/rhs_timeline.tsx
+++ webapp/src/components/rhs/rhs_timeline.tsx
@@ -1,10 +1,21 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import React from 'react';
-import {useDispatch} from 'react-redux';
+import React, {useEffect, useState} from 'react';
+import {useDispatch, useSelector, useStore} from 'react-redux';
 import styled from 'styled-components';
 import moment from 'moment';
+
+import {GlobalState} from 'mattermost-redux/types/store';
+import {Post} from 'mattermost-redux/types/posts';
+import {getPost} from 'mattermost-redux/selectors/entities/posts';
+import {getPost as getPostAction} from 'mattermost-redux/actions/posts';
+import {getUser} from 'mattermost-redux/selectors/entities/users';
+import {getUser as getUserAction} from 'mattermost-redux/actions/users';
+import {UserProfile} from 'mattermost-redux/types/users';
+import {displayUsername} from 'mattermost-redux/utils/user_utils';
+import {getTeammateNameDisplaySetting} from 'mattermost-redux/selectors/entities/preferences';
+import {DispatchFunc} from 'mattermost-redux/types/actions';
 
 import {Incident} from 'src/types/incident';
 import {isMobile} from 'src/mobile';
@@ -36,6 +47,20 @@ const Circle = styled.div`
     border-radius: 50%;
     left: 80px;
     z-index: 3;
+
+    :before {
+        font-family: 'compass-icons';
+        text-rendering: auto;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        content: '\\xf23b';
+        font-size: 12px;
+        font-weight: bold;
+        color: var(--button-bg);
+        transition: transform 0.15s;
+        transform: scale(0) rotate(90deg);
+        position: relative;
+    }
 `;
 
 const TimelineItem = styled.li`
@@ -136,12 +161,65 @@ const SummaryDetail = styled.div`
 //     }
 // `;
 
+type IdToPostFn = (postId: string) => Post;
+type IdToUserFn = (userId: string) => UserProfile;
+
+interface Event {
+    create_at: number;
+    post_id: string;
+    display_name: string;
+    status: string;
+}
+
 interface Props {
     incident: Incident;
 }
 
 const RHSTimeline = (props: Props) => {
     const dispatch = useDispatch();
+    const displayPreference = useSelector<GlobalState, string | undefined>(getTeammateNameDisplaySetting) || 'username';
+    const getStateFn = useStore().getState;
+    const getPostFn = (postId: string) => getPostAction(postId)(dispatch as DispatchFunc, getStateFn);
+    const getUserFn = (userId: string) => getUserAction(userId)(dispatch as DispatchFunc, getStateFn);
+
+    const selectPost = useSelector<GlobalState, IdToPostFn>((state) => (postId: string) => getPost(state, postId));
+    const selectUser = useSelector<GlobalState, IdToUserFn>((state) => (userId: string) => getUser(state, userId));
+
+    const [events, setEvents] = useState<Event[]>([]);
+
+    useEffect(() => {
+        Promise.all(props.incident.status_posts.map(async (p) => {
+            let post = selectPost(p.id) as Post | undefined;
+
+            if (!post) {
+                const ret = await getPostFn(p.id);
+                if (!ret.data) {
+                    return Promise.reject(new Error('no post'));
+                }
+                post = ret.data;
+            }
+
+            let user = selectUser(post.user_id) as UserProfile | undefined;
+
+            if (!user) {
+                const ret = await getUserFn(post.user_id) as {data?: UserProfile, error?: any};
+                if (!ret.data) {
+                    return Promise.reject(new Error('no user'));
+                }
+                user = ret.data;
+            }
+
+            const displayName = displayUsername(user, displayPreference);
+            return {
+                create_at: p.create_at,
+                post_id: p.id,
+                display_name: displayName,
+                status: p.status || 'unset',
+            } as Event;
+        })).then((eventArray) => {
+            setEvents(eventArray);
+        });
+    }, [props.incident.status_posts, displayPreference]);
 
     const goToPost = (e: React.MouseEvent<Element, MouseEvent>, postId: string) => {
         e.preventDefault();
@@ -157,19 +235,19 @@ const RHSTimeline = (props: Props) => {
     return (
         <Timeline>
             {
-                props.incident.status_posts.map((p) => {
+                events.map((event) => {
                     return (
-                        <TimelineItem key={p.id}>
-                            <TimeContainer onClick={(e) => goToPost(e, p.id)}>
-                                <TimeHours>{moment(p.create_at).format('HH:mm:ss')}</TimeHours>
-                                <TimeDay>{moment(p.create_at).format('MMM DD')}</TimeDay>
+                        <TimelineItem key={event.post_id}>
+                            <TimeContainer onClick={(e) => goToPost(e, event.post_id)}>
+                                <TimeHours>{moment(event.create_at).format('HH:mm:ss')}</TimeHours>
+                                <TimeDay>{moment(event.create_at).format('MMM DD')}</TimeDay>
                             </TimeContainer>
-                            <Circle onClick={(e) => goToPost(e, p.id)}/>
+                            <Circle onClick={(e) => goToPost(e, event.post_id)}/>
                             {/*<ClickableLine onClick={() => console.log('add an event')}/>*/}
                             {/*<Button className={'button'}>{'+'}</Button>*/}
-                            <SummaryContainer onClick={(e) => goToPost(e, p.id)}>
+                            <SummaryContainer onClick={(e) => goToPost(e, event.post_id)}>
                                 <SummaryTitle>{'Incident Status Update'}</SummaryTitle>
-                                <SummaryDetail>{'Updated, id: ' + p.id}</SummaryDetail>
+                                <SummaryDetail>{event.display_name + ' updated incident to ' + event.status}</SummaryDetail>
                             </SummaryContainer>
                         </TimelineItem>
                     );
