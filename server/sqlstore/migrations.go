4600241e402f34ebfc5f4c0079b261bf1839d5ac
Christopher Speller <crspeller@gmail.com>
2021-01-24 19:51:46 -0800

    Better db migration.

--- server/sqlstore/migrations.go
+++ server/sqlstore/migrations.go
@@ -340,19 +340,22 @@ var migrations = []Migration{
 		toVersion:   semver.MustParse("0.6.0"),
 		migrationFunc: func(e sqlx.Ext, sqlStore *SQLStore) error {
 			if e.DriverName() == model.DATABASE_DRIVER_MYSQL {
-				if err := addColumnToMySQLTable(e, "IR_Incident", "CurrentStatus", "VARCHAR(1024) NOT NULL DEFAULT ''"); err != nil {
+				if err := addColumnToMySQLTable(e, "IR_Incident", "CurrentStatus", "VARCHAR(1024) NOT NULL DEFAULT 'Active'"); err != nil {
 					return errors.Wrapf(err, "failed adding column CurrentStatus to table IR_Incident")
 				}
 				if err := addColumnToMySQLTable(e, "IR_StatusPosts", "Status", "VARCHAR(1024) NOT NULL DEFAULT ''"); err != nil {
 					return errors.Wrapf(err, "failed adding column Status to table IR_StatusPosts")
 				}
 			} else {
-				if err := addColumnToPGTable(e, "IR_Incident", "CurrentStatus", "TEXT NOT NULL DEFAULT ''"); err != nil {
+				if err := addColumnToPGTable(e, "IR_Incident", "CurrentStatus", "TEXT NOT NULL DEFAULT 'Active'"); err != nil {
 					return errors.Wrapf(err, "failed adding column CurrentStatus to table IR_Incident")
 				}
 				if err := addColumnToPGTable(e, "IR_StatusPosts", "Status", "TEXT NOT NULL DEFAULT ''"); err != nil {
 					return errors.Wrapf(err, "failed adding column Status to table IR_StatusPosts")
 				}
+			}
+			if _, err := e.Exec("UPDATE IR_Incident SET CurrentStatus = 'Resolved' WHERE EndAt != 0"); err != nil {
+				return errors.Wrapf(err, "failed adding column ReminderMessageTemplate to table IR_Incident")
 			}
 			return nil
 		},
