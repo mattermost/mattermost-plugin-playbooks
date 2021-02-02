bf8071aa6cde25d58a6dfd271071ce468e6760cb
Alejandro García Montoro <alejandro.garciamontoro@gmail.com>
2021-02-02 16:55:02 +0100

    Add telemetry on reminder and broadcast features (#441)

--- server/telemetry/rudder.go
+++ server/telemetry/rudder.go
@@ -147,6 +147,7 @@ func (t *RudderTelemetry) ChangeCommander(incdnt *incident.Incident, userID stri
 func (t *RudderTelemetry) UpdateStatus(incdnt *incident.Incident, userID string) {
 	properties := incidentProperties(incdnt, userID)
 	properties["Action"] = actionUpdateStatus
+	properties["ReminderTimerSeconds"] = int(incdnt.PreviousReminder)
 	t.track(eventIncident, properties)
 }
 
@@ -228,14 +229,17 @@ func playbookProperties(pbook playbook.Playbook, userID string) map[string]inter
 	}
 
 	return map[string]interface{}{
-		"UserActualID":        userID,
-		"PlaybookID":          pbook.ID,
-		"TeamID":              pbook.TeamID,
-		"NumChecklists":       len(pbook.Checklists),
-		"TotalChecklistItems": totalChecklistItems,
-		"IsPublic":            pbook.CreatePublicIncident,
-		"NumMembers":          len(pbook.MemberIDs),
-		"NumSlashCommands":    totalChecklistItemsWithCommands,
+		"UserActualID":                userID,
+		"PlaybookID":                  pbook.ID,
+		"TeamID":                      pbook.TeamID,
+		"NumChecklists":               len(pbook.Checklists),
+		"TotalChecklistItems":         totalChecklistItems,
+		"IsPublic":                    pbook.CreatePublicIncident,
+		"NumMembers":                  len(pbook.MemberIDs),
+		"NumSlashCommands":            totalChecklistItemsWithCommands,
+		"ReminderTimerDefaultSeconds": pbook.ReminderTimerDefaultSeconds,
+		"BroadcastChannelID":          pbook.BroadcastChannelID,
+		"UsesReminderMessageTemplate": pbook.ReminderMessageTemplate != "",
 	}
 }
 
