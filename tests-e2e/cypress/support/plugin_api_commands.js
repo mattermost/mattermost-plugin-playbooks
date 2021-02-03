9e092eeddb23bf02b02f391b92a9c1c3eb2da442
Christopher Poile <cpoile@gmail.com>
2021-02-02 19:32:08 -0500

    remove debug statements; don't run e2e on master

--- tests-e2e/cypress/support/plugin_api_commands.js
+++ tests-e2e/cypress/support/plugin_api_commands.js
@@ -169,16 +169,11 @@ Cypress.Commands.add('verifyIncidentActive', (teamId, incidentName, incidentDesc
         assert.equal(incident.end_at, 0);
         assert.equal(incident.name, incidentName);
 
-        cy.log('test 1');
-
         // Only check the description if provided. The server may supply a default depending
         // on how the incident was started.
         if (incidentDescription) {
-            cy.log('test 1.5');
             assert.equal(incident.description, incidentDescription);
         }
-
-        cy.log('test 1.6');
     });
 });
 
