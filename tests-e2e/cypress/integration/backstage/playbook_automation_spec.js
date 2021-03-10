91ebb43a1545bc91ac381e305abfd54a254a7400
Prapti <praptishrestha@gmail.com>
2021-03-09 23:42:59 -0800

    Fixing rest of the playbook automation tests

--- tests-e2e/cypress/integration/backstage/playbook_automation_spec.js
+++ tests-e2e/cypress/integration/backstage/playbook_automation_spec.js
@@ -12,48 +12,20 @@ function createTestUsers(testUser, returnedUser, teamId) {
 }
 
 describe('invite members setting', () => {
-    // const playbookName = 'Playbook (' + Date.now() + ')';
-    // let playbookName;
-    // let playbookId;
-    // let teamId;
-    // let userId;
     let testTeam;
     let testUser1;
-    let testPlaybook;
     let playbookId;
     let testUser2;
     let testUser3;
+    let testUser4;
 
     before(() => {
-        // // # Login as user-1
-        // cy.apiLogin('user-1');
-
-        // // # Create a playbook
-        // cy.apiGetTeamByName('ad-1').then((team) => {
-        //     teamId = team.id;
-        //     cy.apiGetCurrentUser().then((user) => {
-        //         userId = user.id;
-        //         cy.apiCreateTestPlaybook({
-        //             teamId,
-        //             title: playbookName,
-        //             userId,
-        //         }).then((playbook) => {
-        //             playbookId = playbook.id;
-        //         });
-
-        //         cy.verifyPlaybookCreated(teamId, playbookName);
-        //     });
-        // });
         cy.apiInitSetup({createPlaybook: true}).then(({team, user, playbook}) => {
             testTeam = team;
             testUser1 = user;
-            testPlaybook = playbook;
             playbookId = playbook.id;
 
-            cy.log('----- 1st init setup done -----');
-
             cy.apiCreateUser({prefix: 'other'}).then(({user: user2}) => {
-                cy.log('..... now creating user 2 .....');
                 testUser2 = user2;
                 cy.apiAddUserToTeam(team.id, user2.id);
             });
@@ -61,6 +33,11 @@ describe('invite members setting', () => {
             cy.apiCreateUser().then(({user: user3}) => {
                 testUser3 = user3;
                 cy.apiAddUserToTeam(team.id, user3.id);
+            });
+
+            cy.apiCreateUser().then(({user: user4}) => {
+                testUser4 = user4;
+                cy.apiAddUserToTeam(team.id, user4.id);
             });
         });
     });
@@ -144,226 +121,208 @@ describe('invite members setting', () => {
         });
     });
 
-    // it('allows adding new users to an already populated list', () => {
-    //     // # Visit the selected playbook
-    //     cy.visit('/ad-1/com.mattermost.plugin-incident-management/playbooks/' + playbookId);
+    it('allows adding new users to an already populated list', () => {
+        // # Visit the selected playbook
+        cy.visit(`/${testTeam.name}/com.mattermost.plugin-incident-management/playbooks/` + playbookId);
 
-    //     // # Switch to Automation tab
-    //     cy.get('#root').findByText('Automation').click();
+        // # Switch to Automation tab
+        cy.get('#root').findByText('Automation').click();
 
-    //     cy.get('#invite-users').within(() => {
-    //         // * Verify that the toggle is unchecked
-    //         cy.get('label input').should('not.be.checked');
+        cy.get('#invite-users').within(() => {
+            // * Verify that the toggle is unchecked
+            cy.get('label input').should('not.be.checked');
 
-    //         // # Click on the toggle to enable the setting
-    //         cy.get('label input').click({force: true});
+            // # Click on the toggle to enable the setting
+            cy.get('label input').click({force: true});
 
-    //         // * Verify that the toggle is checked
-    //         cy.get('label input').should('be.checked');
+            // * Verify that the toggle is checked
+            cy.get('label input').should('be.checked');
 
-    //         // # Open the invited users selector
-    //         cy.openInvitedUsersSelector();
+            // # Open the invited users selector
+            cy.openInvitedUsersSelector();
 
-    //         // # Add one user
-    //         cy.addInvitedUser('aaron.medina');
+            // # Add one user
+            cy.addInvitedUser(`${testUser3.username}`);
 
-    //         // * Verify that the user shows in the group of invited members
-    //         cy.findByText('INVITED MEMBERS').parent().within(() => {
-    //             cy.findByText('aaron.medina');
-    //         });
+            // * Verify that the user shows in the group of invited members
+            cy.findByText('INVITED MEMBERS').parent().within(() => {
+                cy.findByText(`${testUser3.username}`);
+            });
 
-    //         // # Add a new user
-    //         cy.addInvitedUser('alice.johnston');
+            // # Add a new user
+            cy.addInvitedUser(`${testUser4.username}`);
 
-    //         // * Verify that the badge in the selector shows the correct number of members
-    //         cy.get('.invite-users-selector__control')
-    //             .after('content')
-    //             .should('eq', '2 MEMBERS');
+            // * Verify that the badge in the selector shows the correct number of members
+            cy.get('.invite-users-selector__control')
+                .after('content')
+                .should('eq', '2 MEMBERS');
 
-    //         // * Verify that the user shows in the group of invited members
-    //         cy.findByText('INVITED MEMBERS').parent().within(() => {
-    //             cy.findByText('aaron.medina');
-    //             cy.findByText('alice.johnston');
-    //         });
-    //     });
-    // });
+            // * Verify that the user shows in the group of invited members
+            cy.findByText('INVITED MEMBERS').parent().within(() => {
+                cy.findByText(`${testUser3.username}`);
+                cy.findByText(`${testUser4.username}`);
+            });
+        });
+    });
 
-    // it('allows removing users', () => {
-    //     // # Visit the selected playbook
-    //     cy.visit('/ad-1/com.mattermost.plugin-incident-management/playbooks/' + playbookId);
+    it('allows removing users', () => {
+        // # Visit the selected playbook
+        cy.visit(`/${testTeam.name}/com.mattermost.plugin-incident-management/playbooks/` + playbookId);
 
-    //     // # Switch to Automation tab
-    //     cy.get('#root').findByText('Automation').click();
+        // # Switch to Automation tab
+        cy.get('#root').findByText('Automation').click();
 
-    //     cy.get('#invite-users').within(() => {
-    //         // * Verify that the toggle is unchecked
-    //         cy.get('label input').should('not.be.checked');
+        cy.get('#invite-users').within(() => {
+            // * Verify that the toggle is unchecked
+            cy.get('label input').should('not.be.checked');
 
-    //         // # Click on the toggle to enable the setting
-    //         cy.get('label input').click({force: true});
+            // # Click on the toggle to enable the setting
+            cy.get('label input').click({force: true});
 
-    //         // * Verify that the toggle is checked
-    //         cy.get('label input').should('be.checked');
+            // * Verify that the toggle is checked
+            cy.get('label input').should('be.checked');
 
-    //         // # Open the invited users selector
-    //         cy.openInvitedUsersSelector();
+            // # Open the invited users selector
+            cy.openInvitedUsersSelector();
 
-    //         // # Add a couple of users
-    //         cy.addInvitedUser('aaron.medina');
-    //         cy.addInvitedUser('alice.johnston');
+            // # Add a couple of users
+            cy.addInvitedUser(`${testUser3.username}`);
+            cy.addInvitedUser(`${testUser4.username}`);
 
-    //         // * Verify that the badge in the selector shows the correct number of members
-    //         cy.get('.invite-users-selector__control')
-    //             .after('content')
-    //             .should('eq', '2 MEMBERS');
+            // * Verify that the badge in the selector shows the correct number of members
+            cy.get('.invite-users-selector__control')
+                .after('content')
+                .should('eq', '2 MEMBERS');
 
-    //         // # Remove the first users added
-    //         cy.get('.invite-users-selector__option').eq(0).within(() => {
-    //             cy.findByText('Remove').click();
-    //         });
+            // # Remove the first users added
+            cy.get('.invite-users-selector__option').eq(0).within(() => {
+                cy.findByText('Remove').click();
+            });
 
-    //         // * Verify that there is only one user, the one not removed
-    //         cy.get('.invite-users-selector__control')
-    //             .after('content')
-    //             .should('eq', '1 MEMBER');
+            // * Verify that there is only one user, the one not removed
+            cy.get('.invite-users-selector__control')
+                .after('content')
+                .should('eq', '1 MEMBER');
 
-    //         cy.findByText('INVITED MEMBERS').parent().within(() => {
-    //             cy.get('.invite-users-selector__option')
-    //                 .should('have.length', 1)
-    //                 .contains('alice.johnston');
-    //         });
-    //     });
-    // });
+            cy.findByText('INVITED MEMBERS').parent().within(() => {
+                cy.get('.invite-users-selector__option')
+                    .should('have.length', 1)
+                    .contains(`${testUser4.username}`);
+            });
+        });
+    });
 
-    // it('persists the list of users persists even if the toggle is off', () => {
-    //     // # Visit the selected playbook
-    //     cy.visit('/ad-1/com.mattermost.plugin-incident-management/playbooks/' + playbookId);
+    it('persists the list of users even if the toggle is off', () => {
+        // # Visit the selected playbook
+        cy.visit(`/${testTeam.name}/com.mattermost.plugin-incident-management/playbooks/` + playbookId);
 
-    //     // # Switch to Automation tab
-    //     cy.get('#root').findByText('Automation').click();
+        // # Switch to Automation tab
+        cy.get('#root').findByText('Automation').click();
 
-    //     cy.get('#invite-users').within(() => {
-    //         // * Verify that the toggle is unchecked
-    //         cy.get('label input').should('not.be.checked');
+        cy.get('#invite-users').within(() => {
+            // * Verify that the toggle is unchecked
+            cy.get('label input').should('not.be.checked');
 
-    //         // # Click on the toggle to enable the setting
-    //         cy.get('label input').click({force: true});
+            // # Click on the toggle to enable the setting
+            cy.get('label input').click({force: true});
 
-    //         // * Verify that the toggle is checked
-    //         cy.get('label input').should('be.checked');
+            // * Verify that the toggle is checked
+            cy.get('label input').should('be.checked');
 
-    //         // # Open the invited users selector
-    //         cy.openInvitedUsersSelector();
+            // # Open the invited users selector
+            cy.openInvitedUsersSelector();
 
-    //         // # Add a couple of users
-    //         cy.addInvitedUser('aaron.medina');
-    //         cy.addInvitedUser('alice.johnston');
+            // # Add a couple of users
+            cy.addInvitedUser(`${testUser3.username}`);
+            cy.addInvitedUser(`${testUser4.username}`);
 
-    //         // * Verify that the badge in the selector shows the correct number of members
-    //         cy.get('.invite-users-selector__control')
-    //             .after('content')
-    //             .should('eq', '2 MEMBERS');
+            // * Verify that the badge in the selector shows the correct number of members
+            cy.get('.invite-users-selector__control')
+                .after('content')
+                .should('eq', '2 MEMBERS');
 
-    //         // # Click on the toggle to disable the setting
-    //         cy.get('label input').click({force: true});
+            // # Click on the toggle to disable the setting
+            cy.get('label input').click({force: true});
 
-    //         // * Verify that the toggle is unchecked
-    //         cy.get('label input').should('not.be.checked');
-    //     });
+            // * Verify that the toggle is unchecked
+            cy.get('label input').should('not.be.checked');
+        });
 
-    //     // # Save the playbook
-    //     cy.findByTestId('save_playbook').click();
+        // # Save the playbook
+        cy.findByTestId('save_playbook').click();
 
-    //     // # Navigate again to the playbook
-    //     cy.visit('/ad-1/com.mattermost.plugin-incident-management/playbooks/' + playbookId);
+        // # Navigate again to the playbook
+        cy.visit(`/${testTeam.name}/com.mattermost.plugin-incident-management/playbooks/` + playbookId);
 
-    //     // # Switch to Automation tab
-    //     cy.get('#root').findByText('Automation').click();
+        // # Switch to Automation tab
+        cy.get('#root').findByText('Automation').click();
 
-    //     cy.get('#invite-users').within(() => {
-    //         // * Verify that the toggle is unchecked
-    //         cy.get('label input').should('not.be.checked');
+        cy.get('#invite-users').within(() => {
+            // * Verify that the toggle is unchecked
+            cy.get('label input').should('not.be.checked');
 
-    //         // # Click on the toggle to enable the setting
-    //         cy.get('label input').click({force: true});
+            // # Click on the toggle to enable the setting
+            cy.get('label input').click({force: true});
 
-    //         // * Verify that the toggle is checked
-    //         cy.get('label input').should('be.checked');
+            // * Verify that the toggle is checked
+            cy.get('label input').should('be.checked');
 
-    //         // * Verify that the badge in the selector shows the correct number of members
-    //         cy.get('.invite-users-selector__control')
-    //             .after('content')
-    //             .should('eq', '2 MEMBERS');
+            // * Verify that the badge in the selector shows the correct number of members
+            cy.get('.invite-users-selector__control')
+                .after('content')
+                .should('eq', '2 MEMBERS');
 
-    //         // # Open the invited users selector
-    //         cy.openInvitedUsersSelector();
+            // # Open the invited users selector
+            cy.openInvitedUsersSelector();
 
-    //         // * Verify that the user shows in the group of invited members
-    //         cy.findByText('INVITED MEMBERS').parent().within(() => {
-    //             cy.findByText('aaron.medina');
-    //             cy.findByText('alice.johnston');
-    //         });
-    //     });
-    // });
+            // * Verify that the user shows in the group of invited members
+            cy.findByText('INVITED MEMBERS').parent().within(() => {
+                cy.findByText(`${testUser3.username}`);
+                cy.findByText(`${testUser4.username}`);
+            });
+        });
+    });
 
-    // it('removes invitation from users that are no longer in the team', () => {
-    //     let userToRemove;
+    it('removes invitation from users that are no longer in the team', () => {
+        let userToRemove;
 
-    //     // # Create a playbook with a user that is later removed from the team
-    //     cy.apiLogin('sysadmin').then(() => {
-    //         // # We need to increase the maximum number of users per team; otherwise,
-    //         // adding a new member to the team fails in CI
-    //         cy.apiUpdateConfig({
-    //             TeamSettings: {
-    //                 MaxUsersPerTeam: 1000,
-    //             },
-    //         });
+        // # Create a playbook with a user that is later removed from the team
+        cy.apiLogin('sysadmin').then(() => {
+            // # We need to increase the maximum number of users per team; otherwise,
+            // adding a new member to the team fails in CI
+            cy.apiUpdateConfig({
+                TeamSettings: {
+                    MaxUsersPerTeam: 1000,
+                },
+            });
+            cy.apiRemoveUserFromTeam(testTeam.id, testUser3.id);
+            cy.apiRemoveUserFromTeam(testTeam.id, testUser4.id);
+        }).then(() => {
+            cy.apiLogin(testUser1);
 
-    //         cy.apiCreateUser().then((result) => {
-    //             userToRemove = result.user;
-    //             cy.apiAddUserToTeam(teamId, userToRemove.id);
+            // # Navigate again to the playbook
+            cy.visit(`/${testTeam.name}/com.mattermost.plugin-incident-management/playbooks/` + playbookId);
 
-    //             // # Create a playbook with the user that will be removed from the team.
-    //             cy.apiCreatePlaybook({
-    //                 teamId,
-    //                 title: 'Playbook (' + Date.now() + ')',
-    //                 createPublicIncident: true,
-    //                 memberIDs: [userId],
-    //                 invitedUserIds: [userToRemove.id],
-    //                 inviteUsersEnabled: true,
-    //             }).then((playbook) => {
-    //                 playbookId = playbook.id;
-    //             });
+            // # Switch to Automation tab
+            cy.get('#root').findByText('Automation').click();
 
-    //             // # Remove user from the team
-    //             cy.apiRemoveUserFromTeam(teamId, userToRemove.id);
-    //         });
-    //     }).then(() => {
-    //         cy.apiLogin('user-1');
+            // # Save the playbook
+            cy.findByTestId('save_playbook').click();
 
-    //         // # Navigate again to the playbook
-    //         cy.visit('/ad-1/com.mattermost.plugin-incident-management/playbooks/' + playbookId);
+            // * Make sure the playbook is correctly saved
+            cy.url().should('not.include', playbookId);
 
-    //         // # Switch to Automation tab
-    //         cy.get('#root').findByText('Automation').click();
+            // # Navigate again to the playbook
+            cy.visit(`/${testTeam.name}/com.mattermost.plugin-incident-management/playbooks/` + playbookId);
 
-    //         // # Save the playbook
-    //         cy.findByTestId('save_playbook').click();
+            // # Switch to Automation tab
+            cy.get('#root').findByText('Automation').click();
 
-    //         // * Make sure the playbook is correctly saved
-    //         cy.url().should('not.include', playbookId);
+            // # Open the invited users selector
+            cy.openInvitedUsersSelector();
 
-    //         // # Navigate again to the playbook
-    //         cy.visit('/ad-1/com.mattermost.plugin-incident-management/playbooks/' + playbookId);
-
-    //         // # Switch to Automation tab
-    //         cy.get('#root').findByText('Automation').click();
-
-    //         // # Open the invited users selector
-    //         cy.openInvitedUsersSelector();
-
-    //         // * Verify that there are no invited members
-    //         cy.findByText('INVITED MEMBERS').should('not.exist');
-    //     });
-    // });
+            // * Verify that there are no invited members
+            cy.findByText('INVITED MEMBERS').should('not.exist');
+        });
+    });
 });
