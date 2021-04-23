7d7d28360ef7fe92b24545878e67438342f0098a
Hakan SARIMAN <hknsrmn46@gmail.com>
2021-04-23 17:18:44 +0300

    sqlstore migrate function added for webhook on archive

--- server/sqlstore/migrations.go
+++ server/sqlstore/migrations.go
@@ -670,7 +670,44 @@ var migrations = []Migration{
 					return errors.Wrapf(err, "failed adding column WebhookOnArchiveEnabled to table IR_Playbook")
 				}
 			}
+			return nil
+		},
+	},
+	{
+		fromVersion: semver.MustParse("0.14.0"),
+		toVersion:   semver.MustParse("0.15.0"),
+		migrationFunc: func(e sqlx.Ext, sqlStore *SQLStore) error {
+			if e.DriverName() == model.DATABASE_DRIVER_MYSQL {
+				if err := addColumnToMySQLTable(e, "IR_Incident", "WebhookOnArchiveURL", "TEXT"); err != nil {
+					return errors.Wrapf(err, "failed adding column WebhookOnArchiveURL to table IR_Incident")
+				}
+				if _, err := e.Exec("UPDATE IR_Incident SET WebhookOnArchiveURL = '' WHERE WebhookOnArchiveURL IS NULL"); err != nil {
+					return errors.Wrapf(err, "failed setting default value in column WebhookOnArchiveURL of table IR_Incident")
+				}
 
+				if err := addColumnToMySQLTable(e, "IR_Playbook", "WebhookOnArchiveURL", "TEXT"); err != nil {
+					return errors.Wrapf(err, "failed adding column WebhookOnArchiveURL to table IR_Playbook")
+				}
+				if _, err := e.Exec("UPDATE IR_Playbook SET WebhookOnArchiveURL = '' WHERE WebhookOnArchiveURL IS NULL"); err != nil {
+					return errors.Wrapf(err, "failed setting default value in column WebhookOnArchiveURL of table IR_Playbook")
+				}
+
+				if err := addColumnToMySQLTable(e, "IR_Playbook", "WebhookOnArchiveEnabled", "BOOLEAN DEFAULT FALSE"); err != nil {
+					return errors.Wrapf(err, "failed adding column WebhookOnArchiveEnabled to table IR_Playbook")
+				}
+			} else {
+				if err := addColumnToPGTable(e, "IR_Incident", "WebhookOnArchiveURL", "TEXT DEFAULT ''"); err != nil {
+					return errors.Wrapf(err, "failed adding column WebhookOnArchiveURL to table IR_Incident")
+				}
+
+				if err := addColumnToPGTable(e, "IR_Playbook", "WebhookOnArchiveURL", "TEXT DEFAULT ''"); err != nil {
+					return errors.Wrapf(err, "failed adding column WebhookOnArchiveURL to table IR_Playbook")
+				}
+
+				if err := addColumnToPGTable(e, "IR_Playbook", "WebhookOnArchiveEnabled", "BOOLEAN DEFAULT FALSE"); err != nil {
+					return errors.Wrapf(err, "failed adding column WebhookOnArchiveEnabled to table IR_Playbook")
+				}
+			}
 			return nil
 		},
 	},
