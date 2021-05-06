e21ec3a34478580c99cb7255ad555d11af03f244
Christopher Poile <cpoile@gmail.com>
2021-05-06 14:51:10 -0400

    varchar -> text for mysql

--- server/sqlstore/migrations.go
+++ server/sqlstore/migrations.go
@@ -647,16 +647,24 @@ var migrations = []Migration{
 		toVersion:   semver.MustParse("0.15.0"),
 		migrationFunc: func(e sqlx.Ext, sqlStore *SQLStore) error {
 			if e.DriverName() == model.DATABASE_DRIVER_MYSQL {
-				if err := addColumnToMySQLTable(e, "IR_Playbook", "MessageOnJoin", "VARCHAR(2048)"); err != nil {
+				if err := addColumnToMySQLTable(e, "IR_Playbook", "MessageOnJoin", "TEXT"); err != nil {
 					return errors.Wrapf(err, "failed adding column MessageOnJoin to table IR_Playbook")
+				}
+
+				if _, err := e.Exec("UPDATE IR_Playbook SET MessageOnJoin = '' WHERE MessageOnJoin IS NULL"); err != nil {
+					return errors.Wrapf(err, "failed setting default value in column MessageOnJoin of table IR_Playbook")
 				}
 
 				if err := addColumnToMySQLTable(e, "IR_Playbook", "MessageOnJoinEnabled", "BOOLEAN DEFAULT FALSE"); err != nil {
 					return errors.Wrapf(err, "failed adding column MessageOnJoinEnabled to table IR_Playbook")
 				}
 
-				if err := addColumnToMySQLTable(e, "IR_Incident", "MessageOnJoin", "VARCHAR(2048)"); err != nil {
+				if err := addColumnToMySQLTable(e, "IR_Incident", "MessageOnJoin", "TEXT"); err != nil {
 					return errors.Wrapf(err, "failed adding column MessageOnJoin to table IR_Incident")
+				}
+
+				if _, err := e.Exec("UPDATE IR_Incident SET MessageOnJoin = '' WHERE MessageOnJoin IS NULL"); err != nil {
+					return errors.Wrapf(err, "failed setting default value in column MessageOnJoin of table IR_Incident")
 				}
 
 				if _, err := e.Exec(`
