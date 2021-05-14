a52f193dc913a98c24b033f7eb8665e71bbec31d
madhavhugar <madhav.hugar@gmail.com>
2021-05-14 18:25:38 +0530

    Use handle error with code with receiver

--- server/api/playbooks.go
+++ server/api/playbooks.go
@@ -92,13 +92,13 @@ func (h *PlaybookHandler) createPlaybook(w http.ResponseWriter, r *http.Request)
 	if pbook.WebhookOnStatusUpdateURL != "" {
 		url, err := url.ParseRequestURI(pbook.WebhookOnStatusUpdateURL)
 		if err != nil {
-			HandleErrorWithCode(w, http.StatusBadRequest, "invalid update webhook URL", err)
+			h.HandleErrorWithCode(w, http.StatusBadRequest, "invalid update webhook URL", err)
 			return
 		}
 
 		if url.Scheme != "http" && url.Scheme != "https" {
 			msg := fmt.Sprintf("protocol in update webhook URL is %s; only HTTP and HTTPS are accepted", url.Scheme)
-			HandleErrorWithCode(w, http.StatusBadRequest, msg, errors.Errorf(msg))
+			h.HandleErrorWithCode(w, http.StatusBadRequest, msg, errors.Errorf(msg))
 			return
 		}
 	}
@@ -181,13 +181,13 @@ func (h *PlaybookHandler) updatePlaybook(w http.ResponseWriter, r *http.Request)
 	if pbook.WebhookOnStatusUpdateEnabled {
 		url, err2 := url.ParseRequestURI(pbook.WebhookOnStatusUpdateURL)
 		if err2 != nil {
-			HandleErrorWithCode(w, http.StatusBadRequest, "invalid update webhook URL", err2)
+			h.HandleErrorWithCode(w, http.StatusBadRequest, "invalid update webhook URL", err2)
 			return
 		}
 
 		if url.Scheme != "http" && url.Scheme != "https" {
 			msg := fmt.Sprintf("protocol in update webhook URL is %s; only HTTP and HTTPS are accepted", url.Scheme)
-			HandleErrorWithCode(w, http.StatusBadRequest, msg, errors.Errorf(msg))
+			h.HandleErrorWithCode(w, http.StatusBadRequest, msg, errors.Errorf(msg))
 			return
 		}
 	}
