d3436b8720bccd8c8a2822d7ccdea38a24bde2e3
Christopher Speller <crspeller@gmail.com>
2021-06-15 08:17:59 -0700

    MM-36262 Disable buttons on older or published retrospective reminders. (#584)
    * Disable buttons on older or published retrospective reminders.
    
    * CSS Tweaks.

--- webapp/src/components/assets/buttons.tsx
+++ webapp/src/components/assets/buttons.tsx
@@ -32,7 +32,7 @@ export const PrimaryButton = styled.button`
         opacity: 0;
     }
 
-    &:hover {
+    &:hover:enabled {
         &:before {
             opacity: 1;
         }
@@ -43,7 +43,8 @@ export const PrimaryButton = styled.button`
     }
 
     &:disabled {
-        background: rgba(var(--button-bg-rgb), 0.4);
+        color: rgba(var(--center-channel-color-rgb), 0.32);
+        background: rgba(var(--center-channel-color-rgb), 0.08);
     }
 
     i {
@@ -66,12 +67,16 @@ export const TertiaryButton = styled.button`
     padding: 0 20px;
     transition: all 0.15s ease-out;
 
-    &:hover {
+    &:hover:enabled {
         background: rgba(var(--button-bg-rgb), 0.08);
     }
 
     &:active  {
         background: rgba(var(--button-bg-rgb), 0.16);
+    }
+
+    &:disabled {
+        color: rgba(var(--center-channel-color-rgb), 0.32);
     }
 
     i {
