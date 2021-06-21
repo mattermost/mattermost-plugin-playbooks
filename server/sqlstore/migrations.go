7b6713fba6dd642c0a38bdcb9170b7442680934f
arvinDarmawan <arvin.darmawan@gmail.com>
2021-06-21 18:13:13 +0700

    fix: fix wrong merge conflict on migrations.go

--- server/sqlstore/migrations.go
+++ server/sqlstore/migrations.go
@@ -833,30 +833,6 @@ var migrations = []Migration{
 
 			return nil
 		},
-	}, {
-
-		fromVersion: semver.MustParse("0.20.0"),
-		toVersion:   semver.MustParse("0.21.0"),
-		migrationFunc: func(e sqlx.Ext, sqlStore *SQLStore) error {
-			if e.DriverName() == model.DATABASE_DRIVER_MYSQL {
-
-				if err := addColumnToMySQLTable(e, "IR_Playbook", "ExportChannelOnArchiveEnabled", "BOOLEAN NOT NULL DEFAULT FALSE"); err != nil {
-					return errors.Wrapf(err, "failed adding column ExportChannelOnArchiveEnabled to table IR_Playbook")
-				}
-				if err := addColumnToMySQLTable(e, "IR_Incident", "ExportChannelOnArchiveEnabled", "BOOLEAN NOT NULL DEFAULT FALSE"); err != nil {
-					return errors.Wrapf(err, "failed adding column ExportChannelOnArchiveEnabled to table IR_Incident")
-				}
-			} else {
-				if err := addColumnToPGTable(e, "IR_Playbook", "ExportChannelOnArchiveEnabled", "BOOLEAN NOT NULL DEFAULT FALSE"); err != nil {
-					return errors.Wrapf(err, "failed adding column ExportChannelOnArchiveEnabled to table IR_Playbook")
-				}
-				if err := addColumnToPGTable(e, "IR_Incident", "ExportChannelOnArchiveEnabled", "BOOLEAN NOT NULL DEFAULT FALSE"); err != nil {
-					return errors.Wrapf(err, "failed adding column ExportChannelOnArchiveEnabled to table IR_Incident")
-				}
-			}
-
-			return nil
-		},
 	},
 	{
 		fromVersion: semver.MustParse("0.20.0"),
@@ -949,6 +925,31 @@ var migrations = []Migration{
 
 				if _, err := sqlStore.execBuilder(e, incidentUpdate); err != nil {
 					return errors.Wrapf(err, "failed to update incident's LastStatusUpdateAt for id: %s", row.ID)
+				}
+			}
+
+			return nil
+		},
+	},
+	{
+
+		fromVersion: semver.MustParse("0.22.0"),
+		toVersion:   semver.MustParse("0.23.0"),
+		migrationFunc: func(e sqlx.Ext, sqlStore *SQLStore) error {
+			if e.DriverName() == model.DATABASE_DRIVER_MYSQL {
+
+				if err := addColumnToMySQLTable(e, "IR_Playbook", "ExportChannelOnArchiveEnabled", "BOOLEAN NOT NULL DEFAULT FALSE"); err != nil {
+					return errors.Wrapf(err, "failed adding column ExportChannelOnArchiveEnabled to table IR_Playbook")
+				}
+				if err := addColumnToMySQLTable(e, "IR_Incident", "ExportChannelOnArchiveEnabled", "BOOLEAN NOT NULL DEFAULT FALSE"); err != nil {
+					return errors.Wrapf(err, "failed adding column ExportChannelOnArchiveEnabled to table IR_Incident")
+				}
+			} else {
+				if err := addColumnToPGTable(e, "IR_Playbook", "ExportChannelOnArchiveEnabled", "BOOLEAN NOT NULL DEFAULT FALSE"); err != nil {
+					return errors.Wrapf(err, "failed adding column ExportChannelOnArchiveEnabled to table IR_Playbook")
+				}
+				if err := addColumnToPGTable(e, "IR_Incident", "ExportChannelOnArchiveEnabled", "BOOLEAN NOT NULL DEFAULT FALSE"); err != nil {
+					return errors.Wrapf(err, "failed adding column ExportChannelOnArchiveEnabled to table IR_Incident")
 				}
 			}
 
