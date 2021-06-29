fe2593905a0561bf04a2000a9f9cfcd287a90808
Christopher Poile <cpoile@gmail.com>
2021-06-29 14:51:06 -0400

    mouse cursor as pointer over clickable graphs

--- webapp/src/components/backstage/playbooks/bar_graph.tsx
+++ webapp/src/components/backstage/playbooks/bar_graph.tsx
@@ -85,6 +85,11 @@ const BarGraph = (props: BarGraphProps) => {
                         // eslint-disable-next-line no-underscore-dangle
                         props.onClick(element[0]._index);
                     },
+                    onHover(event: any) {
+                        if (props.onClick) {
+                            event.target.style.cursor = 'pointer';
+                        }
+                    },
                     maintainAspectRatio: false,
                     responsive: true,
                 }}
