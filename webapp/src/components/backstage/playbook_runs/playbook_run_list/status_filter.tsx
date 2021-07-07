c2a4c7a3eaa13136884bb7338220105a8fe9bd9f
Nika Begiashvili <nika.begiashvili@gmail.com>
2021-07-07 16:23:44 +0400

    Implement feature: add multiple status filter capabilities to backstage

--- webapp/src/components/backstage/playbook_runs/playbook_run_list/status_filter.tsx
+++ webapp/src/components/backstage/playbook_runs/playbook_run_list/status_filter.tsx
@@ -3,14 +3,12 @@
 
 import React, {useState} from 'react';
 
-import ReactSelect, {StylesConfig} from 'react-select';
-import styled from 'styled-components';
-
+import {MultiSelect,SelectOption} from '../../../multi_select';
 import './status_filter.scss';
 
 interface Props {
-    default: string | undefined;
-    onChange: (newStatus: string) => void;
+    default: string[] | undefined;
+    onChange: (statuses: string[]) => void;
     options: StatusOption[];
 }
 
@@ -20,118 +18,53 @@ export interface StatusOption {
 }
 
 export function StatusFilter(props: Props) {
-    const [isOpen, setOpen] = useState(false);
-    const toggleOpen = () => {
-        setOpen(!isOpen);
-    };
+    
+    const opts = props.options.map( opt => ({
+        display: opt.label,
+        value: opt.value,
+        selected: props.default?.includes(opt.value) || false,
+        disabled: false
+    }));
 
-    const getDefault = () => {
-        const defaultOption = props.options.find((val) => val.value === props.default);
-        if (defaultOption) {
-            return defaultOption;
+    const [options, setOptions] = useState<SelectOption[]>(opts);
+
+    const onSelectedChange = async (newOptions:SelectOption[],lastAction:SelectOption) => {
+        if(lastAction.value == ''){
+            newOptions = newOptions.map( opt => ({
+                ...opt,
+                selected: lastAction.selected
+            }));
         }
-        return props.options[0];
-    };
 
-    const [selected, setSelected] = useState(getDefault());
+        const selectCnt = newOptions
+            .filter( opt => opt.value != '' )
+            .reduce( (acm,opt) => (acm + (opt.selected ? 1: 0)), 0 );
+        const allCheckbox = newOptions.filter( opt => opt.value == '' )[0];
 
-    const onSelectedChange = async (val: StatusOption) => {
-        toggleOpen();
-        if (val !== selected) {
-            props.onChange(val.value);
-            setSelected(val);
+        allCheckbox.selected = selectCnt == newOptions.length-1;
+
+        setOptions(newOptions);
+
+        if( allCheckbox.selected ){ // everything is selected shouldn't filter using status at all
+            props.onChange([]);    
+        }else if( selectCnt > 0 ){
+            props.onChange(
+                newOptions.filter( opt => opt.selected && opt.value != '' ).map( opt => opt.value )
+            );
+        }else{
+            props.onChange(['_']); // no status is selected, should return empty list
         }
     };
 
     return (
-        <Dropdown
-            isOpen={isOpen}
-            onClose={toggleOpen}
+        <MultiSelect
             target={
-                <button
-                    onClick={toggleOpen}
-                    className='PlaybookRunFilter-button'
-                >
-                    {selected.value === '' ? 'Status' : selected.label}
+                <button className='PlaybookRunFilter-button'>Status
                     {<i className='icon-chevron-down icon--small ml-2'/>}
                 </button>
             }
-        >
-            <ReactSelect
-                autoFocus={true}
-                backspaceRemovesValue={false}
-                components={{DropdownIndicator: null, IndicatorSeparator: null}}
-                controlShouldRenderValue={false}
-                hideSelectedOptions={false}
-                isClearable={false}
-                isSearchable={false}
-                menuIsOpen={true}
-                options={props.options}
-                styles={selectStyles}
-                tabSelectsValue={false}
-                onChange={(option) => onSelectedChange(option as StatusOption)}
-                classNamePrefix='status-filter-select'
-                className='status-filter-select'
-            />
-        </Dropdown>
+            options={options}
+            onChange={onSelectedChange}
+        />
     );
 }
-
-// styles for the select component
-const selectStyles: StylesConfig = {
-    control: (provided) => ({...provided, height: 0, minHeight: 0}),
-    menu: () => ({boxShadow: 'none'}),
-    option: (provided, state) => {
-        const hoverColor = 'rgba(20, 93, 191, 0.08)';
-        const bgHover = state.isFocused ? hoverColor : 'transparent';
-        return {
-            ...provided,
-            backgroundColor: state.isSelected ? hoverColor : bgHover,
-            color: 'unset',
-        };
-    },
-};
-
-// styled components
-interface DropdownProps {
-    children: JSX.Element;
-    isOpen: boolean;
-    target: JSX.Element;
-    onClose: () => void;
-}
-
-const DropdownContainer = styled.div`
-    position: relative;
-`;
-
-const Dropdown = ({children, isOpen, target, onClose}: DropdownProps) => (
-    <DropdownContainer
-        className={`PlaybookRunFilter status-filter-dropdown${isOpen ? ' PlaybookRunFilter--active status-filter-dropdown--active' : ''}`}
-    >
-        {target}
-        {
-            isOpen &&
-            <>
-                <Menu className='PlaybookRunFilter-select status-filter-select__container'>
-                    {children}
-                </Menu>
-                <Blanket onClick={onClose}/>
-            </>
-        }
-    </DropdownContainer>
-);
-
-const Menu = (props: Record<string, any>) => {
-    return (
-        <div {...props}/>
-    );
-};
-
-const Blanket = styled.div`
-    bottom: 0,
-    left: 0,
-    top: 0,
-    right: 0,
-    position: 'fixed',
-    zIndex: 1,
-`;
