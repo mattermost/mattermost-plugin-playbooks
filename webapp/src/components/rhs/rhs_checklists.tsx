f29a70552339891779c7d8fbcc23b506a40e924a
Christopher Poile <cpoile@gmail.com>
2021-07-10 20:01:47 -0400

    remove unneeded fragment

--- webapp/src/components/rhs/rhs_checklists.tsx
+++ webapp/src/components/rhs/rhs_checklists.tsx
@@ -35,86 +35,85 @@ const RHSChecklists = (props: Props) => {
         <InnerContainer>
             <MainTitle>{'Checklists'}</MainTitle>
             {checklists.map((checklist: Checklist, checklistIndex: number) => (
-                <>
-                    <CollapsibleChecklist
-                        title={checklist.title}
-                        items={checklist.items}
-                        index={checklistIndex}
-                    >
-                        <ChecklistContainer className='checklist'>
-                            <DragDropContext
-                                onDragEnd={(result: DropResult) => {
-                                    if (!result.destination) {
-                                        return;
-                                    }
+                <CollapsibleChecklist
+                    key={checklist.title + checklistIndex}
+                    title={checklist.title}
+                    items={checklist.items}
+                    index={checklistIndex}
+                >
+                    <ChecklistContainer className='checklist'>
+                        <DragDropContext
+                            onDragEnd={(result: DropResult) => {
+                                if (!result.destination) {
+                                    return;
+                                }
 
-                                    if (result.destination.droppableId === result.source.droppableId &&
-                                        result.destination.index === result.source.index) {
-                                        return;
-                                    }
+                                if (result.destination.droppableId === result.source.droppableId &&
+                                    result.destination.index === result.source.index) {
+                                    return;
+                                }
 
-                                    const newChecklists = Array.from(checklists);
-                                    const newChecklistItems = Array.from(checklists[checklistIndex].items);
-                                    const [removed] = newChecklistItems.splice(result.source.index, 1);
-                                    newChecklistItems.splice(result.destination.index, 0, removed);
-                                    newChecklists[checklistIndex] = {
-                                        ...newChecklists[checklistIndex],
-                                        items: newChecklistItems,
-                                    };
+                                const newChecklists = Array.from(checklists);
+                                const newChecklistItems = Array.from(checklists[checklistIndex].items);
+                                const [removed] = newChecklistItems.splice(result.source.index, 1);
+                                newChecklistItems.splice(result.destination.index, 0, removed);
+                                newChecklists[checklistIndex] = {
+                                    ...newChecklists[checklistIndex],
+                                    items: newChecklistItems,
+                                };
 
-                                    dispatch(playbookRunUpdated({
-                                        ...props.playbookRun,
-                                        checklists: newChecklists,
-                                    }));
+                                dispatch(playbookRunUpdated({
+                                    ...props.playbookRun,
+                                    checklists: newChecklists,
+                                }));
 
-                                    clientReorderChecklist(props.playbookRun.id, checklistIndex, result.source.index, result.destination.index);
-                                }}
+                                clientReorderChecklist(props.playbookRun.id, checklistIndex, result.source.index, result.destination.index);
+                            }}
+                        >
+                            <Droppable
+                                droppableId='columns'
+                                direction='vertical'
+                                type='checklist'
                             >
-                                <Droppable
-                                    droppableId='columns'
-                                    direction='vertical'
-                                    type='checklist'
-                                >
-                                    {(droppableProvided: DroppableProvided) => (
-                                        <div
-                                            ref={droppableProvided.innerRef}
-                                            {...droppableProvided.droppableProps}
-                                        >
-                                            {checklist.items.map((checklistItem: ChecklistItem, index: number) => (
-                                                <Draggable
-                                                    key={checklistItem.title + index}
-                                                    draggableId={checklistItem.title + index}
-                                                    index={index}
-                                                >
-                                                    {(draggableProvided: DraggableProvided, snapshot: DraggableStateSnapshot) => (
-                                                        <ChecklistItemDetails
-                                                            checklistItem={checklistItem}
-                                                            checklistNum={checklistIndex}
-                                                            itemNum={index}
-                                                            channelId={props.playbookRun.channel_id}
-                                                            playbookRunId={props.playbookRun.id}
-                                                            onChange={(newState: ChecklistItemState) => {
-                                                                setChecklistItemState(props.playbookRun.id, checklistIndex, index, newState);
-                                                            }}
-                                                            onRedirect={() => {
-                                                                if (isMobile()) {
-                                                                    dispatch(toggleRHS());
-                                                                }
-                                                            }}
-                                                            draggableProvided={draggableProvided}
-                                                            dragging={snapshot.isDragging}
-                                                        />
-                                                    )}
-                                                </Draggable>
-                                            ))}
-                                            {droppableProvided.placeholder}
-                                        </div>
-                                    )}
-                                </Droppable>
-                            </DragDropContext>
-                        </ChecklistContainer>
-                    </CollapsibleChecklist>
-                </>
+                                {(droppableProvided: DroppableProvided) => (
+                                    <div
+                                        ref={droppableProvided.innerRef}
+                                        {...droppableProvided.droppableProps}
+                                    >
+                                        {checklist.items.map((checklistItem: ChecklistItem, index: number) => (
+                                            <Draggable
+                                                key={checklistItem.title + index}
+                                                draggableId={checklistItem.title + index}
+                                                index={index}
+                                            >
+                                                {(draggableProvided: DraggableProvided, snapshot: DraggableStateSnapshot) => (
+                                                    <ChecklistItemDetails
+                                                        checklistItem={checklistItem}
+                                                        checklistNum={checklistIndex}
+                                                        itemNum={index}
+                                                        channelId={props.playbookRun.channel_id}
+                                                        playbookRunId={props.playbookRun.id}
+                                                        onChange={(newState: ChecklistItemState) => {
+                                                            setChecklistItemState(props.playbookRun.id, checklistIndex, index, newState);
+                                                        }}
+                                                        onRedirect={() => {
+                                                            if (isMobile()) {
+                                                                dispatch(toggleRHS());
+                                                            }
+                                                        }}
+                                                        draggableProvided={draggableProvided}
+                                                        dragging={snapshot.isDragging}
+                                                    />
+                                                )}
+                                            </Draggable>
+                                        ))}
+                                        {droppableProvided.placeholder}
+                                    </div>
+                                )}
+                            </Droppable>
+                        </DragDropContext>
+                    </ChecklistContainer>
+                </CollapsibleChecklist>
             ))}
         </InnerContainer>
     );
