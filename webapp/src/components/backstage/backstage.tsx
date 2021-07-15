c9c3ed95595ccf2301ec8603336c107681d33460
Jesse Hallam <jesse.hallam@gmail.com>
2021-07-15 15:09:31 -0300

    make playbook navbar sticky

--- webapp/src/components/backstage/backstage.tsx
+++ webapp/src/components/backstage/backstage.tsx
@@ -34,6 +34,7 @@ import CloudModal from 'src/components/cloud_modal';
 
 import StatsView from './stats';
 import SettingsView from './settings';
+import {BackstageNavbar, BackstageNavbarIcon} from './backstage_navbar';
 
 const BackstageContainer = styled.div`
     background: var(--center-channel-bg);
@@ -41,45 +42,6 @@ const BackstageContainer = styled.div`
     display: flex;
     flex-direction: column;
     overflow-y: auto;
-`;
-
-export const BackstageNavbarIcon = styled.button`
-    border: none;
-    outline: none;
-    background: transparent;
-    border-radius: 4px;
-    font-size: 24px;
-    padding: 0;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    cursor: pointer;
-    color: var(--center-channel-color-56);
-
-    &:hover {
-        background: var(--button-bg-08);
-        text-decoration: unset;
-        color: var(--button-bg);
-    }
-`;
-
-export const BackstageNavbar = styled.div`
-    position: sticky;
-    width: 100%;
-    top: 0;
-    z-index: 2;
-
-    display: flex;
-    align-items: center;
-    padding: 28px 31px;
-    background: var(--center-channel-bg);
-    color: var(--center-channel-color);
-    font-family: 'compass-icons';
-    box-shadow: inset 0px -1px 0px var(--center-channel-color-16);
-
-    font-family: 'Open Sans';
-    font-style: normal;
-    font-weight: 600;
 `;
 
 const BackstageTitlebarItem = styled(NavLink)`
