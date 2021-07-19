d82d49021715f7ac920c34ac7fb4b6ceaff6ba2d
Alejandro García Montoro <alejandro.garciamontoro@gmail.com>
2021-07-19 16:42:05 +0200

    Update RHS title

--- webapp/src/components/rhs/rhs_title.tsx
+++ webapp/src/components/rhs/rhs_title.tsx
@@ -4,22 +4,82 @@
 import React, {FC} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import styled from 'styled-components';
+import {Tooltip, OverlayTrigger} from 'react-bootstrap';
 
 import {GlobalState} from 'mattermost-redux/types/store';
+import {getCurrentTeam} from 'mattermost-redux/selectors/entities/teams';
 
-import {PlaybookRun, playbookRunCurrentStatus} from 'src/types/playbook_run';
-
-import StatusBadge from 'src/components/backstage/playbook_runs/status_badge';
+import {PlaybookRun} from 'src/types/playbook_run';
 
 import LeftChevron from 'src/components/assets/icons/left_chevron';
+import ExternalLink from 'src/components/assets/icons/external_link';
 import {RHSState} from 'src/types/rhs';
 import {setRHSViewingList} from 'src/actions';
 import {currentPlaybookRun, currentRHSState} from 'src/selectors';
+import {ButtonIcon} from 'src/components/assets/buttons';
+
+import {navigateToUrl} from 'src/browser_routing';
+
+import {pluginId} from 'src/manifest';
+
+import {OVERLAY_DELAY} from 'src/constants';
+
+const RHSTitle = () => {
+    const dispatch = useDispatch();
+    const playbookRun = useSelector<GlobalState, PlaybookRun | undefined>(currentPlaybookRun);
+    const rhsState = useSelector<GlobalState, RHSState>(currentRHSState);
+
+    if (rhsState === RHSState.ViewingPlaybookRun) {
+        return (
+            <RHSTitleContainer>
+                <Button
+                    onClick={() => dispatch(setRHSViewingList())}
+                    data-testid='back-button'
+                >
+                    <LeftChevron/>
+                </Button>
+                <RHSTitleText data-testid='rhs-title'>{'Run details' || 'Runs'}</RHSTitleText>
+                {playbookRun && <ExternalLinkButton playbookRunID={playbookRun?.id || ''}/> }
+            </RHSTitleContainer>
+        );
+    }
+
+    return (
+        <RHSTitleText>
+            {'Runs in progress'}
+        </RHSTitleText>
+    );
+};
+
+const ExternalLinkButton = ({playbookRunID} : {playbookRunID: string}) => {
+    const currentTeam = useSelector(getCurrentTeam);
+
+    const tooltip = (
+        <Tooltip>
+            {'View run overview'}
+        </Tooltip>
+    );
+
+    return (
+        <OverlayTrigger
+            placement={'top'}
+            delay={OVERLAY_DELAY}
+            overlay={tooltip}
+        >
+            <StyledButtonIcon
+                onClick={() => navigateToUrl(`/${currentTeam.name}/${pluginId}/runs/${playbookRunID}`)}
+            >
+                <ExternalLink/>
+            </StyledButtonIcon>
+        </OverlayTrigger>
+    );
+};
 
 const RHSTitleContainer = styled.div`
     display: flex;
     justify-content: space-between;
-    overflow: hidden;
+    align-items: center;
+    overflow: visible;
 `;
 
 const RHSTitleText = styled.div`
@@ -36,35 +96,9 @@ const Button = styled.button`
     align-items: center;
 `;
 
-const RHSTitle = () => {
-    const dispatch = useDispatch();
-    const playbookRun = useSelector<GlobalState, PlaybookRun | undefined>(currentPlaybookRun);
-    const rhsState = useSelector<GlobalState, RHSState>(currentRHSState);
-
-    if (rhsState === RHSState.ViewingPlaybookRun) {
-        return (
-            <RHSTitleContainer>
-                <Button
-                    onClick={() => dispatch(setRHSViewingList())}
-                    data-testid='back-button'
-                >
-                    <LeftChevron/>
-                </Button><RHSTitleText data-testid='rhs-title'>{playbookRun?.name || 'Runs'}</RHSTitleText>
-                {playbookRun && (
-                    <StatusBadge
-                        status={playbookRunCurrentStatus(playbookRun)}
-                        compact={true}
-                    />
-                )}
-            </RHSTitleContainer>
-        );
-    }
-
-    return (
-        <RHSTitleText>
-            {'Runs in progress'}
-        </RHSTitleText>
-    );
-};
+const StyledButtonIcon = styled(ButtonIcon)`
+    width: 24px;
+    height: 24px;
+`;
 
 export default RHSTitle;
