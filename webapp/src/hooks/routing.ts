0052807df9d835d91728943052dfad9a44d5d6b4
Caleb Roseland <caleb@calebroseland.com>
2021-07-22 15:59:45 -0500

    routing hook, add comment-docs

--- webapp/src/hooks/routing.ts
+++ webapp/src/hooks/routing.ts
@@ -8,12 +8,35 @@ import {TEMPLATE_TITLE_KEY} from 'src/constants';
 import {Playbook} from 'src/types/playbook';
 import {tabInfo} from 'src/components/backstage/playbook_edit';
 
-export function usePlaybooksRouting<TArg extends Playbook | Playbook['id']>(
+type PlaybooksRoutingOptions<T> = {
+    urlOnly?: boolean,
+    onGo?: (arg: T) => void
+}
+
+function id(p: Playbook | Playbook['id']) {
+    return p && typeof p !== 'string' ? p.id : p;
+}
+
+/**
+ * Access backstage routing functions for a given team.
+ * @typeParam T - Type of routing function parameter and argument in callback. Must be {@link Playbook} or {@link Playbook.id}.
+ * @param teamName - Which team-backstage to go to. (see {@link navigateToTeamPluginUrl})
+ * @param options - {@link PlaybooksRoutingOptions} alters behavior of hook
+ *
+ * @example Get URL to go view the given playbook
+ * const {view} = usePlaybooksRouting(currentTeam.name, {urlOnly: true});
+ * const url = view(id);
+ *
+ * @example Navigate to the backstage edit page for the given playbook
+ * const playbookRouting = usePlaybooksRouting(currentTeam.name);
+ * playbookRouting.edit(id);
+ */
+export function usePlaybooksRouting<TParam extends Playbook | Playbook['id']>(
     teamName: Team['name'],
-    {urlOnly, onGo}: {urlOnly?: boolean, onGo?: (arg: TArg) => void} = {},
+    {urlOnly, onGo}: PlaybooksRoutingOptions<TParam> = {},
 ) {
     return useMemo(() => {
-        function go(path: string, p?: TArg) {
+        function go(path: string, p?: TParam) {
             if (!urlOnly) {
                 if (p) {
                     onGo?.(p);
@@ -23,15 +46,11 @@ export function usePlaybooksRouting<TArg extends Playbook | Playbook['id']>(
             return teamPluginUrl(teamName, path);
         }
 
-        function id(p: Playbook | Playbook['id']) {
-            return p && typeof p !== 'string' ? p.id : p;
-        }
-
         return {
-            edit: (p: TArg, tabId?: typeof tabInfo[number]['id']) => {
+            edit: (p: TParam, tabId?: typeof tabInfo[number]['id']) => {
                 return go(`/playbooks/${id(p)}/edit${tabId ? `/${tabId}` : ''}`, p);
             },
-            view: (p: TArg) => {
+            view: (p: TParam) => {
                 return go(`/playbooks/${id(p)}`, p);
             },
             create: (templateTitle?: string) => {
