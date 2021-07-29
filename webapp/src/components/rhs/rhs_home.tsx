20ae47aeed1c0807a646a6d3455ad6cdfe51d8f5
Caleb Roseland <caleb@calebroseland.com>
2021-07-29 09:58:23 -0500

    mask svg const

--- webapp/src/components/rhs/rhs_home.tsx
+++ webapp/src/components/rhs/rhs_home.tsx
@@ -81,6 +81,7 @@ const WelcomeWarn = styled(WelcomeDesc)`
     color: rgba(var(--error-text-color-rgb), 0.72);
 `;
 
+const RunDetailMaskSvg = 'data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="calc(100% - 15px)" viewBox="0 0 400 137" preserveAspectRatio="none"><path d="M0 0H400V122.629C400 122.629 312 137 200 137C101.5 137 0 122.629 0 122.629V0Z"/></svg>';
 type RunDetailProps = {exists: boolean;};
 
 const RunDetail = styled.div<RunDetailProps>`
@@ -95,7 +96,7 @@ const RunDetail = styled.div<RunDetailProps>`
             rgba(var(--center-channel-bg-rgb), 0.25) 100%
         ),
         rgba(var(${({exists}) => (exists ? '--button-bg-rgb' : '--center-channel-color-rgb')}), 0.08);
-    mask-image: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="calc(100% - 15px)" viewBox="0 0 400 137" preserveAspectRatio="none"><path d="M0 0H400V122.629C400 122.629 312 137 200 137C101.5 137 0 122.629 0 122.629V0Z"/></svg>');
+    mask-image: url('${RunDetailMaskSvg}');
 
     > div {
         margin-left: 2rem;
