6e8fb46e24524185abf1c140cf79bce50a15aed1
Alejandro García Montoro <alejandro.garciamontoro@gmail.com>
2021-08-04 09:58:17 +0200

    Fix the newline render fix

--- webapp/src/components/rhs/rhs_about_description.tsx
+++ webapp/src/components/rhs/rhs_about_description.tsx
@@ -107,7 +107,9 @@ const RenderedDescription = styled.div`
         cursor: text;
     }
 
-    white-space: pre-wrap;
+    p {
+        white-space: pre-wrap;
+    }
 `;
 
 export default RHSAboutDescription;
