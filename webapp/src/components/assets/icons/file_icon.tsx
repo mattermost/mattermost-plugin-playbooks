13e3cc6bdcdb8e42706e3499adf86fad2374e62f
Jesse Hallam <jesse.hallam@gmail.com>
2021-08-17 11:57:19 -0300

    MM-37890: fix missing file icon (#693)

--- webapp/src/components/assets/icons/file_icon.tsx
+++ webapp/src/components/assets/icons/file_icon.tsx
@@ -4,7 +4,7 @@
 import React from 'react';
 
 const FileIcon = (props: React.PropsWithoutRef<JSX.IntrinsicElements['i']>): JSX.Element => (
-    <i className={`icon icon-file-outline icon-32 ${props.className}`}/>
+    <i className={`icon icon-file-generic-outline icon-32 ${props.className}`}/>
 );
 
 export default FileIcon;
