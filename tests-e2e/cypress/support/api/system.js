910b6d773afd6fe8764dbd64419349feee00e8ce
Christopher Poile <cpoile@gmail.com>
2021-08-30 15:38:32 -0400

    experiment to fix broken e2e tests 2

--- tests-e2e/cypress/support/api/system.js
+++ tests-e2e/cypress/support/api/system.js
@@ -152,7 +152,7 @@ Cypress.Commands.add('apiUpdateConfig', (newConfig = {}) => {
         // * Check if config can be updated
         expectConfigToBeUpdatable(currentConfig, newConfig);
 
-        const config = merge.all([getDefaultConfig(), currentConfig, newConfig]);
+        const config = merge.all([currentConfig, getDefaultConfig(), newConfig]);
 
         // # Set the modified config
         return cy.request({
