42425e90ec885e9debac5c7292a6a4054e2b5519
Christopher Poile <cpoile@gmail.com>
2021-09-10 04:38:58 -0400

    automation -> actions (#732)

--- server/app/playbook_service.go
+++ server/app/playbook_service.go
@@ -192,7 +192,7 @@ func (s *playbookService) getPlaybookSuggestionsSlackAttachment(playbooks []*Cac
 			Style: "primary",
 		}
 
-		url := fmt.Sprintf("%s/%s/edit/automation", playbooksURL, playbooks[0].ID) // automation tab
+		url := fmt.Sprintf("%s/%s/edit/actions", playbooksURL, playbooks[0].ID) // actions tab
 
 		attachment := &model.SlackAttachment{
 			Actions: []*model.PostAction{yesButton, ignoreButton},
