942fdb90b135ae3f6d9331bcf1a16afbed9538db
Christopher Speller <crspeller@gmail.com>
2021-09-15 12:03:40 -0700

    Add migration for kv store.

--- server/sqlstore/migrations.go
+++ server/sqlstore/migrations.go
@@ -1219,4 +1219,15 @@ var migrations = []Migration{
 			return nil
 		},
 	},
+	{
+		fromVersion: semver.MustParse("0.30.0"),
+		toVersion:   semver.MustParse("0.31.0"),
+		migrationFunc: func(e sqlx.Ext, sqlStore *SQLStore) error {
+			if _, err := e.Exec("UPDATE PluginKeyValueStore SET pluginid='playbooks' WHERE pluginid='com.mattermost.plugin-incident-management'"); err != nil {
+				return errors.Wrapf(err, "failed setting default value in column CategoryName of table IR_Playbook")
+			}
+
+			return nil
+		},
+	},
 }
