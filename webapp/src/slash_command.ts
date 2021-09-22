85f2265ef5c348e93766d0278c8c4d3a67953896
Caleb Roseland <caleb@calebroseland.com>
2021-09-22 12:30:52 -0500

    fix test

--- webapp/src/slash_command.ts
+++ webapp/src/slash_command.ts
@@ -27,7 +27,6 @@ type SlashCommandObj = {message?: string; args?: string[];} | {error: string;} |
 export function makeSlashCommandHook(store: Store) {
     return async (inMessage: any, args: any): Promise<SlashCommandObj> => {
         const state = store.getState();
-        const currentRun = currentPlaybookRun(state);
         const message = inMessage && typeof inMessage === 'string' ? inMessage.trim() : null;
 
         if (message?.startsWith('/playbook run')) {
@@ -37,11 +36,14 @@ export function makeSlashCommandHook(store: Store) {
             return {message: `/playbook run ${clientId}`, args};
         }
 
-        if (message?.startsWith('/playbook update') && inPlaybookRunChannel(state) && currentRun) {
-            const clientId = generateId();
-            store.dispatch(setClientId(clientId));
-            store.dispatch(promptUpdateStatus(currentRun.team_id, currentRun.id, currentRun.playbook_id, currentRun.channel_id));
-            return {};
+        if (message?.startsWith('/playbook update') && inPlaybookRunChannel(state)) {
+            const currentRun = currentPlaybookRun(state);
+            if (currentRun) {
+                const clientId = generateId();
+                store.dispatch(setClientId(clientId));
+                store.dispatch(promptUpdateStatus(currentRun.team_id, currentRun.id, currentRun.playbook_id, currentRun.channel_id));
+                return {};
+            }
         }
 
         if (message?.startsWith('/playbook info')) {
