c2183d4d7a2a2066b667ec409c66f7e88479503e
Christopher Poile <cpoile@gmail.com>
2021-10-10 13:55:28 -0400

    remove apply theme

--- webapp/src/components/backstage/backstage.tsx
+++ webapp/src/components/backstage/backstage.tsx
@@ -1,10 +1,9 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import React, {useEffect} from 'react';
+import React from 'react';
 import {Switch, Route, NavLink, useRouteMatch, Redirect} from 'react-router-dom';
 import {useSelector} from 'react-redux';
-
 import styled from 'styled-components';
 import Icon from '@mdi/react';
 import {mdiThumbsUpDown, mdiClipboardPlayMultipleOutline} from '@mdi/js';
@@ -12,32 +11,23 @@ import {mdiThumbsUpDown, mdiClipboardPlayMultipleOutline} from '@mdi/js';
 import {GlobalState} from 'mattermost-redux/types/store';
 import {getMyTeams} from 'mattermost-redux/selectors/entities/teams';
 import {Team} from 'mattermost-redux/types/teams';
-import {Theme} from 'mattermost-redux/types/preferences';
-import {getTheme} from 'mattermost-redux/selectors/entities/preferences';
 
 import {promptForFeedback} from 'src/client';
-
 import PlaybookRunBackstage
     from 'src/components/backstage/playbook_runs/playbook_run_backstage/playbook_run_backstage';
-
 import PlaybookList from 'src/components/backstage/playbook_list';
 import PlaybookEdit from 'src/components/backstage/playbook_edit';
 import {NewPlaybook} from 'src/components/backstage/new_playbook';
 import {ErrorPageTypes} from 'src/constants';
 import {pluginErrorUrl} from 'src/browser_routing';
 import PlaybookIcon from 'src/components/assets/icons/playbook_icon';
-
 import PlaybookBackstage from 'src/components/backstage/playbooks/playbook_backstage';
 import {useForceDocumentTitle} from 'src/hooks';
 import CloudModal from 'src/components/cloud_modal';
-
-import ErrorPage from '../error_page';
-
-import SettingsView from './settings';
-import {BackstageNavbar} from './backstage_navbar';
-
-import {applyTheme} from './css_utils';
-import RunsPage from './runs_page';
+import ErrorPage from 'src/components/error_page';
+import SettingsView from 'src/components/backstage/settings';
+import {BackstageNavbar} from 'src/components/backstage/backstage_navbar';
+import RunsPage from 'src/components/backstage/runs_page';
 
 const BackstageContainer = styled.div`
     background: var(--center-channel-bg);
@@ -85,24 +75,9 @@ const BackstageBody = styled.div`
 const Backstage = () => {
     //@ts-ignore plugins state is a thing
     const npsAvailable = useSelector<GlobalState, boolean>((state) => Boolean(state.plugins?.plugins?.['com.mattermost.nps']));
-    useEffect(() => {
-        // This class, critical for all the styling to work, is added by ChannelController,
-        // which is not loaded when rendering this root component.
-        document.body.classList.add('app__body');
-        const root = document.getElementById('root');
-        if (root) {
-            root.className += ' channel-view';
-        }
-
-        applyTheme(currentTheme);
-        return function cleanUp() {
-            document.body.classList.remove('app__body');
-        };
-    }, []);
 
     useForceDocumentTitle('Playbooks');
 
-    const currentTheme = useSelector<GlobalState, Theme>(getTheme);
     const teams = useSelector<GlobalState, Team[]>(getMyTeams);
 
     const match = useRouteMatch();
