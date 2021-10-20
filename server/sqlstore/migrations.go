1e2af54fa92a7bb9bd52736dcd983e0f1c0cb309
Jesse Hallam <jesse.hallam@gmail.com>
2021-10-20 14:55:53 -0300

    Test mariadb (#822)
    * test MariaDB
    
    * improve MariaDB compatibility
    
    Drop redundant indices, and fix a MySQL/MariaDB compatibility issue.
    
    * simplify migrations
    
    * store_test_results for Go unit tests
    
    * use forked go-junit-report
    
    * make test-ci, using json
    
    * switch to gotestsum
    
    * gotestsum, faster!
    
    * anchor to GOBIN
    
    * cleanup
    
    * fix --junitfile invocation
    
    * tweak table order when nuking
    
    * make linter happy

--- server/sqlstore/migrations.go
+++ server/sqlstore/migrations.go
@@ -1192,10 +1192,16 @@ var migrations = []Migration{
 				if err := addPrimaryKey(e, sqlStore, "IR_PlaybookMember", "(MemberID, PlaybookID)"); err != nil {
 					return err
 				}
+				if err := dropIndexIfExists(e, sqlStore, "IR_StatusPosts", "posts_unique"); err != nil {
+					return err
+				}
 				if err := addPrimaryKey(e, sqlStore, "IR_StatusPosts", "(IncidentID, PostID)"); err != nil {
 					return err
 				}
 				if err := addPrimaryKey(e, sqlStore, "IR_TimelineEvent", "(ID)"); err != nil {
+					return err
+				}
+				if err := dropIndexIfExists(e, sqlStore, "IR_ViewedChannel", "IR_ViewedChannel_ChannelID_UserID"); err != nil {
 					return err
 				}
 				if err := addPrimaryKey(e, sqlStore, "IR_ViewedChannel", "(ChannelID, UserID)"); err != nil {
@@ -1337,6 +1343,17 @@ var migrations = []Migration{
 				}
 			}
 			return nil
+		},
+	},
+	{
+		fromVersion: semver.MustParse("0.35.0"),
+		toVersion:   semver.MustParse("0.36.0"),
+		migrationFunc: func(e sqlx.Ext, sqlStore *SQLStore) error {
+			if err := dropIndexIfExists(e, sqlStore, "IR_StatusPosts", "posts_unique"); err != nil {
+				return err
+			}
+
+			return dropIndexIfExists(e, sqlStore, "IR_ViewedChannel", "IR_ViewedChannel_ChannelID_UserID")
 		},
 	},
 }
