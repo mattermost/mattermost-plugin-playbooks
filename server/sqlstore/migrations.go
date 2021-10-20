e0ed1f7097006f966d946fdcae74764d9ce1e5d0
iomodo <wineson@gmail.com>
2021-10-20 21:05:29 +0400

    Fix merge

--- server/sqlstore/migrations.go
+++ server/sqlstore/migrations.go
@@ -1385,8 +1385,8 @@ var migrations = []Migration{
 		},
 	},
 	{
-		fromVersion: semver.MustParse("0.35.0"),
-		toVersion:   semver.MustParse("0.36.0"),
+		fromVersion: semver.MustParse("0.36.0"),
+		toVersion:   semver.MustParse("0.37.0"),
 		migrationFunc: func(e sqlx.Ext, sqlStore *SQLStore) error {
 			if e.DriverName() == model.DatabaseDriverMysql {
 				if err := addColumnToMySQLTable(e, "IR_Playbook", "ConcatenatedFollowerIDs", "TEXT"); err != nil {
