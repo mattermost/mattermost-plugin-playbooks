77dfdd1b1315fa0ee7f9c5870daac792429dca1b
clovis <clovis-@live.com>
2021-10-31 23:52:18 -0400

    Add useRelative with playbooks

--- webapp/src/components/rhs/post_card.tsx
+++ webapp/src/components/rhs/post_card.tsx
@@ -124,6 +124,7 @@ const PostCard = (props: Props) => {
                             timestamp: (
                                 <Timestamp
                                     value={props.post.create_at}
+                                    useRelative
                                 />
                             ),
                         })
