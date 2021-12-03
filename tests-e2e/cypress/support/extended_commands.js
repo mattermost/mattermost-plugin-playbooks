b77372691ad64e374ee0c6db9d5929e57ee3e8c2
Cass Caplinger <cass.caplinger@mattermost.com>
2021-12-03 14:32:06 -0500

    e2e: reduce extra cy.visit and reload waits

--- tests-e2e/cypress/support/extended_commands.js
+++ tests-e2e/cypress/support/extended_commands.js
@@ -3,12 +3,12 @@
 
 import * as TIMEOUTS from '../fixtures/timeouts';
 
-Cypress.Commands.overwrite('reload', (originalFn, forceReload, options, duration = TIMEOUTS.THREE_SEC) => {
+Cypress.Commands.overwrite('reload', (originalFn, forceReload, options, duration = TIMEOUTS.ONE_SEC) => {
     originalFn(forceReload, options);
     cy.wait(duration);
 });
 
-Cypress.Commands.overwrite('visit', (originalFn, url, options, duration = TIMEOUTS.THREE_SEC) => {
+Cypress.Commands.overwrite('visit', (originalFn, url, options, duration = TIMEOUTS.ONE_SEC) => {
     originalFn(url, options);
     cy.wait(duration);
 });
