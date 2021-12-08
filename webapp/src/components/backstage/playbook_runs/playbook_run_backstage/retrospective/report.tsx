462b7a0f7c207cb88673b041d1f4f256ccd93b71
Christopher Poile <cpoile@gmail.com>
2021-12-08 17:32:31 -0500

    refactor post & textArea into one component; no need for guard

--- webapp/src/components/backstage/playbook_runs/playbook_run_backstage/retrospective/report.tsx
+++ webapp/src/components/backstage/playbook_runs/playbook_run_backstage/retrospective/report.tsx
@@ -1,29 +1,18 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import React, {useRef, useState} from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components';
 import {useIntl} from 'react-intl';
 import debounce from 'debounce';
-
-import {useSelector} from 'react-redux';
-import {Team} from 'mattermost-redux/types/teams';
-import {getTeam} from 'mattermost-redux/selectors/entities/teams';
-import {GlobalState} from 'mattermost-redux/types/store';
 
 import {PlaybookRun} from 'src/types/playbook_run';
 import {Title} from 'src/components/backstage/playbook_runs/shared';
 import {publishRetrospective, updateRetrospective} from 'src/client';
 import {PrimaryButton} from 'src/components/assets/buttons';
-import {useClickOutsideRef} from 'src/hooks';
 import ReportTextArea
     from 'src/components/backstage/playbook_runs/playbook_run_backstage/retrospective/report_text_area';
-import PostText from 'src/components/post_text';
-import RouteLeavingGuard from 'src/components/backstage/route_leaving_guard';
 import ConfirmModal from 'src/components/widgets/confirmation_modal';
-
-// @ts-ignore
-const WebappUtils = window.WebappUtils;
 
 const editDebounceDelayMilliseconds = 2000;
 
@@ -33,14 +22,9 @@ interface ReportProps {
 }
 
 const Report = (props: ReportProps) => {
-    const textareaRef = useRef(null);
-    const [editing, setEditing] = useState(false);
     const [publishedThisSession, setPublishedThisSession] = useState(false);
     const [showConfirmation, setShowConfirmation] = useState(false);
-    const team = useSelector<GlobalState, Team>((state) => getTeam(state, props.playbookRun.team_id));
     const {formatMessage} = useIntl();
-
-    useClickOutsideRef(textareaRef, () => setEditing(false));
 
     const confirmedPublish = () => {
         publishRetrospective(props.playbookRun.id, props.playbookRun.retrospective);
@@ -78,32 +62,11 @@ const Report = (props: ReportProps) => {
                     </PrimaryButtonSmaller>
                 </HeaderButtonsRight>
             </Header>
-            {
-                editing &&
-                <ReportTextArea
-                    stopEditing={() => {
-                        setEditing(false);
-                        debouncedPersistEditEvent.flush();
-                    }}
-                    initialText={props.playbookRun.retrospective}
-                    onEdit={debouncedPersistEditEvent}
-                />
-            }
-            {
-                !editing &&
-                <PostTextContainer
-                    data-testid={'retro-report-text'}
-                    onClick={() => setEditing(true)}
-                >
-                    <PostText
-                        text={props.playbookRun.retrospective}
-                        team={team}
-                    />
-                </PostTextContainer>
-            }
-            <RouteLeavingGuard
-                navigate={(path) => WebappUtils.browserHistory.push(path)}
-                shouldBlockNavigation={() => editing}
+            <ReportTextArea
+                teamId={props.playbookRun.team_id}
+                initialText={props.playbookRun.retrospective}
+                onEdit={debouncedPersistEditEvent}
+                flushChanges={() => debouncedPersistEditEvent.flush()}
             />
             <ConfirmModal
                 show={showConfirmation}
@@ -129,19 +92,6 @@ const HeaderButtonsRight = styled.div`
 
     > * {
         margin-left: 10px;
-    }
-`;
-
-const PostTextContainer = styled.div`
-    background: var(--center-channel-bg);
-    margin: 8px 0 0 0;
-    padding: 10px 25px 0 16px;
-    border: 1px solid rgba(var(--center-channel-color-rgb), 0.08);
-    border-radius: 8px;
-    flex-grow: 1;
-
-    :hover {
-        cursor: text;
     }
 `;
 
--- webapp/src/components/backstage/playbook_runs/playbook_run_backstage/retrospective/report_text_area.tsx
+++ webapp/src/components/backstage/playbook_runs/playbook_run_backstage/retrospective/report_text_area.tsx
@@ -3,36 +3,63 @@
 
 import React, {useRef, useState} from 'react';
 import styled from 'styled-components';
+import {useSelector} from 'react-redux';
+
+import {GlobalState} from 'mattermost-redux/types/store';
+import {Team} from 'mattermost-redux/types/teams';
+import {getTeam} from 'mattermost-redux/selectors/entities/teams';
 
 import {useClickOutsideRef} from 'src/hooks';
 import {StyledTextarea} from 'src/components/backstage/styles';
+import PostText from 'src/components/post_text';
 
 interface Props {
-    stopEditing: () => void;
+    teamId: string;
     initialText: string;
     onEdit: (text: string) => void;
+    flushChanges: () => void;
 }
 
-const ReportTextArea = ({stopEditing, initialText, onEdit}: Props) => {
+const ReportTextArea = ({initialText, onEdit, flushChanges, teamId}: Props) => {
+    const team = useSelector<GlobalState, Team>((state) => getTeam(state, teamId));
     const textareaRef = useRef(null);
+    const [editing, setEditing] = useState(false);
     const [text, setText] = useState(initialText);
-    useClickOutsideRef(textareaRef, stopEditing);
+    useClickOutsideRef(textareaRef, () => {
+        flushChanges();
+        setEditing(false);
+    });
+
+    if (editing) {
+        return (
+            <StyledTextArea
+                ref={textareaRef}
+                autoFocus={true}
+                onFocus={(e) => {
+                    // move the cursor to the end of the text
+                    const val = e.target.value;
+                    e.target.value = '';
+                    e.target.value = val;
+                }}
+                value={text}
+                onChange={(e) => {
+                    setText(e.target.value);
+                    onEdit(e.target.value);
+                }}
+            />
+        );
+    }
 
     return (
-        <StyledTextArea
-            ref={textareaRef}
-            autoFocus={true}
-            onFocus={(e) => {
-                const val = e.target.value;
-                e.target.value = '';
-                e.target.value = val;
-            }}
-            value={text}
-            onChange={(e) => {
-                setText(e.target.value);
-                onEdit(e.target.value);
-            }}
-        />
+        <PostTextContainer
+            data-testid={'retro-report-text'}
+            onClick={() => setEditing(true)}
+        >
+            <PostText
+                text={text}
+                team={team}
+            />
+        </PostTextContainer>
     );
 };
 
@@ -41,6 +68,19 @@ const StyledTextArea = styled(StyledTextarea)`
     min-height: 200px;
     font-size: 12px;
     flex-grow: 1;
+`;
+
+const PostTextContainer = styled.div`
+    background: var(--center-channel-bg);
+    margin: 8px 0 0 0;
+    padding: 10px 25px 0 16px;
+    border: 1px solid rgba(var(--center-channel-color-rgb), 0.08);
+    border-radius: 8px;
+    flex-grow: 1;
+
+    :hover {
+        cursor: text;
+    }
 `;
 
 export default ReportTextArea;
