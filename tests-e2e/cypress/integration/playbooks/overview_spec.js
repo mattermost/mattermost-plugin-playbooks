dd10c9267b493a020d5a73828d4ea4c62d8a503c
Cass Caplinger <cass.caplinger@mattermost.com>
2021-12-08 16:48:26 -0500

    e2e: add more playbook preview tests

--- tests-e2e/cypress/integration/playbooks/overview_spec.js
+++ tests-e2e/cypress/integration/playbooks/overview_spec.js
@@ -29,6 +29,8 @@ describe('playbooks > overview', () => {
                     teamId: testTeam.id,
                     title: 'Public Playbook',
                     memberIDs: [],
+                    retrospectiveTemplate: 'Retro template text',
+                    retrospectiveReminderIntervalSeconds: 60 * 60 * 24 * 7 // 7 days
                 }).then((playbook) => {
                     testPublicPlaybook = playbook;
                 });
@@ -68,6 +70,122 @@ describe('playbooks > overview', () => {
 
         // * Verify that the user has been redirected to the playbooks not found error page
         cy.url().should('include', '/playbooks/error?type=playbooks');
+    });
+
+    it('should switch to channels and prompt to run when clicking run', () => {
+        // # Navigate directly to the playbook
+        cy.visit(`/playbooks/playbooks/${testPublicPlaybook.id}`);
+
+        // # Click Run Playbook
+        cy.findByTestId('run-playbook').click({force: true});
+
+        // * Verify the playbook run creation dialog has opened
+        cy.get('#interactiveDialogModal').should('exist').within(() => {
+            cy.findByText('Start run').should('exist');
+        });
+    });
+
+    it('should copy playbook link', () => {
+        // # Navigate directly to the playbook
+        cy.visit(`/playbooks/playbooks/${testPublicPlaybook.id}`);
+
+        // # trigger the tooltip
+        cy.get('.icon-link-variant').trigger('mouseover');
+
+        // * Verify tooltip text
+        cy.get('#copy-playbook-link-tooltip').should('contain', 'Copy link to playbook');
+
+        stubClipboard().as('clipboard');
+
+        // # click on copy button
+        cy.get('.icon-link-variant').click().then(() => {
+            // * Verify that tooltip text changed
+            cy.get('#copy-playbook-link-tooltip').should('contain', 'Copied!');
+
+            // * Verify clipboard content
+            cy.get('@clipboard').its('contents').should('contain', `/playbooks/playbooks/${testPublicPlaybook.id}`);
+        });
+    });
+
+    it('shows checklists', () => {
+        cy.apiCreatePlaybook({
+            teamId: testTeam.id,
+            title: 'Playbook',
+            description: 'Cypress Playbook',
+            memberIDs: [],
+            checklists: [
+                {
+                    title: 'Stage 1',
+                    items: [
+                        {title: 'Step 1'},
+                        {title: 'Step 2'},
+                    ],
+                },
+            ],
+            retrospectiveTemplate: 'Cypress test template'
+        }).then((playbook) => {
+            cy.visit(`/playbooks/playbooks/${playbook.id}`);
+        });
+
+        cy.findByTestId('preview-content').within(() => {
+            // * Verify checklist and associated steps
+            cy.findByText('Checklists').next().within(() => {
+                cy.findByText('Stage 1').should('exist');
+                cy.findByText('Step 1').should('exist');
+                cy.findByText('Step 2').should('exist');
+            });
+        });
+    });
+
+    it('shows status update timer', () => {
+        cy.visit(`/playbooks/playbooks/${testPublicPlaybook.id}`);
+        cy.findByTestId('preview-content').within(() => {
+            cy.findByText('Status updates').next().within(() => {
+                cy.findByText('1 day').should('exist');
+            });
+        });
+    });
+
+    it('shows correct retrospective timer and template text', () => {
+        cy.visit(`/playbooks/playbooks/${testPublicPlaybook.id}`);
+
+        cy.findByTestId('preview-content').within(() => {
+            cy.findByText('Retrospective').next().within(() => {
+                cy.findByText('7 days').should('exist');
+                cy.findByText('Retrospective report template').click();
+                cy.findByText('Retro template text').should('exist');
+            });
+        });
+    });
+
+    it('shows statistics in usage tab', () => {
+        // # Start playbook run.
+        const now = Date.now();
+        const playbookRunName = `Run (${now})`;
+        cy.apiRunPlaybook({
+            teamId: testTeam.id,
+            playbookId: testPublicPlaybook.id,
+            playbookRunName,
+            ownerUserId: testUser.id,
+        }).then((playbookRun) => {
+            // # Go to usage view
+            cy.visit(`/playbooks/playbooks/${testPublicPlaybook.id}/usage`);
+
+            // * Verify basic information.
+            cy.findByText('Runs currently in progress').next().should('contain', '1');
+            cy.findByText('Participants currently active').next().should('contain', '1');
+            cy.findByText('Runs finished in the last 30 days').next().should('contain', '0');
+
+            // # End the run so those metrics change.
+            cy.apiFinishRun(playbookRun.id).then(() => {
+                cy.reload();
+
+                // * Verify changes.
+                cy.findByText('Runs currently in progress').next().should('contain', '0');
+                cy.findByText('Participants currently active').next().should('contain', '0');
+                cy.findByText('Runs finished in the last 30 days').next().should('contain', '1');
+            });
+        });
     });
 
     describe('permissions text', () => {
@@ -111,41 +229,6 @@ describe('playbooks > overview', () => {
             // # Verify permissions text
             cy.findByTestId('playbookPermissionsDescription')
                 .contains('2 people can access this playbook');
-        });
-    });
-
-    it('should switch to channels and prompt to run when clicking run', () => {
-        // # Navigate directly to the playbook
-        cy.visit(`/playbooks/playbooks/${testPublicPlaybook.id}`);
-
-        // # Click Run Playbook
-        cy.findByTestId('run-playbook').click({force: true});
-
-        // * Verify the playbook run creation dialog has opened
-        cy.get('#interactiveDialogModal').should('exist').within(() => {
-            cy.findByText('Start run').should('exist');
-        });
-    });
-
-    it('should copy playbook link', () => {
-        // # Navigate directly to the playbook
-        cy.visit(`/playbooks/playbooks/${testPublicPlaybook.id}`);
-
-        // # trigger the tooltip
-        cy.get('.icon-link-variant').trigger('mouseover');
-
-        // * Verify tooltip text
-        cy.get('#copy-playbook-link-tooltip').should('contain', 'Copy link to playbook');
-
-        stubClipboard().as('clipboard');
-
-        // # click on copy button
-        cy.get('.icon-link-variant').click().then(() => {
-            // * Verify that tooltip text changed
-            cy.get('#copy-playbook-link-tooltip').should('contain', 'Copied!');
-
-            // * Verify clipboard content
-            cy.get('@clipboard').its('contents').should('contain', `/playbooks/playbooks/${testPublicPlaybook.id}`);
         });
     });
 
