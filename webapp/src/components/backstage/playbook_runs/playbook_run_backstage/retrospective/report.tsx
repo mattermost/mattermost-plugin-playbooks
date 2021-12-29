d08ccf5943ffba31e169e48c842312a9a57c5240
iomodo <wineson@gmail.com>
2021-12-29 12:38:02 +0400

    Disallow publishing retro repeatedly

--- webapp/src/components/backstage/playbook_runs/playbook_run_backstage/retrospective/report.tsx
+++ webapp/src/components/backstage/playbook_runs/playbook_run_backstage/retrospective/report.tsx
@@ -5,14 +5,16 @@ import React, {useState} from 'react';
 import styled from 'styled-components';
 import {useIntl} from 'react-intl';
 import debounce from 'debounce';
+import {DateTime} from 'luxon';
 
+import FormattedDuration from 'src/components/formatted_duration';
 import {PlaybookRun} from 'src/types/playbook_run';
 import {Title} from 'src/components/backstage/playbook_runs/shared';
 import {publishRetrospective, updateRetrospective} from 'src/client';
 import {PrimaryButton} from 'src/components/assets/buttons';
 import ReportTextArea
     from 'src/components/backstage/playbook_runs/playbook_run_backstage/retrospective/report_text_area';
-import ConfirmModal from 'src/components/widgets/confirmation_modal';
+import ConfirmModalLight from 'src/components/widgets/confirmation_modal_light';
 
 const editDebounceDelayMilliseconds = 2000;
 
@@ -22,26 +24,44 @@ interface ReportProps {
 }
 
 const Report = (props: ReportProps) => {
+    // we are creating the local state for this session to avoid get request
     const [publishedThisSession, setPublishedThisSession] = useState(false);
+    const [publishedAtThisSession, setPublishedAtThisSession] = useState(0);
+
     const [showConfirmation, setShowConfirmation] = useState(false);
     const {formatMessage} = useIntl();
 
     const confirmedPublish = () => {
         publishRetrospective(props.playbookRun.id, props.playbookRun.retrospective);
         setPublishedThisSession(true);
+        setPublishedAtThisSession(DateTime.now().valueOf());
         setShowConfirmation(false);
     };
 
-    let publishButtonText: React.ReactNode = formatMessage({defaultMessage: 'Publish'});
-    if (publishedThisSession) {
-        publishButtonText = (
+    const publishButtonText: React.ReactNode = formatMessage({defaultMessage: 'Publish'});
+    let publishComponent = (
+        <PrimaryButtonSmaller
+            onClick={() => setShowConfirmation(true)}
+        >
+            <TextContainer>{publishButtonText}</TextContainer>
+        </PrimaryButtonSmaller>
+    );
+
+    const isPublished = publishedThisSession || props.playbookRun.retrospective_published_at > 0;
+    if (isPublished) {
+        publishComponent = (
             <>
-                <i className={'icon icon-check'}/>
-                {formatMessage({defaultMessage: 'Published'})}
+                <DisabledPrimaryButtonSmaller>
+                    <TextContainer>{publishButtonText}</TextContainer>
+                </DisabledPrimaryButtonSmaller>
+                <FormattedDuration
+                    from={publishedAtThisSession || props.playbookRun.retrospective_published_at}
+                    ago={true}
+                />
+                <span>{'Published'}</span>
+                <i className={'icon icon-check-all'}/>
             </>
         );
-    } else if (props.playbookRun.retrospective_published_at && !props.playbookRun.retrospective_was_canceled) {
-        publishButtonText = formatMessage({defaultMessage: 'Republish'});
     }
 
     const persistEditEvent = (text: string) => {
@@ -55,23 +75,20 @@ const Report = (props: ReportProps) => {
             <Header>
                 <Title>{formatMessage({defaultMessage: 'Report'})}</Title>
                 <HeaderButtonsRight>
-                    <PrimaryButtonSmaller
-                        onClick={() => setShowConfirmation(true)}
-                    >
-                        <TextContainer>{publishButtonText}</TextContainer>
-                    </PrimaryButtonSmaller>
+                    {publishComponent}
                 </HeaderButtonsRight>
             </Header>
             <ReportTextArea
                 teamId={props.playbookRun.team_id}
                 text={props.playbookRun.retrospective}
+                isEditable={!isPublished}
                 onEdit={debouncedPersistEditEvent}
                 flushChanges={() => debouncedPersistEditEvent.flush()}
             />
-            <ConfirmModal
+            <ConfirmModalLight
                 show={showConfirmation}
-                title={formatMessage({defaultMessage: 'Publish retrospective'})}
-                message={formatMessage({defaultMessage: 'Are you sure you want to publish the retrospective?'})}
+                title={formatMessage({defaultMessage: 'Are you sure you want to publish'})}
+                message={formatMessage({defaultMessage: 'You will not be able to edit the retrospective report after publishing it. Do you want to publish the retrospective report?'})}
                 confirmButtonText={formatMessage({defaultMessage: 'Publish'})}
                 onConfirm={confirmedPublish}
                 onCancel={() => setShowConfirmation(false)}
@@ -89,9 +106,10 @@ const HeaderButtonsRight = styled.div`
     flex-grow: 1;
     display: flex;
     flex-direction: row-reverse;
+    align-items: center;
 
     > * {
-        margin-left: 10px;
+        margin-left: 4px;
     }
 `;
 
@@ -106,6 +124,22 @@ const ReportContainer = styled.div`
 
 const PrimaryButtonSmaller = styled(PrimaryButton)`
     height: 32px;
+`;
+
+const DisabledPrimaryButtonSmaller = styled(PrimaryButtonSmaller)`
+    background: rgba(var(--center-channel-color-rgb),0.08);
+    color: rgba(var(--center-channel-color-rgb),0.32);
+    margin-left: 16px;
+    cursor: default;
+
+    &:active:not([disabled])  {
+        background: rgba(var(--center-channel-color-rgb),0.08);
+    }
+
+    &:hover:enabled {
+        background: rgba(var(--center-channel-color-rgb),0.08);
+        cursor: default;
+    }
 `;
 
 const TextContainer = styled.span`
