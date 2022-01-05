41161136fb385313cd20a23ab0449a423ae29222
Shota Gvinepadze <wineson@gmail.com>
2022-01-05 11:53:25 +0400

    Covert urls to relative (#953)

--- server/app/urls.go
+++ server/app/urls.go
@@ -7,6 +7,7 @@ const (
 	RunsPath      = "/playbooks/runs"
 )
 
+// relative urls
 func getRunDetailsRelativeURL(playbookRunID string) string {
 	return fmt.Sprintf("%s/%s", RunsPath, playbookRunID)
 }
@@ -15,6 +16,15 @@ func getPlaybookDetailsRelativeURL(playbookID string) string {
 	return fmt.Sprintf("%s/%s", PlaybooksPath, playbookID)
 }
 
+func getPlaybooksNewRelativeURL() string {
+	return fmt.Sprintf("%s/new", PlaybooksPath)
+}
+
+func getActionsTabRelativeURL(playbookID string) string {
+	return fmt.Sprintf("%s/%s/edit/actions", PlaybooksPath, playbookID)
+}
+
+// absolute urls
 func getRunDetailsURL(siteURL string, playbookRunID string) string {
 	return fmt.Sprintf("%s%s", siteURL, getRunDetailsRelativeURL(playbookRunID))
 }
@@ -33,4 +43,12 @@ func getPlaybooksNewURL(siteURL string) string {
 
 func getPlaybookDetailsURL(siteURL string, playbookID string) string {
 	return fmt.Sprintf("%s%s", siteURL, getPlaybookDetailsRelativeURL(playbookID))
+}
+
+func getChannelURL(siteURL string, teamName string, channelName string) string {
+	return fmt.Sprintf("%s/%s/channels/%s",
+		siteURL,
+		teamName,
+		channelName,
+	)
 }
