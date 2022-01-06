4da195445ed270ddeae51d1a7850768d67580332
Christopher Speller <crspeller@gmail.com>
2022-01-06 11:53:46 -0800

    Fix autoselection of team in playbook creation modal. (#965)

--- webapp/src/components/team/team_selector.tsx
+++ webapp/src/components/team/team_selector.tsx
@@ -36,7 +36,7 @@ interface Props {
     customControl?: (props: ControlProps<Option, boolean>) => React.ReactElement;
     controlledOpenToggle?: boolean;
     teams: Team[];
-    onSelectedChange?: (teamId?: string) => void;
+    onSelectedChange?: (teamId: string) => void;
     customControlProps?: any;
     showOnRight?: boolean;
 }
@@ -122,7 +122,7 @@ export default function TeamSelector(props: Props) {
             return;
         }
         if (props.onSelectedChange) {
-            props.onSelectedChange(value?.teamId);
+            props.onSelectedChange(value?.teamId || '');
         }
     };
 
