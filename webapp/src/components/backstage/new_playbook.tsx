b13fac424977b97926f54fdb178a0100fd4206cd
Alejandro García Montoro <alejandro.garciamontoro@gmail.com>
2022-01-11 16:22:57 +0100

    Remove playbook number restriction in webapp

--- webapp/src/components/backstage/new_playbook.tsx
+++ webapp/src/components/backstage/new_playbook.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 import {Redirect} from 'react-router-dom';
 
 import PlaybookEdit from 'src/components/backstage/playbook_edit/playbook_edit';
-import {useAllowPlaybookCreationInTeams} from 'src/hooks';
 import {pluginUrl} from 'src/browser_routing';
 
 export const NewPlaybook = () => {
-    const allowedTeams = useAllowPlaybookCreationInTeams();
     const searchParams = Object.fromEntries(new URLSearchParams(location.search));
 
-    if (!searchParams.teamId || !allowedTeams.get(searchParams.teamId)) {
+    if (!searchParams.teamId) {
         return <Redirect to={pluginUrl('/playbooks')}/>;
     }
 
