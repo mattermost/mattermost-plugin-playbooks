25d1c2f40e6688d325f447f8ad6a48a88d0a5df9
Caleb Roseland <caleb@calebroseland.com>
2022-02-01 12:28:07 -0600

    fix e2e

--- tests-e2e/cypress/integration/channels/rhs/home_spec.js
+++ tests-e2e/cypress/integration/channels/rhs/home_spec.js
@@ -61,8 +61,8 @@ describe('channels > rhs > home', () => {
             const templates = [
                 {name: 'Blank', checklists: '1 checklist', actions: '1 action'},
                 {name: 'Product Release', checklists: '4 checklists', actions: '3 actions'},
-                {name: 'Customer Onboarding', checklists: '4 checklists', actions: '3 actions'},
                 {name: 'Incident Resolution', checklists: '4 checklists', actions: '4 actions'},
+                {name: 'Customer Onboarding', checklists: '4 checklists', actions: '3 actions'},
                 {name: 'Feature Lifecycle', checklists: '5 checklists', actions: '3 actions'},
                 {name: 'Employee Onboarding', checklists: '5 checklists', actions: '2 actions'},
                 {name: 'Bug Bash', checklists: '5 checklists', actions: '3 actions'},
