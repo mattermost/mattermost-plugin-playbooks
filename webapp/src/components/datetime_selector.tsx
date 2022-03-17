1406385a3db5fd2ac86f114dd5e790a4dda11d60
gbochora <g.bochorishvili@freeuni.edu.ge>
2022-03-17 18:30:55 +0400

    Add missing file

---
+++ webapp/src/components/datetime_selector.tsx
@@ -0,0 +1,303 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+import React, {useEffect, useMemo, useState} from 'react';
+import {useIntl, FormattedMessage} from 'react-intl';
+import ReactSelect, {ActionTypes, ControlProps, StylesConfig} from 'react-select';
+import styled from 'styled-components';
+
+import {parse, ParsingOption} from 'chrono-node';
+
+import {DateTime, Duration} from 'luxon';
+import debounce from 'debounce';
+
+import {useClientRect} from 'src/hooks';
+import Dropdown from 'src/components/dropdown';
+
+import {Timestamp} from 'src/webapp_globals';
+
+import {defaultMakeOptions, infer, Mode, Option} from './datetime_input';
+
+import {formatDuration} from './formatted_duration';
+
+interface ActionObj {
+    action: ActionTypes;
+}
+
+export type DateTimeOption = {
+    value: DateTime | Duration | null;
+    label?: string | JSX.Element | null;
+    mode?: Mode.DateTimeValue | Mode.DurationValue;
+    labelRHS?: JSX.Element;
+}
+
+type Props = {
+    testId?: string
+    date?:number
+    mode?: Mode.DateTimeValue | Mode.DurationValue;
+    placeholder: React.ReactNode;
+    onlyPlaceholder?: boolean;
+    isClearable?: boolean;
+    customControl?: (props: ControlProps<DateTimeOption, boolean>) => React.ReactElement;
+    controlledOpenToggle?: boolean;
+    onSelectedChange: (value: DateTimeOption | undefined | null) => void;
+    customControlProps?: any;
+    showOnRight?: boolean;
+    className?: string;
+    makeOptions?: (
+        query: string,
+        datetimeResults: DateTime[],
+        durationResults: Duration[],
+        mode: Mode,
+    ) => Option[] | null;
+
+}
+
+const optionFromMillis = (ms: number, mode: Mode.DateTimeValue | Mode.DurationValue) => ({
+    value: mode === Mode.DateTimeValue ? DateTime.fromMillis(ms) : Duration.fromMillis(ms),
+    mode,
+});
+
+const makeDefaultOptions = (mode: Mode.DateTimeValue | Mode.DurationValue) => {
+    let dateTime = DateTime.now();
+    dateTime = dateTime.endOf('day');
+
+    const list: DateTimeOption[] = [];
+    list.push(
+        {
+            ...optionFromMillis(dateTime.millisecond, mode),
+            label: <FormattedMessage defaultMessage='Today'/>,
+            labelRHS: (<LabelRight>{dateTime.weekdayShort}</LabelRight>),
+        }
+    );
+
+    dateTime = dateTime.plus({days: 1});
+    list.push(
+        {
+            ...optionFromMillis(dateTime.millisecond, mode),
+            label: <FormattedMessage defaultMessage='Tomorrow'/>,
+            labelRHS: (<LabelRight>{dateTime.weekdayShort}</LabelRight>),
+        }
+    );
+
+    dateTime = dateTime.plus({days: 6});
+    list.push(
+        {
+            ...optionFromMillis(dateTime.millisecond, mode),
+            label: <FormattedMessage defaultMessage='Next week'/>,
+            labelRHS: (<LabelRight>{dateTime.toLocaleString({weekday: 'short', day: '2-digit', month: 'short'})}</LabelRight>),
+        }
+    );
+    return list;
+};
+
+const selectedValueOption = (value: number, mode: Mode.DateTimeValue | Mode.DurationValue) => ({
+    ...optionFromMillis(value, mode),
+    labelRHS: (<CheckIcon className={'icon icon-check'}/>),
+});
+
+const chronoParsingOptions: ParsingOption = {forwardDate: true};
+
+export const DateTimeSelector = ({
+    date,
+    mode = Mode.DateTimeValue,
+    makeOptions = defaultMakeOptions,
+    ...props
+}: Props) => {
+    const {formatMessage} = useIntl();
+
+    const [isOpen, setOpen] = useState(false);
+    const toggleOpen = () => {
+        setOpen(!isOpen);
+    };
+
+    // Allow the parent component to control the open state -- only after mounting.
+    const [oldOpenToggle, setOldOpenToggle] = useState(props.controlledOpenToggle);
+    useEffect(() => {
+        // eslint-disable-next-line no-undefined
+        if (props.controlledOpenToggle !== undefined && props.controlledOpenToggle !== oldOpenToggle) {
+            setOpen(!isOpen);
+            setOldOpenToggle(props.controlledOpenToggle);
+        }
+    }, [props.controlledOpenToggle]);
+
+    const onSelectedChange = async (value: DateTimeOption | undefined, action: ActionObj) => {
+        if (action.action === 'clear') {
+            props.onSelectedChange(null);
+            return;
+        }
+        toggleOpen();
+        props.onSelectedChange(value);
+    };
+
+    // Decide where to open the datetime selector
+    const [rect, ref] = useClientRect();
+    const [moveUp, setMoveUp] = useState(0);
+    const [options, setOptionsDateTime] = useState<DateTimeOption[] | null>(null);
+
+    useEffect(() => {
+        if (!rect) {
+            setMoveUp(0);
+            return;
+        }
+
+        const innerHeight = window.innerHeight;
+        const numProfilesShown = Math.min(6, options ? options.length : optionsDefault.length);
+        const spacePerProfile = 48;
+        const dropdownYShift = 27;
+        const dropdownReqSpace = 80;
+        const extraSpace = 10;
+        const dropdownBottom = rect.top + dropdownYShift + dropdownReqSpace + (numProfilesShown * spacePerProfile) + extraSpace;
+        setMoveUp(Math.max(0, dropdownBottom - innerHeight));
+    }, [rect, options?.length]);
+
+    let target;
+    if (props.onlyPlaceholder) {
+        target = (
+            <div
+                onClick={toggleOpen}
+            >
+                {props.placeholder}
+            </div>
+        );
+    }
+    const targetWrapped = (
+        <div
+            data-testid={props.testId}
+            ref={ref}
+            className={props.className}
+        >
+            {target}
+        </div>
+    );
+
+    const optionsDefault = useMemo(() => {
+        const defaults = makeDefaultOptions(mode);
+
+        if (date) {
+            defaults.push(selectedValueOption(date, mode));
+        }
+        return defaults;
+    }, [date]);
+
+    const updateOptions = useMemo(() => debounce((query: string) => {
+        // eslint-disable-next-line no-undefined
+        const datetimes = parse(query, undefined, chronoParsingOptions).map(({start}) => DateTime.fromJSDate(start.date()));
+        const duration = infer(query, Mode.DurationValue);
+        let optionsNew;
+        if (makeOptions) {
+            optionsNew = makeOptions(query, datetimes, duration ? [duration] : [], mode) as DateTimeOption[];
+        }
+        setOptionsDateTime(optionsNew || optionsDefault);
+    }, 150), [setOptionsDateTime, makeOptions]);
+
+    const noDropdown = {DropdownIndicator: null, IndicatorSeparator: null};
+    const components = props.customControl ? {
+        ...noDropdown,
+        Control: props.customControl,
+    } : noDropdown;
+
+    return (
+        <Dropdown
+            isOpen={isOpen}
+            onClose={toggleOpen}
+            target={targetWrapped}
+            showOnRight={props.showOnRight}
+            moveUp={moveUp}
+        >
+            <ReactSelect
+                isMulti={false}
+                filterOption={null}
+                onInputChange={updateOptions}
+                autoFocus={true}
+                components={components}
+                controlShouldRenderValue={false}
+                menuIsOpen={true}
+                options={options || optionsDefault}
+                placeholder={formatMessage({defaultMessage: 'Specify date/time (“in 4 hours”, “May 1”...)'})}
+                styles={selectStyles}
+                onChange={onSelectedChange}
+                classNamePrefix='playbook-run-user-select'
+                className='playbook-run-user-select'
+                formatOptionLabel={OptionLabel}
+                {...props.customControlProps}
+            />
+        </Dropdown>
+    );
+};
+
+const TIME_SPEC = {
+    locale: 'en',
+    useDate: (_: string, {weekday, day, month, year}: any) => ({weekday, day, month, year}),
+};
+
+const OptionLabel = ({label, value, mode, labelRHS}: DateTimeOption) => {
+    if (label) {
+        return (
+            <Wrapper>
+                {label}
+                {labelRHS && <Right>{labelRHS}</Right>}
+            </Wrapper>
+        );
+    }
+
+    if (!value) {
+        return null;
+    }
+
+    if (mode === Mode.DateTimeValue || (!mode && DateTime.isDateTime(value))) {
+        const timestamp = (
+            <Timestamp
+                value={DateTime.isDateTime(value) ? value : DateTime.now().plus(value)}
+                {...TIME_SPEC}
+            />
+        );
+        return (
+            <Wrapper>
+                {timestamp}
+                {labelRHS && <Right>{labelRHS}</Right>}
+            </Wrapper>
+        );
+    }
+    return Duration.isDuration(value) && formatDuration(value, 'long');
+};
+
+// styles for the select component
+const selectStyles: StylesConfig<DateTimeOption, boolean> = {
+    control: (provided) => ({...provided, minWidth: 240, margin: 8}),
+    menu: () => ({boxShadow: 'none', width: '340px'}),
+    option: (provided, state) => {
+        const hoverColor = 'rgba(20, 93, 191, 0.08)';
+        const bgHover = state.isFocused ? hoverColor : 'transparent';
+        return {
+            ...provided,
+            backgroundColor: state.isSelected ? hoverColor : bgHover,
+            color: 'unset',
+        };
+    },
+};
+
+export default DateTimeSelector;
+
+const Wrapper = styled.div`
+    display: flex;
+    flex: 1;
+`;
+
+const Right = styled.div`
+    flex-grow: 1;
+    display: flex;
+    justify-content: flex-end;
+`;
+
+const CheckIcon = styled.i`
+    color: var(--button-bg);
+	font-size: 22px;
+`;
+
+const LabelRight = styled.div`
+    font-weight: 400;
+    font-size: 12px;
+    line-height: 16px;
+    color: rgba(var(--center-channel-color-rgb), 0.56);
+`;
