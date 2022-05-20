34aeb38134498bc7d9e74095f1b83bcc1f7cb5c4
Jesse Hallam <jesse@mattermost.com>
2022-05-20 15:22:02 -0300

    MM-42206: avoid concurrent profile fetch (#1177)
    * MM-44032: avoid infinite loop in useEnsureProfiles
    
    Avoid re-querying users unless the requested set of users has actually changed. Also, only actually request unknown users, not ones already in Redux.
    
    Fixes: https://mattermost.atlassian.net/browse/MM-44032
    
    * add useEnsureProfile for the single user case
    
    This reduces the number of requests on the run overview page to only the number of components, vs. every single time each component renders.
    
    * MM-42206: avoid concurrent profile fetch
    
    Change `useProfilesInTeam`, `useProfilesInChannel`, and `useProfilesInCurrentChannel` to detect and avoid the thundering herd problem when multiple components all need access to the requested user profiles.
    
    Fixes: https://mattermost.atlassian.net/browse/MM-42206
    
    * comments, switch to set
    
    * rename to locks for clarity

--- webapp/src/hooks/general.test.ts
+++ webapp/src/hooks/general.test.ts
@@ -1,11 +1,15 @@
 import {renderHook, act} from '@testing-library/react-hooks';
 import * as redux from 'react-redux';
-import {getProfilesByIds} from 'mattermost-redux/actions/users';
+import {getProfilesByIds, getProfilesInTeam, getProfilesInChannel} from 'mattermost-redux/actions/users';
 
-import {useEnsureProfile, useEnsureProfiles} from './general';
+import {PROFILE_CHUNK_SIZE} from 'src/constants';
+
+import {clearLocks, useEnsureProfile, useEnsureProfiles, useProfilesInTeam, useProfilesInCurrentChannel, useProfilesInChannel} from './general';
 
 jest.mock('mattermost-redux/actions/users', () => ({
     getProfilesByIds: jest.fn(),
+    getProfilesInTeam: jest.fn(),
+    getProfilesInChannel: jest.fn(),
 }));
 
 describe('useEnsureProfile', () => {
@@ -213,5 +217,494 @@ describe('useEnsureProfiles', () => {
         expect(getProfilesByIds).toHaveBeenCalledTimes(1);
 
         jest.clearAllMocks();
+    });
+});
+
+describe('useProfilesInTeam', () => {
+    it('dispatches if no team members have been loaded', async () => {
+        const useSelectorSpy = jest.spyOn(redux, 'useSelector');
+        const profilesInCurrentTeam = [] as String[];
+        const currentTeamId = 'team_id';
+
+        let calls = 0;
+
+        useSelectorSpy.mockImplementation(() => {
+            // since useSelector is called twice, we mock one return value, then the other.
+            if (calls++ % 2 === 0) {
+                return profilesInCurrentTeam;
+            }
+            return currentTeamId;
+        });
+
+        const useDispatchSpy = jest.spyOn(redux, 'useDispatch');
+        const mockDispatchFn = jest.fn();
+        useDispatchSpy.mockReturnValue(mockDispatchFn);
+
+        const {rerender} = renderHook(() => {
+            useProfilesInTeam();
+        });
+        expect(mockDispatchFn).toHaveBeenCalledTimes(1);
+        expect(getProfilesInTeam).toHaveBeenCalledWith('team_id', 0, PROFILE_CHUNK_SIZE);
+
+        rerender();
+        expect(mockDispatchFn).toHaveBeenCalledTimes(1);
+
+        jest.clearAllMocks();
+        clearLocks();
+    });
+
+    it('dispatches if the current team changes', async () => {
+        const useSelectorSpy = jest.spyOn(redux, 'useSelector');
+        const profilesInCurrentTeam = [] as String[];
+        let currentTeamId = 'team_id';
+
+        let calls = 0;
+        useSelectorSpy.mockImplementation(() => {
+            // since useSelector is called twice, we mock one return value, then the other.
+            if (calls++ % 2 === 0) {
+                return profilesInCurrentTeam;
+            }
+            return currentTeamId;
+        });
+
+        const useDispatchSpy = jest.spyOn(redux, 'useDispatch');
+        const mockDispatchFn = jest.fn();
+        useDispatchSpy.mockReturnValue(mockDispatchFn);
+
+        const {rerender} = renderHook(() => {
+            useProfilesInTeam();
+        });
+        expect(mockDispatchFn).toHaveBeenCalledTimes(1);
+        expect(getProfilesInTeam).toHaveBeenCalledWith('team_id', 0, PROFILE_CHUNK_SIZE);
+
+        rerender();
+        expect(mockDispatchFn).toHaveBeenCalledTimes(1);
+
+        currentTeamId = 'new_team_id';
+        rerender();
+        expect(mockDispatchFn).toHaveBeenCalledTimes(2);
+        expect(getProfilesInTeam).toHaveBeenCalledWith('new_team_id', 0, PROFILE_CHUNK_SIZE);
+
+        jest.clearAllMocks();
+        clearLocks();
+    });
+
+    it('does not dispatch after loading team members', async () => {
+        const useSelectorSpy = jest.spyOn(redux, 'useSelector');
+        let profilesInCurrentTeam = [] as String[];
+        const currentTeamId = 'team_id';
+
+        let calls = 0;
+        useSelectorSpy.mockImplementation(() => {
+            // since useSelector is called twice, we mock one return value, then the other.
+            if (calls++ % 2 === 0) {
+                return profilesInCurrentTeam;
+            }
+            return currentTeamId;
+        });
+
+        const useDispatchSpy = jest.spyOn(redux, 'useDispatch');
+        const mockDispatchFn = jest.fn();
+        useDispatchSpy.mockReturnValue(mockDispatchFn);
+
+        const {rerender} = renderHook(() => {
+            useProfilesInTeam();
+        });
+        expect(mockDispatchFn).toHaveBeenCalledTimes(1);
+        expect(getProfilesInTeam).toHaveBeenCalledWith('team_id', 0, PROFILE_CHUNK_SIZE);
+
+        profilesInCurrentTeam = ['user_1', 'user_2'];
+        rerender();
+        expect(mockDispatchFn).toHaveBeenCalledTimes(1);
+
+        jest.clearAllMocks();
+        clearLocks();
+    });
+
+    it('does not dispatch if team members are already loaded', async () => {
+        const useSelectorSpy = jest.spyOn(redux, 'useSelector');
+        const profilesInCurrentTeam = ['user_1', 'user_2'];
+        const currentTeamId = 'team_id';
+
+        let calls = 0;
+        useSelectorSpy.mockImplementation(() => {
+            // since useSelector is called twice, we mock one return value, then the other.
+            if (calls++ % 2 === 0) {
+                return profilesInCurrentTeam;
+            }
+            return currentTeamId;
+        });
+
+        const useDispatchSpy = jest.spyOn(redux, 'useDispatch');
+        const mockDispatchFn = jest.fn();
+        useDispatchSpy.mockReturnValue(mockDispatchFn);
+
+        const {rerender} = renderHook(() => {
+            useProfilesInTeam();
+        });
+        expect(mockDispatchFn).toHaveBeenCalledTimes(0);
+
+        rerender();
+        expect(mockDispatchFn).toHaveBeenCalledTimes(0);
+
+        jest.clearAllMocks();
+        clearLocks();
+    });
+
+    it('does not dispatch if already fetching', async () => {
+        const useSelectorSpy = jest.spyOn(redux, 'useSelector');
+        const profilesInCurrentTeam = [] as String[];
+        let currentTeamId = 'team_id';
+
+        let calls = 0;
+        useSelectorSpy.mockImplementation(() => {
+            // since useSelector is called twice, we mock one return value, then the other.
+            if (calls++ % 2 === 0) {
+                return profilesInCurrentTeam;
+            }
+            return currentTeamId;
+        });
+
+        const useDispatchSpy = jest.spyOn(redux, 'useDispatch');
+        const mockDispatchFn = jest.fn();
+        useDispatchSpy.mockReturnValue(mockDispatchFn);
+
+        const {rerender: rerender1} = renderHook(() => {
+            useProfilesInTeam();
+        });
+        const {rerender: rerender2} = renderHook(() => {
+            useProfilesInTeam();
+        });
+        expect(mockDispatchFn).toHaveBeenCalledTimes(1);
+        expect(getProfilesInTeam).toHaveBeenCalledWith('team_id', 0, PROFILE_CHUNK_SIZE);
+
+        rerender1();
+        rerender2();
+        expect(mockDispatchFn).toHaveBeenCalledTimes(1);
+
+        currentTeamId = 'new_team_id';
+        rerender1();
+        rerender2();
+        expect(mockDispatchFn).toHaveBeenCalledTimes(2);
+        expect(getProfilesInTeam).toHaveBeenCalledWith('new_team_id', 0, PROFILE_CHUNK_SIZE);
+
+        jest.clearAllMocks();
+        clearLocks();
+    });
+});
+
+describe('useProfilesInCurrentChannel', () => {
+    it('dispatches if no channel members have been loaded', async () => {
+        const useSelectorSpy = jest.spyOn(redux, 'useSelector');
+        const profilesInChannel = [] as String[];
+        const currentChannelId = 'channel_id';
+
+        let calls = 0;
+        useSelectorSpy.mockImplementation(() => {
+            // since useSelector is called twice, we mock one return value, then the other.
+            if (calls++ % 2 === 0) {
+                return currentChannelId;
+            }
+            return profilesInChannel;
+        });
+
+        const useDispatchSpy = jest.spyOn(redux, 'useDispatch');
+        const mockDispatchFn = jest.fn();
+        useDispatchSpy.mockReturnValue(mockDispatchFn);
+
+        const {rerender} = renderHook(() => {
+            useProfilesInCurrentChannel();
+        });
+        expect(mockDispatchFn).toHaveBeenCalledTimes(1);
+        expect(getProfilesInChannel).toHaveBeenCalledWith('channel_id', 0, PROFILE_CHUNK_SIZE);
+
+        rerender();
+        expect(mockDispatchFn).toHaveBeenCalledTimes(1);
+
+        jest.clearAllMocks();
+        clearLocks();
+    });
+
+    it('dispatches if the channel changes', async () => {
+        const useSelectorSpy = jest.spyOn(redux, 'useSelector');
+        const profilesInChannel = [] as String[];
+        let currentChannelId = 'channel_id';
+
+        let calls = 0;
+        useSelectorSpy.mockImplementation(() => {
+            // since useSelector is called twice, we mock one return value, then the other.
+            if (calls++ % 2 === 0) {
+                return currentChannelId;
+            }
+            return profilesInChannel;
+        });
+
+        const useDispatchSpy = jest.spyOn(redux, 'useDispatch');
+        const mockDispatchFn = jest.fn();
+        useDispatchSpy.mockReturnValue(mockDispatchFn);
+
+        const {rerender} = renderHook(() => {
+            useProfilesInCurrentChannel();
+        });
+        expect(mockDispatchFn).toHaveBeenCalledTimes(1);
+        expect(getProfilesInChannel).toHaveBeenCalledWith('channel_id', 0, PROFILE_CHUNK_SIZE);
+
+        rerender();
+        expect(mockDispatchFn).toHaveBeenCalledTimes(1);
+
+        currentChannelId = 'new_channel_id';
+        rerender();
+        expect(mockDispatchFn).toHaveBeenCalledTimes(2);
+        expect(getProfilesInChannel).toHaveBeenCalledWith('new_channel_id', 0, PROFILE_CHUNK_SIZE);
+
+        jest.clearAllMocks();
+        clearLocks();
+    });
+
+    it('does not dispatch after loading channel members', async () => {
+        const useSelectorSpy = jest.spyOn(redux, 'useSelector');
+        let profilesInChannel = [] as String[];
+        const currentChannelId = 'channel_id';
+
+        let calls = 0;
+        useSelectorSpy.mockImplementation(() => {
+            // since useSelector is called twice, we mock one return value, then the other.
+            if (calls++ % 2 === 0) {
+                return currentChannelId;
+            }
+            return profilesInChannel;
+        });
+
+        const useDispatchSpy = jest.spyOn(redux, 'useDispatch');
+        const mockDispatchFn = jest.fn();
+        useDispatchSpy.mockReturnValue(mockDispatchFn);
+
+        const {rerender} = renderHook(() => {
+            useProfilesInCurrentChannel();
+        });
+        expect(mockDispatchFn).toHaveBeenCalledTimes(1);
+        expect(getProfilesInChannel).toHaveBeenCalledWith('channel_id', 0, PROFILE_CHUNK_SIZE);
+
+        profilesInChannel = ['user_1', 'user_2'];
+        rerender();
+        expect(mockDispatchFn).toHaveBeenCalledTimes(1);
+
+        jest.clearAllMocks();
+        clearLocks();
+    });
+
+    it('does not dispatch if channel members are already loaded', async () => {
+        const useSelectorSpy = jest.spyOn(redux, 'useSelector');
+        const profilesInChannel = ['user_1', 'user_2'];
+        const currentChannelId = 'channel_id';
+
+        let calls = 0;
+        useSelectorSpy.mockImplementation(() => {
+            // since useSelector is called twice, we mock one return value, then the other.
+            if (calls++ % 2 === 0) {
+                return currentChannelId;
+            }
+            return profilesInChannel;
+        });
+
+        const useDispatchSpy = jest.spyOn(redux, 'useDispatch');
+        const mockDispatchFn = jest.fn();
+        useDispatchSpy.mockReturnValue(mockDispatchFn);
+
+        const {rerender} = renderHook(() => {
+            useProfilesInCurrentChannel();
+        });
+        expect(mockDispatchFn).toHaveBeenCalledTimes(0);
+
+        rerender();
+        expect(mockDispatchFn).toHaveBeenCalledTimes(0);
+
+        jest.clearAllMocks();
+        clearLocks();
+    });
+
+    it('does not dispatch if already fetching', async () => {
+        const useSelectorSpy = jest.spyOn(redux, 'useSelector');
+        const profilesInChannel = [] as String[];
+        let currentChannelId = 'channel_id';
+
+        let calls = 0;
+        useSelectorSpy.mockImplementation(() => {
+            // since useSelector is called twice, we mock one return value, then the other.
+            if (calls++ % 2 === 0) {
+                return currentChannelId;
+            }
+            return profilesInChannel;
+        });
+
+        const useDispatchSpy = jest.spyOn(redux, 'useDispatch');
+        const mockDispatchFn = jest.fn();
+        useDispatchSpy.mockReturnValue(mockDispatchFn);
+
+        const {rerender: rerender1} = renderHook(() => {
+            useProfilesInCurrentChannel();
+        });
+        const {rerender: rerender2} = renderHook(() => {
+            useProfilesInCurrentChannel();
+        });
+        expect(mockDispatchFn).toHaveBeenCalledTimes(1);
+        expect(getProfilesInChannel).toHaveBeenCalledWith('channel_id', 0, PROFILE_CHUNK_SIZE);
+
+        rerender1();
+        rerender2();
+        expect(mockDispatchFn).toHaveBeenCalledTimes(1);
+
+        currentChannelId = 'new_channel_id';
+        rerender1();
+        rerender2();
+        expect(mockDispatchFn).toHaveBeenCalledTimes(2);
+        expect(getProfilesInChannel).toHaveBeenCalledWith('new_channel_id', 0, PROFILE_CHUNK_SIZE);
+
+        jest.clearAllMocks();
+        clearLocks();
+    });
+});
+
+describe('useProfilesInChannel', () => {
+    it('dispatches if no channel members have been loaded', async () => {
+        const useSelectorSpy = jest.spyOn(redux, 'useSelector');
+        const profilesInChannel = [] as String[];
+        const channelId = 'channel_id';
+
+        useSelectorSpy.mockReturnValue(profilesInChannel);
+
+        const useDispatchSpy = jest.spyOn(redux, 'useDispatch');
+        const mockDispatchFn = jest.fn();
+        useDispatchSpy.mockReturnValue(mockDispatchFn);
+
+        const {rerender} = renderHook(() => {
+            useProfilesInChannel(channelId);
+        });
+        expect(mockDispatchFn).toHaveBeenCalledTimes(1);
+        expect(getProfilesInChannel).toHaveBeenCalledWith('channel_id', 0, PROFILE_CHUNK_SIZE);
+
+        rerender();
+        expect(mockDispatchFn).toHaveBeenCalledTimes(1);
+
+        jest.clearAllMocks();
+        clearLocks();
+    });
+
+    it('dispatches if the channel changes', async () => {
+        const useSelectorSpy = jest.spyOn(redux, 'useSelector');
+        const profilesInChannel = [] as String[];
+        let channelId = 'channel_id';
+
+        useSelectorSpy.mockReturnValue(profilesInChannel);
+
+        const useDispatchSpy = jest.spyOn(redux, 'useDispatch');
+        const mockDispatchFn = jest.fn();
+        useDispatchSpy.mockReturnValue(mockDispatchFn);
+
+        const {rerender} = renderHook(() => {
+            useProfilesInChannel(channelId);
+        });
+        expect(mockDispatchFn).toHaveBeenCalledTimes(1);
+        expect(getProfilesInChannel).toHaveBeenCalledWith('channel_id', 0, PROFILE_CHUNK_SIZE);
+
+        rerender();
+        expect(mockDispatchFn).toHaveBeenCalledTimes(1);
+
+        channelId = 'new_channel_id';
+        rerender();
+        expect(mockDispatchFn).toHaveBeenCalledTimes(2);
+        expect(getProfilesInChannel).toHaveBeenCalledWith('new_channel_id', 0, PROFILE_CHUNK_SIZE);
+
+        jest.clearAllMocks();
+        clearLocks();
+    });
+
+    it('does not dispatch after loading channel members', async () => {
+        const useSelectorSpy = jest.spyOn(redux, 'useSelector');
+        let profilesInChannel = [] as String[];
+        const channelId = 'channel_id';
+
+        useSelectorSpy.mockReturnValue(profilesInChannel);
+
+        const useDispatchSpy = jest.spyOn(redux, 'useDispatch');
+        const mockDispatchFn = jest.fn();
+        useDispatchSpy.mockReturnValue(mockDispatchFn);
+
+        const {rerender} = renderHook(() => {
+            useProfilesInChannel(channelId);
+        });
+        expect(mockDispatchFn).toHaveBeenCalledTimes(1);
+        expect(getProfilesInChannel).toHaveBeenCalledWith('channel_id', 0, PROFILE_CHUNK_SIZE);
+
+        profilesInChannel = ['user_1', 'user_2'];
+        rerender();
+        expect(mockDispatchFn).toHaveBeenCalledTimes(1);
+
+        jest.clearAllMocks();
+        clearLocks();
+    });
+
+    it('does not dispatch if channel members are already loaded', async () => {
+        const useSelectorSpy = jest.spyOn(redux, 'useSelector');
+        const profilesInChannel = ['user_1', 'user_2'];
+        const channelId = 'channel_id';
+
+        useSelectorSpy.mockReturnValue(profilesInChannel);
+
+        const useDispatchSpy = jest.spyOn(redux, 'useDispatch');
+        const mockDispatchFn = jest.fn();
+        useDispatchSpy.mockReturnValue(mockDispatchFn);
+
+        const {rerender} = renderHook(() => {
+            useProfilesInChannel(channelId);
+        });
+        expect(mockDispatchFn).toHaveBeenCalledTimes(0);
+
+        rerender();
+        expect(mockDispatchFn).toHaveBeenCalledTimes(0);
+
+        jest.clearAllMocks();
+        clearLocks();
+    });
+
+    it('does not dispatch if already fetching', async () => {
+        const useSelectorSpy = jest.spyOn(redux, 'useSelector');
+        const profilesInChannel = [] as String[];
+        let channelId = 'channel_id';
+        const channelId2 = 'channel_id2';
+
+        useSelectorSpy.mockReturnValue(profilesInChannel);
+
+        const useDispatchSpy = jest.spyOn(redux, 'useDispatch');
+        const mockDispatchFn = jest.fn();
+        useDispatchSpy.mockReturnValue(mockDispatchFn);
+
+        const {rerender: rerender1} = renderHook(() => {
+            useProfilesInChannel(channelId);
+        });
+        const {rerender: rerender2} = renderHook(() => {
+            useProfilesInChannel(channelId);
+        });
+        const {rerender: rerender3} = renderHook(() => {
+            useProfilesInChannel(channelId2);
+        });
+        expect(mockDispatchFn).toHaveBeenCalledTimes(2);
+        expect(getProfilesInChannel).toHaveBeenCalledWith('channel_id', 0, PROFILE_CHUNK_SIZE);
+        expect(getProfilesInChannel).toHaveBeenCalledWith('channel_id2', 0, PROFILE_CHUNK_SIZE);
+
+        rerender1();
+        rerender2();
+        rerender3();
+        expect(mockDispatchFn).toHaveBeenCalledTimes(2);
+
+        channelId = 'new_channel_id';
+        rerender1();
+        rerender2();
+        rerender3();
+        expect(mockDispatchFn).toHaveBeenCalledTimes(3);
+        expect(getProfilesInChannel).toHaveBeenCalledWith('new_channel_id', 0, PROFILE_CHUNK_SIZE);
+
+        jest.clearAllMocks();
+        clearLocks();
     });
 });
