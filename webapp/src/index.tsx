e4562f4010891199521345c5bc109126ba73ff8e
José Peso <trilopin@users.noreply.github.com>
2022-05-25 18:00:12 +0200

    Update webapp/src/index.tsx
    Co-authored-by: Caleb Roseland <caleb@calebroseland.com>

--- webapp/src/index.tsx
+++ webapp/src/index.tsx
@@ -148,9 +148,6 @@ export default class Plugin {
         registry.registerRootComponent(PostMenuModal);
         registry.registerRootComponent(ChannelActionsModal);
 
-        // Playbooks shared portal
-        registry.registerRootComponent(() => <div id='playbooks-root-portal'/>);
-
         // App Bar icon
         if (registry.registerAppBarComponent) {
             const siteUrl = getConfig(store.getState())?.SiteURL || '';
