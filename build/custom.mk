591e2af1ab891cae06088377fb43fb260ea79748
Christopher Speller <crspeller@gmail.com>
2022-06-02 16:10:02 -0700

    Remove old tests and mocks.

--- build/custom.mk
+++ build/custom.mk
@@ -4,19 +4,9 @@ GO_BUILD_FLAGS += -ldflags "-X main.rudderDataplaneURL=$(MM_RUDDER_DATAPLANE_URL
 ## Generate mocks.
 mocks:
 ifneq ($(HAS_SERVER),)
-	mockgen -destination server/config/mocks/mock_service.go github.com/mattermost/mattermost-plugin-playbooks/server/config Service
 	mockgen -destination server/bot/mocks/mock_logger.go github.com/mattermost/mattermost-plugin-playbooks/server/bot Logger
 	mockgen -destination server/bot/mocks/mock_poster.go github.com/mattermost/mattermost-plugin-playbooks/server/bot Poster
-	mockgen -destination server/app/mocks/mock_playbook_run_service.go github.com/mattermost/mattermost-plugin-playbooks/server/app PlaybookRunService
-	mockgen -destination server/app/mocks/mock_playbook_run_store.go github.com/mattermost/mattermost-plugin-playbooks/server/app PlaybookRunStore
 	mockgen -destination server/app/mocks/mock_job_once_scheduler.go github.com/mattermost/mattermost-plugin-playbooks/server/app JobOnceScheduler
-	mockgen -destination server/app/mocks/mock_playbook_service.go github.com/mattermost/mattermost-plugin-playbooks/server/app PlaybookService
-	mockgen -destination server/app/mocks/mock_channel_action_service.go github.com/mattermost/mattermost-plugin-playbooks/server/app ChannelActionService
-	mockgen -destination server/app/mocks/mock_channel_action_store.go github.com/mattermost/mattermost-plugin-playbooks/server/app ChannelActionStore
-	mockgen -destination server/app/mocks/mock_playbook_store.go github.com/mattermost/mattermost-plugin-playbooks/server/app PlaybookStore
-	mockgen -destination server/app/mocks/mock_keywords_ignorer.go github.com/mattermost/mattermost-plugin-playbooks/server/app KeywordsThreadIgnorer
-	mockgen -destination server/app/mocks/mock_user_info_store.go github.com/mattermost/mattermost-plugin-playbooks/server/app UserInfoStore
-	mockgen -destination server/app/mocks/mock_license_checker.go github.com/mattermost/mattermost-plugin-playbooks/server/app LicenseChecker
 	mockgen -destination server/sqlstore/mocks/mock_kvapi.go github.com/mattermost/mattermost-plugin-playbooks/server/sqlstore KVAPI
 	mockgen -destination server/sqlstore/mocks/mock_storeapi.go github.com/mattermost/mattermost-plugin-playbooks/server/sqlstore StoreAPI
 	mockgen -destination server/sqlstore/mocks/mock_configurationapi.go github.com/mattermost/mattermost-plugin-playbooks/server/sqlstore ConfigurationAPI
