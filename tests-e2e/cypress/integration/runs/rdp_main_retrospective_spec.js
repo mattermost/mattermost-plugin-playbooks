4dc2f14555d969c73462b0f7e791c2eec06b77ea
Jose Peso <jmpeso@gmail.com>
2022-07-08 14:12:08 +0200

    retro e2e migrated

--- tests-e2e/cypress/integration/runs/rdp_main_retrospective_spec.js
+++ tests-e2e/cypress/integration/runs/rdp_main_retrospective_spec.js
@@ -34,13 +34,32 @@ const editAndPublishRetro = () => {
     getRetro().get('.icon-check-all').should('be.visible');
 };
 
+const getMetricInput = (index) => getRetro().getStyledComponent('InputContainer').eq(index);
+
+const verifyMetricInput = (index, title, target, description) => {
+    getMetricInput(index).within(() => {
+        cy.getStyledComponent('Title').contains(title);
+
+        if (target) {
+            cy.getStyledComponent('TargetTitle').contains(target);
+        } else {
+            cy.getStyledComponent('TargetTitle').should('not.exist');
+        }
+
+        if (description) {
+            cy.getStyledComponent('HelpText').contains(description);
+        }
+    });
+};
+
 const getRetro = () => cy.findByTestId('run-retrospective-section');
 
-describe('runs > run details page > retrospective', () => {
+describe('runs > run details page', () => {
     let testTeam;
     let testUser;
     let testViewerUser;
     let testPublicPlaybook;
+    let testPublicPlaybookWithMetrics;
     let testRun;
 
     before(() => {
@@ -66,6 +85,36 @@ describe('runs > run details page > retrospective', () => {
             }).then((playbook) => {
                 testPublicPlaybook = playbook;
             });
+
+            // # Create a public playbook
+            cy.apiCreatePlaybook({
+                teamId: testTeam.id,
+                title: 'Public Playbook',
+                memberIDs: [],
+                createPublicPlaybookRun: true,
+                metrics: [
+                    {
+                        title: 'title1',
+                        description: 'description1',
+                        type: 'metric_duration',
+                        target: 720000,
+                    },
+                    {
+                        title: 'title2',
+                        description: 'description2',
+                        type: 'metric_currency',
+                        target: 40,
+                    },
+                    {
+                        title: 'title3',
+                        description: 'description3',
+                        type: 'metric_integer',
+                        target: 30,
+                    },
+                ]
+            }).then((playbook) => {
+                testPublicPlaybookWithMetrics = playbook;
+            });
         });
     });
 
@@ -75,62 +124,331 @@ describe('runs > run details page > retrospective', () => {
 
         // # Login as testUser
         cy.apiLogin(testUser);
-
-        cy.apiRunPlaybook({
-            teamId: testTeam.id,
-            playbookId: testPublicPlaybook.id,
-            playbookRunName: 'the run name',
-            ownerUserId: testUser.id,
-        }).then((playbookRun) => {
-            testRun = playbookRun;
-
-            // # Visit the playbook run
-            cy.visit(`/playbooks/run_details/${playbookRun.id}`);
-        });
-    });
-    const commonTests = () => {
-        it('is visible', () => {
-            // * Verify the retrospective section is present
-            getRetro().should('be.visible');
-        });
-
-        it('has title', () => {
-            // * Verify the retrospective section has a title
-            getRetro().find('h3').contains('Retrospective');
-        });
-
-        it('has template text', () => {
-            // * Verify the retrospective section has a title
-            getRetro().findByTestId('retro-report-text').contains('This is a retrospective template.');
-        });
-    };
-
-    describe('as participant', () => {
-        commonTests();
-
-        it('publishing posts to run channel', () => {
-            editAndPublishRetro();
-
-            // # Switch to the run channel
-            cy.findByTestId('runinfo-channel').click();
-
-            // * Verify the modified retro text is posted
-            cy.getStyledComponent('CustomPostContent').should('exist').contains('Edited retrospective.');
-        });
-
-        it('can be published once', () => {
-            editAndPublishRetro();
-            getRetro().findByText('Publish').should('not.be.enabled');
-        });
     });
 
-    describe('as viewer', () => {
-        beforeEach(() => {
-            cy.apiLogin(testViewerUser).then(() => {
-                cy.visit(`/playbooks/run_details/${testRun.id}`);
+    describe('retrospective', () => {
+        const commonTests = () => {
+            it('is visible', () => {
+                // * Verify the retrospective section is present
+                getRetro().should('be.visible');
+            });
+
+            it('has title', () => {
+                // * Verify the retrospective section has a title
+                getRetro().find('h3').contains('Retrospective');
+            });
+
+            it('has template text', () => {
+                // * Verify the retrospective section has a title
+                getRetro().findByTestId('retro-report-text').contains('This is a retrospective template.');
+            });
+
+            it('has no metrics', () => {
+                // * Verify there are no metric for this playbook
+                getRetro().getStyledComponent('InputContainer').should('not.exist');
+            });
+        };
+        describe('as participant', () => {
+            beforeEach(() => {
+                cy.apiRunPlaybook({
+                    teamId: testTeam.id,
+                    playbookId: testPublicPlaybook.id,
+                    playbookRunName: 'the run name',
+                    ownerUserId: testUser.id,
+                }).then((playbookRun) => {
+                    testRun = playbookRun;
+
+                    // # Visit the playbook run
+                    cy.visit(`/playbooks/run_details/${playbookRun.id}`);
+                });
+            });
+
+            commonTests();
+
+            it('publishing posts to run channel', () => {
+                editAndPublishRetro();
+
+                // # Switch to the run channel
+                cy.findByTestId('runinfo-channel').click();
+
+                // * Verify the modified retro text is posted
+                cy.getStyledComponent('CustomPostContent').should('exist').contains('Edited retrospective.');
+            });
+
+            it('can be published once', () => {
+                editAndPublishRetro();
+
+                // * Verify the button is disabled
+                getRetro().findByText('Publish').should('not.be.enabled');
             });
         });
 
-        commonTests();
+        describe('as viewer', () => {
+            before(() => {
+                cy.apiRunPlaybook({
+                    teamId: testTeam.id,
+                    playbookId: testPublicPlaybook.id,
+                    playbookRunName: 'the run name',
+                    ownerUserId: testUser.id,
+                }).then((playbookRun) => {
+                    testRun = playbookRun;
+
+                    // # Visit the playbook run
+                    cy.visit(`/playbooks/run_details/${playbookRun.id}`);
+                });
+            });
+            beforeEach(() => {
+                cy.apiLogin(testViewerUser).then(() => {
+                    cy.visit(`/playbooks/run_details/${testRun.id}`);
+                });
+            });
+
+            commonTests();
+
+            it('text is not clickable', () => {
+                getRetro().findByTestId('retro-report-text').click();
+                getRetro().find('textarea').should('not.exist');
+            });
+
+            it('there is no publish button', () => {
+                getRetro().findByText('Publish').should('not.exist');
+            });
+        });
+    });
+
+    describe('metrics', () => {
+        const commonTests = () => {
+            it('inputs info(title, target, description) and order', () => {
+                // * Verify the created metrics
+                verifyMetricInput(0, 'title1', '12 minutes', 'description1');
+                verifyMetricInput(1, 'title2', '40', 'description2');
+                verifyMetricInput(2, 'title3', '30', 'description3');
+            });
+        };
+
+        describe('as participant', () => {
+            beforeEach(() => {
+                cy.apiRunPlaybook({
+                    teamId: testTeam.id,
+                    playbookId: testPublicPlaybookWithMetrics.id,
+                    playbookRunName: 'the run name',
+                    ownerUserId: testUser.id,
+                }).then((playbookRun) => {
+                    testRun = playbookRun;
+                });
+            });
+            beforeEach(() => {
+                cy.visit(`/playbooks/run_details/${testRun.id}`);
+            });
+
+            commonTests();
+
+            it('inputs, null and zero values', () => {
+                // # Set targets to null and update playbook
+                testPublicPlaybookWithMetrics.metrics[0].target = null;
+                testPublicPlaybookWithMetrics.metrics[1].target = 0;
+                cy.apiUpdatePlaybook(testPublicPlaybookWithMetrics).then(() => {
+                    // # Navigate directly to the retro tab
+                    cy.visit(`/playbooks/run_details/${testRun.id}`);
+
+                    // * Verify if Target text(top | left) is removed
+                    verifyMetricInput(0, 'title1', null, 'description1');
+                    verifyMetricInput(1, 'title2', '0', 'description2');
+                    verifyMetricInput(2, 'title3', '30', 'description3');
+                });
+            });
+
+            it('auto save', () => {
+                getRetro().within(() => {
+                    // # Enter metric values
+                    cy.get('input[type=text]').eq(0).click();
+                    cy.get('input[type=text]').eq(0).type('12:11:10')
+                        .tab().type('56')
+                        .tab().type('123');
+
+                    // # Click outside
+                    cy.findByText('Retrospective').click({force: true});
+
+                    // * Validate if values persist
+                    cy.get('input[type=text]').eq(0).should('have.value', '12:11:10');
+                    cy.get('input[type=text]').eq(1).should('have.value', '56');
+                    cy.get('input[type=text]').eq(2).should('have.value', '123');
+
+                    // # Enter new values
+                    cy.get('input[type=text]').eq(0).click();
+                    cy.get('input[type=text]').eq(0).clear().type('12:00:10')
+                        .tab().clear().type('20')
+                        .tab().clear().type('21');
+                });
+
+                // # Wait 2 sec to auto save
+                cy.wait(2000);
+
+                // # Reload page
+                cy.visit(`/playbooks/run_details/${testRun.id}`);
+
+                getRetro().within(() => {
+                    // * Validate if values are saved
+                    cy.get('input[type=text]').eq(0).should('have.value', '12:00:10');
+                    cy.get('input[type=text]').eq(1).should('have.value', '20');
+                    cy.get('input[type=text]').eq(2).should('have.value', '21');
+                });
+            });
+
+            it('save empty and zero values', () => {
+                getRetro().within(() => {
+                    // # Enter metric values
+                    cy.get('input[type=text]').eq(0).click();
+                    cy.get('input[type=text]').eq(0).clear().type('00:00:00')
+                        .tab().type('7')
+                        .tab().type('0');
+
+                    // # Click outside
+                    cy.findByText('Retrospective').click({force: true});
+
+                    // * Validate if values persist
+                    cy.get('input[type=text]').eq(0).should('have.value', '00:00:00');
+                    cy.get('input[type=text]').eq(1).should('have.value', '7');
+                    cy.get('input[type=text]').eq(2).should('have.value', '0');
+
+                    // # Clear first two metrics values
+                    cy.get('input[type=text]').eq(0).click();
+                    cy.get('input[type=text]').eq(0).clear()
+                        .tab().clear();
+
+                    // # Click outside
+                    cy.findByText('Retrospective').click({force: true});
+
+                    // * Validate if values persist
+                    cy.get('input[type=text]').eq(0).should('have.value', '');
+                    cy.get('input[type=text]').eq(1).should('have.value', '');
+                    cy.get('input[type=text]').eq(2).should('have.value', '0');
+                });
+            });
+
+            it('only valid values are saved. check error messages', () => {
+                getRetro().within(() => {
+                    // # Enter invalid metric values
+                    cy.get('input[type=text]').eq(0).click();
+                    cy.get('input[type=text]').eq(0).type('5')
+                        .tab().type('56d')
+                        .tab().type('125');
+
+                    // * Validate error messages
+                    cy.getStyledComponent('ErrorText').eq(0).contains('Please enter a duration in the format: dd:hh:mm (e.g., 12:00:00).');
+                    cy.getStyledComponent('ErrorText').eq(1).contains('Please enter a number.');
+
+                    // # Click outside
+                    cy.findByText('Retrospective').click({force: true});
+                });
+
+                // # Reload page and navigate to the retro tab
+                cy.visit(`/playbooks/run_details/${testRun.id}`);
+
+                getRetro().within(() => {
+                    // * Validate that values are not saved
+                    cy.get('input[type=text]').eq(0).should('have.value', '');
+                    cy.get('input[type=text]').eq(1).should('have.value', '');
+                    cy.get('input[type=text]').eq(2).should('have.value', '125');
+
+                    // # Enter new metric values
+                    cy.get('input[type=text]').eq(0).click();
+                    cy.get('input[type=text]').eq(0).type('s')
+                        .tab().type('d')
+                        .tab().type('k');
+                });
+            });
+
+            it.only('publish retro', () => {
+                getRetro().within(() => {
+                    //# Enter metric invalid values
+                    cy.get('input[type=text]').eq(0).click();
+                    cy.get('input[type=text]').eq(0).type('20:00:12d')
+                        .tab().type('56')
+                        .tab().type('125v');
+
+                    // # Publish
+                    cy.findByRole('button', {name: 'Publish'}).click();
+                });
+
+                // * Verify we're not showing the publish retro confirmation modal
+                cy.get('#confirm-modal-light').should('not.exist');
+
+                getRetro().within(() => {
+                    //# Enter empty metric values
+                    cy.get('input[type=text]').eq(0).click();
+                    cy.get('input[type=text]').eq(0).clear()
+                        .tab().clear()
+                        .tab().clear().type(24);
+
+                    // # Publish
+                    cy.findByRole('button', {name: 'Publish'}).click();
+
+                    // * Validate error messages
+                    cy.getStyledComponent('ErrorText').eq(0).contains('Please fill in the metric value.');
+                    cy.getStyledComponent('ErrorText').eq(1).contains('Please fill in the metric value.');
+                    cy.getStyledComponent('ErrorText').should('have.length', 2);
+                });
+
+                // * Verify we're not showing the publish retro confirmation modal
+                cy.get('#confirm-modal-light').should('not.exist');
+
+                getRetro().within(() => {
+                    //# Enter valid metric values
+                    cy.get('input[type=text]').eq(0).click();
+                    cy.get('input[type=text]').eq(0).type('09:87:12')
+                        .tab().type(123);
+
+                    // # Publish
+                    cy.findByRole('button', {name: 'Publish'}).click();
+                });
+
+                // * Verify we're showing the publish retro confirmation modal
+                cy.get('#confirm-modal-light').contains('Are you sure you want to publish?');
+
+                // # Publish
+                cy.findByRole('button', {name: 'Publish'}).click();
+
+                getRetro().within(() => {
+                    // * Verify that retro got published
+                    cy.get('.icon-check-all').should('be.visible');
+
+                    // * Verify that metrics inputs are disabled
+                    cy.get('input[type=text]').each(($el) => {
+                        cy.wrap($el).should('not.be.enabled');
+                    });
+                });
+            });
+        });
+
+        describe('as viewer', () => {
+            before(() => {
+                cy.apiLogin(testUser);
+
+                cy.apiRunPlaybook({
+                    teamId: testTeam.id,
+                    playbookId: testPublicPlaybookWithMetrics.id,
+                    playbookRunName: 'the run name',
+                    ownerUserId: testUser.id,
+                }).then((playbookRun) => {
+                    testRun = playbookRun;
+                });
+            });
+
+            beforeEach(() => {
+                cy.apiLogin(testViewerUser).then(() => {
+                    cy.visit(`/playbooks/run_details/${testRun.id}`);
+                });
+            });
+
+            commonTests();
+
+            it('are not editable', () => {
+                // * Verify that inputs are disabled
+                getMetricInput(0).find('input').should('be.disabled');
+                getMetricInput(1).find('input').should('be.disabled');
+                getMetricInput(2).find('input').should('be.disabled');
+            });
+        });
     });
 });
