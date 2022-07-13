c7651fb8d9d4570f7ba43a801dbec8c3206cda4d
Alejandro García Montoro <alejandro.garciamontoro@gmail.com>
2022-07-13 12:46:56 +0200

    Add Run Info Overview tests

--- tests-e2e/cypress/integration/runs/rdp_rhs_runinfo_spec.js
+++ tests-e2e/cypress/integration/runs/rdp_rhs_runinfo_spec.js
@@ -13,7 +13,11 @@ describe('runs > run details page > run info', () => {
     let testPublicPlaybook;
     let testRun;
 
-    const getRHS = () => cy.findByRole('complementary');
+    const getRHSOverview = () => cy.findByRole('complementary').contains("section", "Overview");
+
+    const getOverviewEntry = (entryName) => {
+        return getRHSOverview().findByTestId(`runinfo-${entryName}`);
+    }
 
     before(() => {
         cy.apiInitSetup().then(({team, user}) => {
@@ -60,10 +64,62 @@ describe('runs > run details page > run info', () => {
         });
     });
 
+    const commonTests = () => {
+
+        it('Playbook entry links to the playbook', () => {
+            // # Click on the Playbook entry
+            getOverviewEntry('playbook').click();
+
+            // * Verify the we're in the right playbook page
+            cy.url().should('include', '/playbooks/playbooks');
+            cy.findByTestId('playbook-editor-title').contains(testPublicPlaybook.title);
+        });
+
+        it('Owner entry shows the owner', () => {
+            // * Verify that the owner is shown
+            getOverviewEntry('owner').contains(testUser.username);
+        });
+
+        it('Participants entry shows the participants', () => {
+            // * Verify that the participants are rendered
+            getOverviewEntry('participants').within(() => {
+                cy.getStyledComponent('Participants').within(() => {
+                    cy.getStyledComponent('UserPic').should('exist');
+                })
+            });
+        });
+    }
+
     describe('as participant', () => {
+        commonTests();
+
+        it('Following button can be toggled', () => {
+            getOverviewEntry('following').within(() => {
+                // * Verify that the user shows in the following list
+                cy.getStyledComponent('UserRow').within(() => {
+                    cy.getStyledComponent('UserPic').should('have.length', 1);
+                });
+
+                // # Click the Following button
+                cy.findByRole('button', {name: /Following/}).click({force: true});
+
+                // * Verify that it now says (exactly) Follow
+                cy.findByRole('button', {name: /^Follow$/}).should('exist');
+
+                // * Verify that the user no longer shows in the following list
+                cy.getStyledComponent('UserRow').should('not.exist');
+
+                // # Click the Follow button
+                cy.findByRole('button', {name: /^Follow$/}).click({force: true});
+
+                // * Verify that it now says Following
+                cy.findByRole('button', {name: /Following/}).should('exist');
+            })
+        })
+
         it('click channel link navigates to run\'s channel', () => {
             // # Click on channel item
-            getRHS().findByTestId('runinfo-channel').click();
+            getOverviewEntry('channel').click();
 
             // * Assert we navigated correctly
             cy.url().should('include', `${testTeam.name}/channels/the-run-name`);
@@ -77,9 +133,37 @@ describe('runs > run details page > run info', () => {
             });
         });
 
+        commonTests();
+
+        it('Following button can be toggled', () => {
+            getOverviewEntry('following').within(() => {
+                // * Verify that the user is not in the following list
+                cy.getStyledComponent('UserRow').within(() => {
+                    cy.getStyledComponent('UserPic').should('have.length', 1);
+                });
+
+                // # Click the Follow button
+                cy.findByRole('button', {name: /^Follow$/}).click({force: true});
+
+                // * Verify that it now says Following
+                cy.findByRole('button', {name: /Following/}).should('exist');
+
+                // * Verify that the user is now in the following list
+                cy.getStyledComponent('UserRow').within(() => {
+                    cy.getStyledComponent('UserPic').should('have.length', 2);
+                });
+
+                // # Click the Follow button
+                cy.findByRole('button', {name: /Following/}).click({force: true});
+
+                // * Verify that it now says (exactly) Follow
+                cy.findByRole('button', {name: /^Follow$/}).should('exist');
+            });
+        });
+
         it('there is no channel link', () => {
             // * Assert that the link is not present
-            getRHS().findByTestId('runinfo-channel').should('not.exist');
+            getOverviewEntry('channel').should('not.exist');
         });
     });
 });
