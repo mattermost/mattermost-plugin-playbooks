e6a15a58fade94865d4451ae48c2bc6e2e234f00
Alejandro García Montoro <alejandro.garciamontoro@gmail.com>
2022-07-13 18:24:33 +0200

    Add Run Info Key Metrics tests

--- tests-e2e/cypress/integration/runs/rdp_rhs_runinfo_spec.js
+++ tests-e2e/cypress/integration/runs/rdp_rhs_runinfo_spec.js
@@ -13,12 +13,6 @@ describe('runs > run details page > run info', () => {
     let testPublicPlaybook;
     let testRun;
 
-    const getRHSOverview = () => cy.findByRole('complementary').contains("section", "Overview");
-
-    const getOverviewEntry = (entryName) => {
-        return getRHSOverview().findByTestId(`runinfo-${entryName}`);
-    }
-
     before(() => {
         cy.apiInitSetup().then(({team, user}) => {
             testTeam = team;
@@ -64,106 +58,276 @@ describe('runs > run details page > run info', () => {
         });
     });
 
-    const commonTests = () => {
+    const getRHSSection = (title) => cy.findByRole('complementary').contains('section', title);
 
-        it('Playbook entry links to the playbook', () => {
-            // # Click on the Playbook entry
-            getOverviewEntry('playbook').click();
+    describe('> overview', () => {
+        const getOverviewEntry = (entryName) => (
+            getRHSSection('Overview').findByTestId(`runinfo-${entryName}`)
+        );
 
-            // * Verify the we're in the right playbook page
-            cy.url().should('include', '/playbooks/playbooks');
-            cy.findByTestId('playbook-editor-title').contains(testPublicPlaybook.title);
-        });
+        const commonTests = () => {
+            it('Playbook entry links to the playbook', () => {
+                // # Click on the Playbook entry
+                getOverviewEntry('playbook').click();
 
-        it('Owner entry shows the owner', () => {
-            // * Verify that the owner is shown
-            getOverviewEntry('owner').contains(testUser.username);
-        });
+                // * Verify the we're in the right playbook page
+                cy.url().should('include', '/playbooks/playbooks');
+                cy.findByTestId('playbook-editor-title').contains(testPublicPlaybook.title);
+            });
 
-        it('Participants entry shows the participants', () => {
-            // * Verify that the participants are rendered
-            getOverviewEntry('participants').within(() => {
-                cy.getStyledComponent('Participants').within(() => {
-                    cy.getStyledComponent('UserPic').should('exist');
+            it('Owner entry shows the owner', () => {
+                // * Verify that the owner is shown
+                getOverviewEntry('owner').contains(testUser.username);
+            });
+
+            it('Participants entry shows the participants', () => {
+                // * Verify that the participants are rendered
+                getOverviewEntry('participants').within(() => {
+                    cy.getStyledComponent('Participants').within(() => {
+                        cy.getStyledComponent('UserPic').should('exist');
+                    })
+                });
+            });
+        }
+
+        describe('as participant', () => {
+            commonTests();
+
+            it('Following button can be toggled', () => {
+                getOverviewEntry('following').within(() => {
+                    // * Verify that the user shows in the following list
+                    cy.getStyledComponent('UserRow').within(() => {
+                        cy.getStyledComponent('UserPic').should('have.length', 1);
+                    });
+
+                    // # Click the Following button
+                    cy.findByRole('button', {name: /Following/}).click({force: true});
+
+                    // * Verify that it now says (exactly) Follow
+                    cy.findByRole('button', {name: /^Follow$/}).should('exist');
+
+                    // * Verify that the user no longer shows in the following list
+                    cy.getStyledComponent('UserRow').should('not.exist');
+
+                    // # Click the Follow button
+                    cy.findByRole('button', {name: /^Follow$/}).click({force: true});
+
+                    // * Verify that it now says Following
+                    cy.findByRole('button', {name: /Following/}).should('exist');
                 })
+            })
+
+            it('click channel link navigates to run\'s channel', () => {
+                // # Click on channel item
+                getOverviewEntry('channel').click();
+
+                // * Assert we navigated correctly
+                cy.url().should('include', `${testTeam.name}/channels/the-run-name`);
             });
         });
-    }
 
-    describe('as participant', () => {
-        commonTests();
-
-        it('Following button can be toggled', () => {
-            getOverviewEntry('following').within(() => {
-                // * Verify that the user shows in the following list
-                cy.getStyledComponent('UserRow').within(() => {
-                    cy.getStyledComponent('UserPic').should('have.length', 1);
+        describe('as viewer', () => {
+            beforeEach(() => {
+                cy.apiLogin(testViewerUser).then(() => {
+                    cy.visit(`/playbooks/run_details/${testRun.id}`);
                 });
+            });
 
-                // # Click the Following button
-                cy.findByRole('button', {name: /Following/}).click({force: true});
+            commonTests();
 
-                // * Verify that it now says (exactly) Follow
-                cy.findByRole('button', {name: /^Follow$/}).should('exist');
+            it('Following button can be toggled', () => {
+                getOverviewEntry('following').within(() => {
+                    // * Verify that the user is not in the following list
+                    cy.getStyledComponent('UserRow').within(() => {
+                        cy.getStyledComponent('UserPic').should('have.length', 1);
+                    });
 
-                // * Verify that the user no longer shows in the following list
-                cy.getStyledComponent('UserRow').should('not.exist');
+                    // # Click the Follow button
+                    cy.findByRole('button', {name: /^Follow$/}).click({force: true});
 
-                // # Click the Follow button
-                cy.findByRole('button', {name: /^Follow$/}).click({force: true});
+                    // * Verify that it now says Following
+                    cy.findByRole('button', {name: /Following/}).should('exist');
 
-                // * Verify that it now says Following
-                cy.findByRole('button', {name: /Following/}).should('exist');
-            })
-        })
+                    // * Verify that the user is now in the following list
+                    cy.getStyledComponent('UserRow').within(() => {
+                        cy.getStyledComponent('UserPic').should('have.length', 2);
+                    });
 
-        it('click channel link navigates to run\'s channel', () => {
-            // # Click on channel item
-            getOverviewEntry('channel').click();
+                    // # Click the Follow button
+                    cy.findByRole('button', {name: /Following/}).click({force: true});
 
-            // * Assert we navigated correctly
-            cy.url().should('include', `${testTeam.name}/channels/the-run-name`);
+                    // * Verify that it now says (exactly) Follow
+                    cy.findByRole('button', {name: /^Follow$/}).should('exist');
+                });
+            });
+
+            it('there is no channel link', () => {
+                // * Assert that the link is not present
+                getOverviewEntry('channel').should('not.exist');
+            });
         });
     });
 
-    describe('as viewer', () => {
-        beforeEach(() => {
-            cy.apiLogin(testViewerUser).then(() => {
-                cy.visit(`/playbooks/run_details/${testRun.id}`);
+
+    describe.only('> key metrics', () => {
+        describe('playbook without metrics', () => {
+            describe('it should not render', () => {
+                it('as participant', () => {
+                    getRHSSection('Key Metrics').should('not.exist');
+                });
+
+                it('as viewer', () => {
+                    cy.apiLogin(testViewerUser).then(() => {
+                        cy.visit(`/playbooks/run_details/${testRun.id}`);
+                    });
+                    getRHSSection('Key Metrics').should('not.exist');
+                });
             });
         });
 
-        commonTests();
+        describe('playbook with metrics', () => {
+            let playbookWithMetrics;
+            let runWithMetrics;
 
-        it('Following button can be toggled', () => {
-            getOverviewEntry('following').within(() => {
-                // * Verify that the user is not in the following list
-                cy.getStyledComponent('UserRow').within(() => {
-                    cy.getStyledComponent('UserPic').should('have.length', 1);
+            before(() => {
+                // # Login as testUser
+                cy.apiLogin(testUser);
+
+                // # Create a public playbook with metrics
+                cy.apiCreatePlaybook({
+                    teamId: testTeam.id,
+                    title: 'Public Playbook with metrics',
+                    memberIDs: [],
+                    metrics: [
+                        {
+                            title: 'Duration',
+                            description: 'duration',
+                            type: 'metric_duration',
+                            target: 6000,
+                        },
+                        {
+                            title: 'Currency',
+                            description: 'currency',
+                            type: 'metric_currency',
+                            target: 100,
+                        },
+                        {
+                            title: 'Integer',
+                            description: 'integer',
+                            type: 'metric_integer',
+                            target: 1,
+                        },
+                    ],
+                }).then((playbook) => {
+                    playbookWithMetrics = playbook;
                 });
-
-                // # Click the Follow button
-                cy.findByRole('button', {name: /^Follow$/}).click({force: true});
-
-                // * Verify that it now says Following
-                cy.findByRole('button', {name: /Following/}).should('exist');
-
-                // * Verify that the user is now in the following list
-                cy.getStyledComponent('UserRow').within(() => {
-                    cy.getStyledComponent('UserPic').should('have.length', 2);
-                });
-
-                // # Click the Follow button
-                cy.findByRole('button', {name: /Following/}).click({force: true});
-
-                // * Verify that it now says (exactly) Follow
-                cy.findByRole('button', {name: /^Follow$/}).should('exist');
             });
-        });
 
-        it('there is no channel link', () => {
-            // * Assert that the link is not present
-            getOverviewEntry('channel').should('not.exist');
+            beforeEach(() => {
+                // # Size the viewport to show the RHS without covering posts.
+                cy.viewport('macbook-13');
+
+                // # Login as testUser
+                cy.apiLogin(testUser);
+
+                cy.apiRunPlaybook({
+                    teamId: testTeam.id,
+                    playbookId: playbookWithMetrics.id,
+                    playbookRunName: 'the run name',
+                    ownerUserId: testUser.id,
+                }).then((playbookRun) => {
+                    runWithMetrics = playbookRun;
+
+                    // # Visit the playbook run
+                    cy.visit(`/playbooks/run_details/${playbookRun.id}`);
+                });
+            });
+
+            const commonTests = () => {
+                it('key metrics is present', () => {
+                    getRHSSection('Key Metrics').should('exist');
+                });
+
+                it('link scrolls to retrospective', () => {
+                    cy.findByRole('link', {name: /View Retrospective/}).click({force: true});
+                    cy.url().should('contain', '#playbookrun-retrospective');
+                });
+
+                it('metric items scroll to corresponding metric', () => {
+                    getRHSSection('Key Metrics').within(() => {
+                        playbookWithMetrics.metrics.forEach((metric) => {
+                            cy.findByText(metric.title).click({force: true});
+                            cy.url().should('contain', `#playbook-run-retrospective${metric.id}`);
+                        });
+                    });
+                });
+            }
+
+            describe('as participant', () => {
+                commonTests();
+
+                it('metric items show Add value if empty', () => {
+                    getRHSSection('Key Metrics').within(() => {
+                        playbookWithMetrics.metrics.forEach((metric) => {
+                            cy.findByText(metric.title).parent().contains('Add value...');
+                        });
+                    });
+                });
+
+                it('click on metric items, type and see the result in the RHS', () => {
+                    const testData = {
+                        'metric_duration': {
+                            input: '12:06:03',
+                            expected: '12d, 6h, 3m',
+                        },
+                        'metric_currency': {
+                            input: '5000',
+                            expected: '5000',
+                        },
+                        'metric_integer': {
+                            input: '42',
+                            expected: '42',
+                        },
+                    }
+
+                    // # Type the values for the metrics
+                    getRHSSection('Key Metrics').within(() => {
+                        playbookWithMetrics.metrics.forEach((metric) => {
+                            // # Click on the metric row
+                            cy.findByText(metric.title).click();
+
+                            // # Type a value for the metric
+                            cy.focused().type(testData[metric.type].input);
+                        });
+                    });
+
+                    // * Verify that the RHS is updated with those values
+                    getRHSSection('Key Metrics').within(() => {
+                        playbookWithMetrics.metrics.forEach((metric) => {
+                            // * Verify that the metric was updated in the RHS
+                            cy.findByText(metric.title).parent().contains(testData[metric.type].expected);
+                        });
+                    });
+                });
+            });
+
+            describe('as viewer', () => {
+                beforeEach(() => {
+                    cy.apiLogin(testViewerUser).then(() => {
+                        cy.visit(`/playbooks/run_details/${runWithMetrics.id}`);
+                    });
+                });
+
+                commonTests();
+
+                it.only('metric items show - if empty', () => {
+                    getRHSSection('Key Metrics').within(() => {
+                        playbookWithMetrics.metrics.forEach((metric) => {
+                            cy.findByText(metric.title).parent().contains('-');
+                        });
+                    });
+                });
+            });
         });
     });
 });
