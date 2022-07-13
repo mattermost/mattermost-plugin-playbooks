ec98374bdf16841bef099c225542ec8bad7cbd2d
Jose Peso <jmpeso@gmail.com>
2022-07-13 08:45:25 +0200

    fix escape quotes

--- webapp/src/components/backstage/playbook_runs/playbook_run/header.tsx
+++ webapp/src/components/backstage/playbook_runs/playbook_run/header.tsx
@@ -68,7 +68,7 @@ export const RunHeader = ({playbookRun, playbookRunMetadata, role, onViewInfo, o
             return;
         }
         await dispatch(joinChannel(currentUserId, playbookRun.team_id, playbookRun.channel_id, playbookRunMetadata.channel_name));
-        addToast(formatMessage({defaultMessage: 'You've joined this run.'}), ToastType.Success);
+        addToast(formatMessage({defaultMessage: 'You\'ve joined this run.'}), ToastType.Success);
     };
 
     return (
@@ -121,8 +121,7 @@ export const RunHeader = ({playbookRun, playbookRunMetadata, role, onViewInfo, o
             <ConfirmModal
                 show={showGetInvolvedConfirm}
                 title={formatMessage({defaultMessage: 'Confirm get involved'})}
-                message={channel === null ? formatMessage({defaultMessage: 'Your participation request will be sent to the run channel.'}) : formatMessage({defaultMessage: 'You're about to join this run.'})
-                }
+                message={channel === null ? formatMessage({defaultMessage: 'Your participation request will be sent to the run channel.'}) : formatMessage({defaultMessage: 'You\'re about to join this run.'})}
                 confirmButtonText={formatMessage({defaultMessage: 'Confirm'})}
                 onConfirm={() => {
                     onConfirmGetInvolved();
