1029cc2a5bd8d4b473ff6d322f0c26044c4c63df
iomodo <wineson@gmail.com>
2022-07-13 20:36:34 +0400

    Filter playbooks for LHS

--- server/sqlstore/playbook.go
+++ server/sqlstore/playbook.go
@@ -382,12 +382,14 @@ func (p *playbookStore) GetPlaybooks() ([]app.Playbook, error) {
 func (p *playbookStore) GetPlaybooksForTeam(requesterInfo app.RequesterInfo, teamID string, opts app.PlaybookFilterOptions) (app.GetPlaybooksResults, error) {
 	// Check that you are a playbook member or there are no restrictions.
 	permissionsAndFilter := sq.Expr(`(
-			p.Public = true
-			OR EXISTS(SELECT 1
-					FROM IR_PlaybookMember as pm
-					WHERE pm.PlaybookID = p.ID
-					AND pm.MemberID = ?)
+			EXISTS(SELECT 1
+				FROM IR_PlaybookMember as pm
+				WHERE pm.PlaybookID = p.ID
+				AND pm.MemberID = ?)
 		)`, requesterInfo.UserID)
+	if !opts.WithMembershipOnly { // return all public playbooks and private ones user is member of
+		permissionsAndFilter = sq.Or{sq.Expr(`p.Public = true`), permissionsAndFilter}
+	}
 	teamLimitExpr := buildTeamLimitExpr(requesterInfo.UserID, teamID, "p")
 
 	queryForResults := p.store.builder.
