6cbc4cb5426ff6c763b41c471f276741fce1a0dc
Christopher Speller <crspeller@gmail.com>
2022-08-03 09:02:11 -0700

    Fix more e2e

--- tests-e2e/cypress/integration/channels/rhs/home_spec.js
+++ tests-e2e/cypress/integration/channels/rhs/home_spec.js
@@ -95,7 +95,7 @@ describe('channels > rhs > home', () => {
         it('without pre-populated channel name template', () => {
             cy.findByText('Team Playbook').closest('[data-testid="rhs-home-item"]').find('[data-testid="run-playbook"]').click();
 
-            cy.get('#interactiveDialogModal').within(() => {
+            cy.get('#playbooks_run_playbook_dialog').within(() => {
                 // * Verify run name prompt
                 cy.get('input').eq(1).should('be.empty');
             });
@@ -104,7 +104,7 @@ describe('channels > rhs > home', () => {
         it('with pre-populated channel name template', () => {
             cy.findByText('Channel Name Template').closest('[data-testid="rhs-home-item"]').find('[data-testid="run-playbook"]').click();
 
-            cy.get('#interactiveDialogModal').within(() => {
+            cy.get('#playbooks_run_playbook_dialog').within(() => {
                 // * Verify run name prompt
                 cy.get('input').eq(1).should('have.value', 'templated name');
             });
