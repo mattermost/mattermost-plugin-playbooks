089dcd9b3b07b2d6b503293dbb65d7ce33e4ab29
Giorgi Bochorishvili <g.bochorishvili@freeuni.edu.ge>
2022-08-16 13:36:17 +0400

    Mm 44247 morph v0.4.0 >> v0.7.0 (#1405)
    * Add v0.4.0, v0.5.0 and v0.6.0 migrations

--- server/sqlstore/migrations/mysql/000010_add_previous_reminder_to_ir_incident.down.sql
+++ server/sqlstore/migrations/mysql/000010_add_previous_reminder_to_ir_incident.down.sql
@@ -3,9 +3,9 @@ SET @preparedStatement = (SELECT IF(
         SELECT 1 FROM INFORMATION_SCHEMA.COLUMNS
         WHERE table_name = 'IR_Incident'
         AND table_schema = DATABASE()
-        AND column_name = 'BroadcastChannelID'
+        AND column_name = 'PreviousReminder'
     ),
-    'ALTER TABLE IR_Incident DROP COLUMN BroadcastChannelID;',
+    'ALTER TABLE IR_Incident DROP COLUMN PreviousReminder;',
     'SELECT 1;'
 ));
 
