d18afdc0d3cf01c65e7fa22d18933965ba371fce
Christopher Speller <crspeller@gmail.com>
2022-08-18 07:57:57 -0700

    Remove race test

--- server/api_runs_test.go
+++ server/api_runs_test.go
@@ -172,8 +172,6 @@ func TestRunCreation(t *testing.T) {
 		})
 		assert.NoError(t, err)
 		assert.NotNil(t, run)
-		// assert some data has been injected
-		assert.Len(t, run.ParticipantIDs, 1)
 	})
 
 	t.Run("create valid run without playbook", func(t *testing.T) {
