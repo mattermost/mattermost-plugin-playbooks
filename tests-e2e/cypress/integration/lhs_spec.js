8331619846ffe7341eaa59fb8bde237691fd73ee
gbochora <g.bochorishvili@freeuni.edu.ge>
2022-08-31 09:16:37 +0400

    Add more e2e tests

--- tests-e2e/cypress/integration/lhs_spec.js
+++ tests-e2e/cypress/integration/lhs_spec.js
@@ -33,8 +33,8 @@ describe('lhs', () => {
         });
     });
 
-    const getRunDropdownItemByText = (runName, itemName) => {
-        cy.findByTestId('Runs').findByTestId(runName).findByTestId('menuButton').click();
+    const getRunDropdownItemByText = (groupName, runName, itemName) => {
+        cy.findByTestId(groupName).findByTestId(runName).findByTestId('menuButton').click();
         return cy.findByTestId('dropdownmenu').findByText(itemName);
     };
 
@@ -73,16 +73,91 @@ describe('lhs', () => {
         it('can copy link', () => {
             stubClipboard().as('clipboard');
 
-            getRunDropdownItemByText(playbookRun.name, 'Copy link').click().then(() => {
+            // # Click on Copy link menu item
+            getRunDropdownItemByText('Runs', playbookRun.name, 'Copy link').click().then(() => {
                 // * Verify clipboard content
                 cy.get('@clipboard').its('contents').should('contain', `/playbooks/runs/${playbookRun.id}`);
             });
         });
 
-        it('can make favorite', () => {
-            getRunDropdownItemByText(playbookRun.name, 'Favorite').click().then(() => {
+        it('can favorite / unfavorite', () => {
+            // # Click on favorite menu item
+            getRunDropdownItemByText('Runs', playbookRun.name, 'Favorite').click().then(() => {
                 // * Verify the run is added to favorites
                 cy.findByTestId('Favorite').findByTestId(playbookRun.name).should('exist');
+
+                // # Click on unfavorite menu item
+                getRunDropdownItemByText('Favorite', playbookRun.name, 'Unfavorite').click().then(() => {
+                    // * Verify the run is removed from favorites
+                    cy.findByTestId('Runs').findByTestId(playbookRun.name).should('exist');
+                });
+            });
+        });
+    });
+
+    describe('playbook dot menu', () => {
+        beforeEach(() => {
+            // # Size the viewport to show the RHS without covering posts.
+            cy.viewport('macbook-13');
+
+            // # Login as testUser
+            cy.apiLogin(testUser);
+
+            // # Create a public playbook
+            cy.apiCreatePlaybook({
+                teamId: testTeam.id,
+                title: 'the run name(' + Date.now() + ')',
+                memberIDs: [],
+            }).then((playbook) => {
+                testPublicPlaybook = playbook;
+
+                // # Visit the playbooks page
+                cy.visit('/playbooks/playbooks}');
+
+                // # LHS render takes a few seconds, wait for it
+                cy.wait(5000);
+            });
+        });
+
+        it('shows on click', () => {
+            // # Click dot menu
+            cy.findByTestId('Playbooks').findByTestId(testPublicPlaybook.title).findByTestId('menuButton').click();
+
+            // * Assert context menu is opened
+            cy.findByTestId('dropdownmenu').should('be.visible');
+        });
+
+        it('can copy link', () => {
+            stubClipboard().as('clipboard');
+
+            // # Click on Copy link menu item
+            getRunDropdownItemByText('Playbooks', testPublicPlaybook.title, 'Copy link').click().then(() => {
+                // * Verify clipboard content
+                cy.get('@clipboard').its('contents').should('contain', `/playbooks/playbooks/${testPublicPlaybook.id}`);
+            });
+        });
+
+        it('can favorite / unfavorite', () => {
+            // # Click on favorite menu item
+            getRunDropdownItemByText('Playbooks', testPublicPlaybook.title, 'Favorite').click().then(() => {
+                // * Verify the playbook is added to favorites
+                cy.findByTestId('Favorite').findByTestId(testPublicPlaybook.title).should('exist');
+
+                // # Click on unfavorite menu item
+                getRunDropdownItemByText('Favorite', testPublicPlaybook.title, 'Unfavorite').click().then(() => {
+                    // * Verify the playbook is removed from favorites
+                    cy.findByTestId('Playbooks').findByTestId(testPublicPlaybook.title).should('exist');
+                });
+            });
+        });
+
+        it('can leave', () => {
+            stubClipboard().as('clipboard');
+
+            // # Click on Leave menu item
+            getRunDropdownItemByText('Playbooks', testPublicPlaybook.title, 'Leave').click().then(() => {
+                // * Verify the playbook is removed from the list
+                cy.findByTestId('Playbooks').findByTestId(testPublicPlaybook.title).should('not.exist');
             });
         });
     });
