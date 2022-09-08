62e62e7f0b4505c64cfdccb874613518a80a35a0
Jesse Hallam <jesse.hallam@gmail.com>
2022-09-08 16:56:54 -0300

    MM-46829: Cherry-pick limit size incoming requests
    This PR cherry picks a portion of 4dd42f9e174a5acb1c4ccad6b038f5b819226dd5.

--- server/sqlstore/playbook_run.go
+++ server/sqlstore/playbook_run.go
@@ -1391,6 +1391,10 @@ func toSQLPlaybookRun(playbookRun app.PlaybookRun) (*sqlPlaybookRun, error) {
 		return nil, errors.Wrapf(err, "failed to marshal checklist json for playbook run id '%s'", playbookRun.ID)
 	}
 
+	if len(checklistsJSON) > maxJSONLength {
+		return nil, errors.Wrapf(err, "checklist json for playbook run id '%s' is too long (max %d)", playbookRun.ID, maxJSONLength)
+	}
+
 	return &sqlPlaybookRun{
 		PlaybookRun:                           playbookRun,
 		ChecklistsJSON:                        checklistsJSON,
