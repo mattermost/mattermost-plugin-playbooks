4dd42f9e174a5acb1c4ccad6b038f5b819226dd5
José Peso <trilopin@users.noreply.github.com>
2022-09-08 09:48:41 +0200

    limit size for all incoming requests and migrate postgres text column  (#1463)
    Limit incoming HTTP requests to be 5MB large. Any requests larger than that will fail. mattermost-server limit request with more size than max_file_size, playbooks plugin adds a lower limit: 5MB.
    
    Additionally, PostgreSQL text fields have been migrated to varchar with the limits we already apply to MySQL (some of them need revision, but it's not part of this PR). The following strategy has been used:
    - if text in postgre and varchar in MySQL -> change to varchar with MySQL limit
    - if text in postgre and text in MySQL -> change to varchar with implicit MySQL limit (65535)
    - if JSON, checked explicitly in code
    
    Migrations have been done inside one huge migration but with individual queries. Key points:
    - no data loss, if current data is longer than the target type, the individual change will fail
    - if one field change fails, the rest will be applied
    
    Fixes https://mattermost.atlassian.net/browse/MM-46829

--- server/api_runs_test.go
+++ server/api_runs_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"net/http"
+	"strings"
 	"testing"
 	"time"
 
@@ -277,6 +278,23 @@ func TestRunCreation(t *testing.T) {
 		assert.Equal(t, (now+durations[1])/10000, run.Checklists[0].Items[1].DueDate/10000)
 		assert.Equal(t, (now+durations[2])/10000, run.Checklists[1].Items[0].DueDate/10000)
 		assert.Zero(t, run.Checklists[1].Items[1].DueDate)
+	})
+}
+
+func TestCreateInvalidRuns(t *testing.T) {
+	e := Setup(t)
+	e.CreateBasic()
+
+	t.Run("fails if description is longer than 4096", func(t *testing.T) {
+		run, err := e.PlaybooksClient.PlaybookRuns.Create(context.Background(), client.PlaybookRunCreateOptions{
+			Name:        "test run",
+			OwnerUserID: e.RegularUser.Id,
+			TeamID:      e.BasicTeam.Id,
+			PlaybookID:  e.BasicPlaybook.ID,
+			Description: strings.Repeat("A", 4097),
+		})
+		requireErrorWithStatusCode(t, err, http.StatusInternalServerError)
+		assert.Nil(t, run)
 	})
 }
 
@@ -961,6 +979,30 @@ func TestChecklistManagement(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestChecklisFailTooLarge(t *testing.T) {
+	e := Setup(t)
+	e.CreateBasic()
+
+	t.Run("checklist creation - failure: too large checklist", func(t *testing.T) {
+		run, err := e.PlaybooksClient.PlaybookRuns.Create(context.Background(), client.PlaybookRunCreateOptions{
+			Name:        "Run name",
+			OwnerUserID: e.RegularUser.Id,
+			TeamID:      e.BasicTeam.Id,
+			PlaybookID:  e.BasicPlaybook.ID,
+		})
+		require.NoError(t, err)
+		require.Len(t, run.Checklists, 0)
+
+		err = e.PlaybooksClient.PlaybookRuns.CreateChecklist(context.Background(), run.ID, client.Checklist{
+			Title: "My regular title",
+			Items: []client.ChecklistItem{
+				{Title: "Item title", Description: strings.Repeat("A", (256*1024)+1)},
+			},
+		})
+		require.Error(t, err)
+	})
 }
 
 func TestRunActions(t *testing.T) {
