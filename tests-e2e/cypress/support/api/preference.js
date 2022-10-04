76a1ab47c60ba614cb2f9b81ac161740012c0294
Jose Peso <jmpeso@gmail.com>
2022-10-04 15:51:36 +0200

    bypass crt tutorial

--- tests-e2e/cypress/support/api/preference.js
+++ tests-e2e/cypress/support/api/preference.js
@@ -298,6 +298,12 @@ Cypress.Commands.add('apiDisableTutorials', (userId) => {
         },
         {
             user_id: userId,
+            category: 'crt_thread_pane_step',
+            name: userId,
+            value: '999',
+        },
+        {
+            user_id: userId,
             category: 'playbook_preview',
             name: userId,
             value: '999',
