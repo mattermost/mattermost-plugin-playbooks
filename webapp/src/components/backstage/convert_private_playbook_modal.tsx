1f4eb70090781109be43a016ce12c6eeaeec710a
cannalee90@gmail.com <cannalee90@gmail.com>
2022-10-26 01:15:41 +0900

    apply custom-hook for playbook acces modal

--- webapp/src/components/backstage/convert_private_playbook_modal.tsx
+++ webapp/src/components/backstage/convert_private_playbook_modal.tsx
@@ -3,14 +3,16 @@ import {useIntl} from 'react-intl';
 
 import ConfirmModal from 'src/components/widgets/confirmation_modal';
 import {useEditPlaybook} from 'src/hooks';
+import {PlaybookWithChecklist} from 'src/types/playbook';
 
 type ConfirmPlaybookConvertPrivateReturn = [React.ReactNode, (show: boolean) => void];
 type Props = {
     playbookId: string,
     refetch?: () => void | undefined,
+    updater?: (update: Partial<PlaybookWithChecklist>) => void,
 }
 
-const useConfirmPlaybookConvertPrivateModal = ({playbookId, refetch}: Props): ConfirmPlaybookConvertPrivateReturn => {
+const useConfirmPlaybookConvertPrivateModal = ({playbookId, refetch, updater}: Props): ConfirmPlaybookConvertPrivateReturn => {
     const {formatMessage} = useIntl();
     const [showMakePrivateConfirm, setShowMakePrivateConfirm] = useState(false);
     const [playbook, updatePlaybook] = useEditPlaybook(playbookId, refetch);
@@ -22,7 +24,9 @@ const useConfirmPlaybookConvertPrivateModal = ({playbookId, refetch}: Props): Co
             message={formatMessage({defaultMessage: 'When you convert to a private playbook, membership and run history is preserved. This change is permanent and cannot be undone. Are you sure you want to convert {playbookTitle} to a private playbook?'}, {playbookTitle: playbook?.title})}
             confirmButtonText={formatMessage({defaultMessage: 'Confirm'})}
             onConfirm={() => {
-                if (playbookId) {
+                if (playbookId && updater) {
+                    updater({public: false});
+                } else if (playbookId) {
                     updatePlaybook({public: false});
                 }
                 setShowMakePrivateConfirm(false);
