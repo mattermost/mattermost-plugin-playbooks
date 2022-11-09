907b64258d70bedcfa0645c52a24ade61267bc3b
Cass Caplinger <cass.caplinger@mattermost.com>
2022-11-09 10:35:47 -0500

    lint

--- tests-e2e/cypress/integration/playbooks/edit_metrics_spec.js
+++ tests-e2e/cypress/integration/playbooks/edit_metrics_spec.js
@@ -282,7 +282,7 @@ describe('playbooks > edit_metrics', () => {
         });
 
         describe('delete metric', () => {
-            it.only('verifies when clicking delete button; saved metrics have different confirmation text; deleted metrics are deleted', () => {
+            it('verifies when clicking delete button; saved metrics have different confirmation text; deleted metrics are deleted', () => {
                 // # Visit the selected playbook
                 cy.visit(`/playbooks/playbooks/${testPlaybook.id}`);
 
