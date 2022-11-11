989f555b4556fef40467851c9c2f7d0a1fc59da1
Christopher Speller <crspeller@gmail.com>
2022-11-10 22:10:05 -0800

    Add some more field validation.

--- tests-e2e/cypress/integration/runs/rdp_rhs_participants_spec.js
+++ tests-e2e/cypress/integration/runs/rdp_rhs_participants_spec.js
@@ -146,6 +146,8 @@ describe('runs > run details page > rhs > participants', () => {
                     expect(interception.request.body.name).to.eq('playbookrun_participate');
                     expect(interception.request.body.type).to.eq('track');
                     expect(interception.request.body.properties.from).to.eq('run_details');
+                    expect(interception.request.body.properties.trigger).to.eq('add_participant');
+                    expect(interception.request.body.properties.count).to.eq('2');
                 });
 
                 // * Verify modal message is correct
