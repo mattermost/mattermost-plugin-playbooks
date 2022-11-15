68f0681e47ea795ea369e711f9d47179ad28bd40
Jose Peso <jmpeso@gmail.com>
2022-11-15 12:08:52 +0100

    morph like queries for migration, fix comment

--- server/sqlstore/migrations/future/postgres/0.61.down.sql
+++ server/sqlstore/migrations/future/postgres/0.61.down.sql
@@ -1,11 +1,6 @@
 ALTER TABLE IR_Playbook DROP COLUMN IF EXISTS ChannelID;
 ALTER TABLE IR_Playbook DROP COLUMN IF EXISTS ChannelMode;
 
--- Drop unique constraint and kee the index
+-- add unique constraint to channelid index
 ALTER TABLE IR_Incident  ADD CONSTRAINT ir_incident_channelid_key UNIQUE(ChannelID);
 
--- update empty names on incident table with channels data
-UPDATE IR_Incident i
-SET name=c.DisplayName
-FROM Channels c
-WHERE  c.id=i.ChannelID AND i.Name='';
