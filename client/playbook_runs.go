215567c3dfa2b237b811e5333cc7dfa9cc7c09bc
José Peso <trilopin@users.noreply.github.com>
2022-11-17 14:23:59 +0100

    MM-48461 respect fullname setting at getOwners
    Honour PrivacySettings.ShowFullName value at getOwners endpoint.
    
    Added client function and tests.

--- client/playbook_runs.go
+++ client/playbook_runs.go
@@ -282,3 +282,19 @@ func (s *PlaybookRunService) PublishRetrospective(ctx context.Context, playbookR
 
 	return err
 }
+
+func (s *PlaybookRunService) GetOwners(ctx context.Context) ([]OwnerInfo, error) {
+	req, err := s.client.newRequest(http.MethodGet, "runs/owners", nil)
+	if err != nil {
+		return nil, err
+	}
+
+	owners := make([]OwnerInfo, 0)
+	resp, err := s.client.do(ctx, req, &owners)
+	if err != nil {
+		return nil, err
+	}
+	resp.Body.Close()
+
+	return owners, nil
+}
