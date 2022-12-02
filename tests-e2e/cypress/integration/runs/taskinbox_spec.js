d981ec8190ee06a179aca9d08bfcca1997c4871c
Jesse Hallam <jesse.hallam@gmail.com>
2022-12-02 15:57:58 -0400

    MM-48957: Release Task Inbox

--- tests-e2e/cypress/integration/runs/taskinbox_spec.js
+++ tests-e2e/cypress/integration/runs/taskinbox_spec.js
@@ -26,13 +26,6 @@ describe('Task Inbox >', () => {
                 cy.apiAddUserToTeam(testTeam.id, adminUser.id);
             });
 
-            cy.apiGetConfig().then(({config}) => {
-                expFeaturesFlag = config.PluginSettings.Plugins.playbooks.enableexperimentalfeatures;
-                if (!expFeaturesFlag) {
-                    cy.apiEnsureFeatureFlag('enableexperimentalfeatures', true);
-                }
-            });
-
             // Create another user in the same team
             cy.apiCreateUser().then(({user: viewer}) => {
                 testViewerUser = viewer;
@@ -74,14 +67,6 @@ describe('Task Inbox >', () => {
         });
     });
 
-    after(() => {
-        if (!expFeaturesFlag) {
-            cy.apiLogin(testAdmin).then(() => {
-                cy.apiEnsureFeatureFlag('enableexperimentalfeatures', expFeaturesFlag);
-            });
-        }
-    });
-
     beforeEach(() => {
         // # Size the viewport to show the RHS without covering posts.
         cy.viewport('macbook-13');
@@ -96,6 +81,38 @@ describe('Task Inbox >', () => {
     });
 
     const getRHS = () => cy.get('#playbooks-backstage-sidebar-right');
+
+    it('icon in global header, with experimental feature flag', () => {
+        // # Enable experimental feature flag
+        cy.apiAdminLogin().then(() => {
+            cy.apiEnsureFeatureFlag('enableexperimentalfeatures', true);
+
+            // # Login as testUser
+            cy.apiLogin(testUser);
+        });
+
+        // # Visit the playbooks product
+        cy.visit('/playbooks');
+
+        // # Verify icon present in global header icon to open
+        cy.findByTestId('header-task-inbox-icon').click();
+    });
+
+    it('icon in global header, without experimental feature flag', () => {
+        // # Disable experimental feature flag
+        cy.apiAdminLogin().then(() => {
+            cy.apiEnsureFeatureFlag('enableexperimentalfeatures', false);
+
+            // # Login as testUser
+            cy.apiLogin(testUser);
+        });
+
+        // # Visit the playbooks product
+        cy.visit('/playbooks');
+
+        // # Verify icon present in global header icon to open
+        cy.findByTestId('header-task-inbox-icon').click();
+    });
 
     it('icon toggles taskinbox view', () => {
         // # Intercept all calls to telemetry
