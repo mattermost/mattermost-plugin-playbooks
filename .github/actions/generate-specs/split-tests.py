e265d459ed94afcd7b00a94fccd51261add775dd
Antonis Stamatiou <stamatiou.antonis@gmail.com>
2023-01-12 13:39:33 +0200

    feat(ci): Generate all runs from I declaration

--- .github/actions/generate-specs/split-tests.py
+++ .github/actions/generate-specs/split-tests.py
@@ -4,17 +4,17 @@ from json import dumps
 from math import ceil
 
 def find_files(search_path):
-  result = []
+  files_list = []
 
   # Find all spec files for cypress tests
   for root, dir, files in os.walk(search_path):
     for file in files:
       if match(".*_spec.js$",file):
-        result.append(os.path.join(root,file))
-  return result
+        files_list.append(os.path.join(root,file))
+  return files_list
 
+# Split to chunks based on the parallelism provided
 def split(input_list, parallelism):
-  # Split to chunks based on the parallelism provided
   chunk_size = ceil(len(input_list) / parallelism)
   for i in range(0, len(input_list), chunk_size):
     yield input_list[i:i + chunk_size]
@@ -25,14 +25,10 @@ search_path = os.getenv("SEARCH_PATH")
 
 os.chdir(directory)
 files = find_files(search_path)
-endMatrix = {
-  "include": []
-}
+specs = []
 
-
-# Generate JSON matrix for GHA
+# Generate JSON list of specs for GHA
 for i,  spec_list in enumerate(split(files,parallelism)):
-  specs = {"specs": ','.join(spec_list)}
-  endMatrix["include"].append(specs)
+  specs.append(','.join(spec_list))
 
-print(dumps(endMatrix))
+print(dumps(specs))
