c6a0629b65b3800c2947b581d984f2d76c913c65
Julien Tant <julien@craftyx.fr>
2023-02-01 17:32:48 -0700

    add a test to help detect potention sync issues with work template

---
+++ webapp/src/components/templates/template_data.test.ts
@@ -0,0 +1,22 @@
+import TemplateData from './template_data';
+
+describe('TemplateData', () => {
+    // if a template is missing, this might break the work templates feature.
+    // if this breaks, contact the @channel team to figure out what should be done.
+    const knownTemplatesInWorkTemplate = [
+        'Product Release',
+        'Incident Resolution',
+        'Customer Onboarding',
+        'Employee Onboarding',
+        'Feature Lifecycle',
+        'Bug Bash',
+    ];
+
+    knownTemplatesInWorkTemplate.forEach((templateName) => {
+        it(`should contains ${templateName} for work template`, () => {
+            expect(
+                TemplateData.find((template) => template.title.trim() === templateName),
+            ).not.toBeUndefined();
+        });
+    });
+});
