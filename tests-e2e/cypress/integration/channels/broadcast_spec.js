413f67e09402a62467a599d2e2144a8fa719e580
Jose Peso <jmpeso@gmail.com>
2023-02-02 12:25:24 +0100

    fix e2e selectors after post migration

--- tests-e2e/cypress/integration/channels/broadcast_spec.js
+++ tests-e2e/cypress/integration/channels/broadcast_spec.js
@@ -324,8 +324,8 @@ const verifyInitialAndStatusPostInBroadcast = (testTeam, channelName, runName, i
                 // * Thread should have two posts
                 cy.findAllByRole('listitem').should('have.length', 2);
 
-                // * Root should be announcement
-                cy.get('.thread__root').contains(initialMessage);
+                // * The first should be announcement
+                cy.findAllByRole('listitem').eq(0).contains(initialMessage);
 
                 // * Latest post should be update
                 cy.get(`#rhsPost_${lastPostId}`).contains(
@@ -348,7 +348,7 @@ const deleteLatestPostRoot = (testTeam, channelName) => {
         // # Open RHS comment menu
         cy.clickPostCommentIcon(lastPostId);
 
-        cy.get('.thread__root').then((root) => {
+        cy.findAllByRole('listitem').eq(0).then((root) => {
             const rootId = root.attr('id').slice(8);
 
             // # Click root's post dot menu.
