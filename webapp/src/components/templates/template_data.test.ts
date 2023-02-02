b50cab4d8845ee45e58e0642421d07cee25ec58d
Julien Tant <julien@craftyx.fr>
2023-02-01 17:35:33 -0700

    comment

--- webapp/src/components/templates/template_data.test.ts
+++ webapp/src/components/templates/template_data.test.ts
@@ -2,6 +2,7 @@ import TemplateData from './template_data';
 
 describe('TemplateData', () => {
     // if a template is missing, this might break the work templates feature.
+    // reminder: because playbook templates don't have an ID, we rely on their name to identify them.
     // if this breaks, contact the @channel team to figure out what should be done.
     const knownTemplatesInWorkTemplate = [
         'Product Release',
