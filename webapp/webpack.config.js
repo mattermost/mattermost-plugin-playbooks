203b69afde222d22c0626730f08710e0bf448f7b
Jose Peso <jmpeso@gmail.com>
2023-02-27 16:16:10 +0100

    webpack config cleanup

--- webapp/webpack.config.js
+++ webapp/webpack.config.js
@@ -1,49 +1,14 @@
-const exec = require('child_process').exec;
-
 const path = require('path');
 
 const webpack = require('webpack');
 const {ModuleFederationPlugin} = require('webpack').container;
 
-const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
-
-const PLUGIN_ID = require('../plugin.json').id;
-
 const NPM_TARGET = process.env.npm_lifecycle_event; //eslint-disable-line no-process-env
 const TARGET_IS_PRODUCT = NPM_TARGET?.endsWith(':product');
 const targetIsDevServer = NPM_TARGET === 'dev-server';
-let mode = 'production';
-let devtool = 'source-map';
-if (NPM_TARGET === 'debug' || NPM_TARGET === 'debug:watch' || targetIsDevServer) {
-    mode = 'development';
-    devtool = 'eval-cheap-module-source-map';
-}
-
+const mode = 'production';
+const devtool = 'source-map';
 const plugins = [];
-if (NPM_TARGET === 'build:watch' || NPM_TARGET === 'debug:watch') {
-    plugins.push({
-        apply: (compiler) => {
-            compiler.hooks.watchRun.tap('WatchStartPlugin', () => {
-                // eslint-disable-next-line no-console
-                console.log('Change detected. Rebuilding webapp.');
-            });
-            compiler.hooks.afterEmit.tap('AfterEmitPlugin', () => {
-                exec('cd .. && make deploy-from-watch', (err, stdout, stderr) => {
-                    if (stdout) {
-                        process.stdout.write(stdout);
-                    }
-                    if (stderr) {
-                        process.stderr.write(stderr);
-                    }
-                });
-            });
-        },
-    });
-}
-
-if (targetIsDevServer) {
-    plugins.push(new ReactRefreshWebpackPlugin());
-}
 
 let config = {
     entry: './src/remote_entry.ts',
@@ -113,88 +78,65 @@ let config = {
     plugins,
 };
 
-if (TARGET_IS_PRODUCT) {
-    // Set up module federation
-    function makeSingletonSharedModules(packageNames) {
-        const sharedObject = {};
+// Set up module federation
+function makeSingletonSharedModules(packageNames) {
+    const sharedObject = {};
 
-        for (const packageName of packageNames) {
-            // Set both versions to false so that the version of this module provided by the web app will be used
-            sharedObject[packageName] = {
-                requiredVersion: false,
-                singleton: true,
-                version: false,
-            };
-        }
-
-        return sharedObject;
+    for (const packageName of packageNames) {
+        // Set both versions to false so that the version of this module provided by the web app will be used
+        sharedObject[packageName] = {
+            requiredVersion: false,
+            singleton: true,
+            version: false,
+        };
     }
 
-    config.plugins.push(new ModuleFederationPlugin({
-        name: 'playbooks',
-        filename: 'remote_entry.js',
-        exposes: {
-            '.': './src/index',
-
-            // This probably won't need to be exposed in the long run, but its a POC for exposing multiple modules
-            './manifest': './src/manifest',
-        },
-        shared: [
-            '@mattermost/client',
-            '@types/luxon',
-            '@types/react-bootstrap',
-
-            makeSingletonSharedModules([
-                'react',
-                'react-dom',
-                'react-intl',
-                'react-redux',
-                'react-router-dom',
-                'styled-components',
-            ]),
-        ],
-    }));
-
-    config.plugins.push(new webpack.DefinePlugin({
-        'process.env.TARGET_IS_PRODUCT': TARGET_IS_PRODUCT, // TODO We might want a better name for this
-    }));
-
-    config.output = {
-        path: path.join(__dirname, '/dist'),
-    };
-    config.externals = {
-        react: 'React',
-        'react-dom': 'ReactDOM',
-        redux: 'Redux',
-        luxon: 'Luxon',
-        'react-redux': 'ReactRedux',
-        'prop-types': 'PropTypes',
-        'react-bootstrap': 'ReactBootstrap',
-        'react-router-dom': 'ReactRouterDom',
-        'react-intl': 'ReactIntl',
-    };
-} else {
-    config.resolve.alias['react-intl'] = path.resolve(__dirname, '../../webapp/node_modules/react-intl/');
-
-    config.externals = {
-        react: 'React',
-        'react-dom': 'ReactDOM',
-        redux: 'Redux',
-        luxon: 'Luxon',
-        'react-redux': 'ReactRedux',
-        'prop-types': 'PropTypes',
-        'react-bootstrap': 'ReactBootstrap',
-        'react-router-dom': 'ReactRouterDom',
-        'react-intl': 'ReactIntl',
-    };
-
-    config.output = {
-        devtoolNamespace: PLUGIN_ID,
-        path: path.join(__dirname, '/dist'),
-        publicPath: '/',
-        filename: 'main.js',
-    };
+    return sharedObject;
 }
+
+config.plugins.push(new ModuleFederationPlugin({
+    name: 'playbooks',
+    filename: 'remote_entry.js',
+    exposes: {
+        '.': './src/index',
+
+        // This probably won't need to be exposed in the long run, but its a POC for exposing multiple modules
+        './manifest': './src/manifest',
+    },
+    shared: [
+        '@mattermost/client',
+        '@types/luxon',
+        '@types/react-bootstrap',
+
+        makeSingletonSharedModules([
+            'react',
+            'react-dom',
+            'react-intl',
+            'react-redux',
+            'react-router-dom',
+            'styled-components',
+        ]),
+    ],
+}));
+
+config.plugins.push(new webpack.DefinePlugin({
+    'process.env.TARGET_IS_PRODUCT': TARGET_IS_PRODUCT, // TODO We might want a better name for this
+}));
+
+config.output = {
+    path: path.join(__dirname, '/dist'),
+};
+config.externals = {
+    react: 'React',
+    'react-dom': 'ReactDOM',
+    redux: 'Redux',
+    luxon: 'Luxon',
+    'react-redux': 'ReactRedux',
+    'prop-types': 'PropTypes',
+    'react-bootstrap': 'ReactBootstrap',
+    'react-router-dom': 'ReactRouterDom',
+    'react-intl': 'ReactIntl',
+};
 
 if (targetIsDevServer) {
     config = {
@@ -226,7 +168,7 @@ if (targetIsDevServer) {
 }
 
 if (NPM_TARGET === 'start:product') {
-    const url = new URL(process.env.MM_BOARDS_DEV_SERVER_URL ?? 'http://localhost:9007'); //eslint-disable-line no-process-env
+    const url = new URL(process.env.MM_PLAYBOOKS_DEV_SERVER_URL ?? 'http://localhost:9007'); //eslint-disable-line no-process-env
 
     config.devServer = {
         https: url.protocol === 'https:' && {
