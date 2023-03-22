f8ed77e50bafdb1b6fe52876626f4567f3fe199b
Jesse Hallam <jesse.hallam@gmail.com>
2023-03-22 13:54:06 -0300

    playbooks: fix more non-determinism in tests

--- server/api_graphql_runs_test.go
+++ server/api_graphql_runs_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"sort"
 	"testing"
 
 	"github.com/graph-gophers/graphql-go"
@@ -456,14 +457,18 @@ func TestGraphQLChangeRunParticipants(t *testing.T) {
 		run, err = e.PlaybooksClient.PlaybookRuns.Get(context.TODO(), run.ID)
 		require.NoError(t, err)
 		require.Len(t, run.ParticipantIDs, 2)
-		assert.Equal(t, e.RegularUser.Id, run.ParticipantIDs[0])
-		assert.Equal(t, e.RegularUser2.Id, run.ParticipantIDs[1])
+		expected := []string{e.RegularUser.Id, e.RegularUser2.Id}
+		sort.Strings(expected)
+		sort.Strings(run.ParticipantIDs)
+		assert.Equal(t, expected, run.ParticipantIDs)
 
 		meta, err := e.PlaybooksClient.PlaybookRuns.GetMetadata(context.TODO(), run.ID)
 		require.NoError(t, err)
 		require.Len(t, meta.Followers, 2)
-		assert.Equal(t, e.RegularUser.Id, meta.Followers[0])
-		assert.Equal(t, e.RegularUser2.Id, meta.Followers[1])
+		expected = []string{e.RegularUser.Id, e.RegularUser2.Id}
+		sort.Strings(expected)
+		sort.Strings(meta.Followers)
+		assert.Equal(t, expected, meta.Followers)
 
 		member, appErr := e.A.GetChannelMember(request.EmptyContext(nil), run.ChannelID, e.RegularUser2.Id)
 		require.Nil(t, appErr)
@@ -499,14 +504,18 @@ func TestGraphQLChangeRunParticipants(t *testing.T) {
 		run, err = e.PlaybooksClient.PlaybookRuns.Get(context.TODO(), run.ID)
 		require.NoError(t, err)
 		require.Len(t, run.ParticipantIDs, 2)
-		assert.Equal(t, e.RegularUser.Id, run.ParticipantIDs[0])
-		assert.Equal(t, e.RegularUser2.Id, run.ParticipantIDs[1])
+		expected := []string{e.RegularUser.Id, e.RegularUser2.Id}
+		sort.Strings(expected)
+		sort.Strings(run.ParticipantIDs)
+		assert.Equal(t, expected, run.ParticipantIDs)
 
 		meta, err := e.PlaybooksClient.PlaybookRuns.GetMetadata(context.TODO(), run.ID)
 		require.NoError(t, err)
 		require.Len(t, meta.Followers, 2)
-		assert.Equal(t, e.RegularUser.Id, meta.Followers[0])
-		assert.Equal(t, e.RegularUser2.Id, meta.Followers[1])
+		expected = []string{e.RegularUser.Id, e.RegularUser2.Id}
+		sort.Strings(expected)
+		sort.Strings(meta.Followers)
+		assert.Equal(t, expected, meta.Followers)
 
 		member, appErr := e.A.GetChannelMember(request.EmptyContext(nil), run.ChannelID, e.RegularUser2.Id)
 		require.Nil(t, appErr)
@@ -647,8 +656,10 @@ func TestGraphQLChangeRunParticipants(t *testing.T) {
 		run, err = e.PlaybooksClient.PlaybookRuns.Get(context.TODO(), run.ID)
 		require.NoError(t, err)
 		require.Len(t, run.ParticipantIDs, 2)
-		assert.Equal(t, e.RegularUser.Id, run.ParticipantIDs[0])
-		assert.Equal(t, e.RegularUser2.Id, run.ParticipantIDs[1])
+		expected := []string{e.RegularUser.Id, e.RegularUser2.Id}
+		sort.Strings(expected)
+		sort.Strings(run.ParticipantIDs)
+		assert.Equal(t, expected, run.ParticipantIDs)
 	})
 }
 
