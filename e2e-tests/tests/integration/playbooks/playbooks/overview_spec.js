3719dd84a0b635a9e7df09cb4627cf053c67e627
Julien Tant <julien@craftyx.fr>
2024-06-21 07:13:31 -0700

    fix my alphabet

--- e2e-tests/tests/integration/playbooks/playbooks/overview_spec.js
+++ e2e-tests/tests/integration/playbooks/playbooks/overview_spec.js
@@ -106,7 +106,7 @@ describe('playbooks > overview', {testIsolation: true}, () => {
 
     it('redirects to not found error if the playbook is unknown', () => {
         // # Visit the URL of a non-existing playbook
-        cy.visit('/playbooks/playbooks/abcdefghijklmnopqrstuvwx');
+        cy.visit('/playbooks/playbooks/abcdefghijklmnopqrstuvwxyz');
 
         // * Verify that the user has been redirected to the playbooks not found error page
         cy.url().should('include', '/playbooks/error?type=playbooks');
