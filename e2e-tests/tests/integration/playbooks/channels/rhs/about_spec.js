1d1ed3f9e14f09ed65c9433bc4a9ee24ae63fa78
Caleb Roseland <caleb@calebroseland.com>
2025-04-07 16:02:40 -0500

    lint

--- e2e-tests/tests/integration/playbooks/channels/rhs/about_spec.js
+++ e2e-tests/tests/integration/playbooks/channels/rhs/about_spec.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import {HALF_SEC, ONE_SEC} from '../../../../fixtures/timeouts';
+import {ONE_SEC} from '../../../../fixtures/timeouts';
 
 // ***************************************************************
 // - [#] indicates a test step (e.g. # Go to a page)
