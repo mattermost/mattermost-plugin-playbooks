d45c9bba74119dffd5de47256eb5a8f3d1bb288d
maria.nunez <maria.nunez@mattermost.com>
2025-05-09 13:46:46 -0400

    Updated server public and added support for Enterprise Advanced in webapp

--- webapp/src/hooks/license.ts
+++ webapp/src/hooks/license.ts
@@ -3,57 +3,57 @@
 
 import {useSelector} from 'react-redux';
 
-import {isE10LicensedOrDevelopment, isE20LicensedOrDevelopment} from 'src/license';
+import {checkProfessionalLicensed, isEnterpriseLicensedOrDevelopment} from 'src/license';
 
 // useAllowAddMessageToTimelineInCurrentTeam returns whether a user can add a
 // post to the timeline in the current team
 export function useAllowAddMessageToTimelineInCurrentTeam() {
-    return useSelector(isE10LicensedOrDevelopment);
+    return useSelector(checkProfessionalLicensed);
 }
 
 // useAllowChannelExport returns whether exporting the channel is allowed
 export function useAllowChannelExport() {
-    return useSelector(isE20LicensedOrDevelopment);
+    return useSelector(isEnterpriseLicensedOrDevelopment);
 }
 
 // useAllowPlaybookStatsView returns whether the server is licensed to show
 // the stats in the playbook backstage dashboard
 export function useAllowPlaybookStatsView() {
-    return useSelector(isE20LicensedOrDevelopment);
+    return useSelector(isEnterpriseLicensedOrDevelopment);
 }
 
 // useAllowPlaybookAndRunMetrics returns whether the server is licensed to
 // enter and show playbook and run metrics
 export function useAllowPlaybookAndRunMetrics() {
-    return useSelector(isE20LicensedOrDevelopment);
+    return useSelector(isEnterpriseLicensedOrDevelopment);
 }
 
 // useAllowRetrospectiveAccess returns whether the server is licenced for
 // the retrospective feature.
 export function useAllowRetrospectiveAccess() {
-    return useSelector(isE10LicensedOrDevelopment);
+    return useSelector(checkProfessionalLicensed);
 }
 
 // useAllowPrivatePlaybooks returns whether the server is licenced for
 // creating private playbooks
 export function useAllowPrivatePlaybooks() {
-    return useSelector(isE20LicensedOrDevelopment);
+    return useSelector(isEnterpriseLicensedOrDevelopment);
 }
 
 // useAllowSetTaskDueDate returns whether the server is licensed for
 // setting / editing checklist item due date
 export function useAllowSetTaskDueDate() {
-    return useSelector(isE10LicensedOrDevelopment);
+    return useSelector(checkProfessionalLicensed);
 }
 
 // useAllowMakePlaybookPrivate returns whether the server is licenced for
 // converting public playbooks to private
 export function useAllowMakePlaybookPrivate() {
-    return useSelector(isE20LicensedOrDevelopment);
+    return useSelector(isEnterpriseLicensedOrDevelopment);
 }
 
 // useAllowRequestUpdate returns whether the server is licenced for
 // requesting an update
 export function useAllowRequestUpdate() {
-    return useSelector(isE10LicensedOrDevelopment);
+    return useSelector(checkProfessionalLicensed);
 }
