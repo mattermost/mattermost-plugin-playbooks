8e8cf62e8db933a15a3d17ad4a410641fb6aac34
Julien Tant <785518+JulienTant@users.noreply.github.com>
2025-08-04 08:25:04 -0700

    Fix test compatibility with Mattermost v11 (#2053)

--- e2e-tests/tests/support/ui/playbooks.js
+++ e2e-tests/tests/support/ui/playbooks.js
@@ -5,7 +5,7 @@ import * as TIMEOUTS from '../../fixtures/timeouts';
 const playbookRunStartCommand = '/playbook run';
 
 Cypress.Commands.add('startPlaybookRun', (playbookName, playbookRunName) => {
-    cy.get('#interactiveDialogModal').should('exist').within(() => {
+    cy.get('#appsModal').should('exist').within(() => {
         // # Select playbook
         cy.selectPlaybookFromDropdown(playbookName);
 
@@ -13,10 +13,10 @@ Cypress.Commands.add('startPlaybookRun', (playbookName, playbookRunName) => {
         cy.findByTestId('playbookRunNameinput').type(playbookRunName, {force: true});
 
         // # Submit
-        cy.get('#interactiveDialogSubmit').click();
+        cy.get('#appsModalSubmit').click();
     });
 
-    cy.get('#interactiveDialogModal').should('not.exist');
+    cy.get('#appsModal').should('not.exist');
 });
 
 // Opens playbook run dialog using the `/playbook run` slash command
@@ -98,10 +98,10 @@ Cypress.Commands.add('createPlaybook', (teamName, playbookName) => {
 
 // Select the playbook from the dropdown menu
 Cypress.Commands.add('selectPlaybookFromDropdown', (playbookName) => {
-    cy.findByTestId('autoCompleteSelector').should('exist').within(() => {
+    cy.findByTestId('playbookID').should('exist').within(() => {
         cy.get('input').click().type(playbookName.toLowerCase(), {force: true});
-        cy.get('#suggestionList').contains(playbookName).click({force: true});
     });
+    cy.document().its('body').find('#react-select-2-listbox').contains(playbookName).click({force: true});
 });
 
 Cypress.Commands.add('createPost', (message) => {
@@ -113,21 +113,21 @@ Cypress.Commands.add('addPostToTimelineUsingPostMenu', (playbookRunName, summary
     cy.clickPostDotMenu(postId);
     cy.findByTestId('playbookRunAddToTimeline').click();
 
-    cy.get('#interactiveDialogModal').should('exist').within(() => {
+    cy.get('#appsModal').should('exist').within(() => {
         // # Select playbook run
-        cy.findByTestId('autoCompleteSelector').should('exist').within(() => {
+        cy.findByTestId('playbookID').should('exist').within(() => {
             cy.get('input').click().type(playbookRunName);
-            cy.get('#suggestionList').contains(playbookRunName).click({force: true});
         });
+        cy.document().its('body').find('#react-select-2-listbox').contains(playbookRunName).click({force: true});
 
         // # Type playbook run name
         cy.findByTestId('summaryinput').clear().type(summary, {force: true});
 
         // # Submit
-        cy.get('#interactiveDialogSubmit').click();
+        cy.get('#appsModalSubmit').click();
     });
 
-    cy.get('#interactiveDialogModal').should('not.exist');
+    cy.get('#appsModal').should('not.exist');
 });
 
 Cypress.Commands.add('openSelector', () => {
