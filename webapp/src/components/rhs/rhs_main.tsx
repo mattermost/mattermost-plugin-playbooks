fd9eda6454a85313860df105491dae8aa1f42e58
JG Heithcock <jgheithcock@gmail.com>
2025-10-23 00:49:35 +0200

    Fix bug where you couldn't go to list if only one checklist

--- webapp/src/components/rhs/rhs_main.tsx
+++ webapp/src/components/rhs/rhs_main.tsx
@@ -175,13 +175,14 @@ const RightHandSidebar = () => {
     const currentChannelId = useSelector<GlobalState, string>(getCurrentChannelId);
     const [currentRunId, setCurrentRunId] = useState<string|undefined>();
     const [skipNextDetailNav, setSkipNextDetailNav] = useState(false);
+    const [userNavigatedAway, setUserNavigatedAway] = useState(false);
     const [listOptions, setListOptions] = useState<RunListOptions>(defaultListOptions);
     const fetchedRuns = useFilteredSortedRuns(currentChannelId, listOptions);
     const {isLoading} = usePlaybooksCrud({team_id: currentTeam?.id}, {infinitePaging: true});
 
     // If there is only one active run in this channel select it.
     useEffect(() => {
-        if (fetchedRuns.runsInProgress && fetchedRuns.runsInProgress.length === 1) {
+        if (fetchedRuns.runsInProgress && fetchedRuns.runsInProgress.length === 1 && !userNavigatedAway) {
             const singleRunID = fetchedRuns.runsInProgress[0].id;
             if (singleRunID !== currentRunId && !skipNextDetailNav) {
                 setCurrentRunId(singleRunID);
@@ -190,11 +191,13 @@ const RightHandSidebar = () => {
         if (skipNextDetailNav) {
             setSkipNextDetailNav(false);
         }
-    }, [currentChannelId, currentRunId, fetchedRuns.runsInProgress, skipNextDetailNav]);
+    }, [currentChannelId, currentRunId, fetchedRuns.runsInProgress, skipNextDetailNav, userNavigatedAway]);
 
-    // Reset the list options on channel change
+    // Reset the list options and navigation state on channel change
     useEffect(() => {
         setListOptions(defaultListOptions);
+        setUserNavigatedAway(false);
+        setCurrentRunId(undefined);
     }, [currentChannelId]);
 
     if (!fetchedRuns.runsInProgress || !fetchedRuns.runsFinished) {
@@ -204,6 +207,7 @@ const RightHandSidebar = () => {
     const clearCurrentRunId = () => {
         fetchedRuns.refetch();
         setCurrentRunId(undefined);
+        setUserNavigatedAway(true);
     };
 
     const handleOnCreateRun = (runId: string, channelId: string) => {
