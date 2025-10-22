89e01dcd493d00178ea2f74f6b5376a0e7549df5
Joram Wilander <joram@mattermost.com>
2025-10-22 10:45:26 +0200

    Fix playbook checklist commands

--- server/app/playbook_run_service.go
+++ server/app/playbook_run_service.go
@@ -1737,7 +1737,7 @@ func (s *PlaybookRunServiceImpl) GetPlaybookRunsForChannelByUser(channelID strin
 			PerPage:   1000,
 			Sort:      SortByCreateAt,
 			Direction: DirectionDesc,
-			Types:     []string{RunTypePlaybook},
+			Types:     []string{RunTypePlaybook, RunTypeChannelChecklist},
 		},
 	)
 
