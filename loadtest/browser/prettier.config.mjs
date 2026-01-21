9c8e98b237f42589ea080566a01cb2b572f77cd6
M-ZubairAhmed <m-zubairahmed@protonmail.com>
2026-01-22 03:59:35 +0530

    Add Prettier configuration for code formatting in loadtest browser

---
+++ loadtest/browser/prettier.config.mjs
@@ -0,0 +1,10 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  trailingComma: "all",
+  bracketSpacing: false,
+};
+
+export default config;
