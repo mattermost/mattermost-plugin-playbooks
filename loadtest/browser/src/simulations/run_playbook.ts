d4d05fce67387c998a8e5aeb19d2b0a4c6e794c8
M-ZubairAhmed <m-zubairahmed@protonmail.com>
2026-02-16 00:18:33 +0530

    Update peerDependencies: @mattermost/loadtest-browser-lib to ^1.31.1, @mattermost/playwright-lib to ^11.4.0, @playwright/test to ^1.58.2.

--- loadtest/browser/src/simulations/run_playbook.ts
+++ loadtest/browser/src/simulations/run_playbook.ts
@@ -2,7 +2,7 @@
 // See LICENSE.txt for license information.
 
 import {type Page} from "@playwright/test";
-import type {Logger} from "@mattermost/loadtest-browser-lib";
+import {type Logger} from "@mattermost/loadtest-browser-lib";
 
 export async function runPlaybook(page: Page, log: Logger): Promise<void> {
   log.info("run--runPlaybook");
