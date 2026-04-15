3886a22c7588647fd48074345f08227e27384012
Harrison Healey <harrisonmhealey@gmail.com>
2026-04-15 10:05:01 -0400

    Use idiomatic way of constructing useAppDispatch/useAppSelector

--- webapp/src/hooks/redux.ts
+++ webapp/src/hooks/redux.ts
@@ -1,11 +1,11 @@
 // Copyright (c) 2020-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import {useDispatch, useSelector} from 'react-redux';
-import type {TypedUseSelectorHook} from 'react-redux';
+import {type UseDispatch, useDispatch, useSelector} from 'react-redux';
 import type {GlobalState} from '@mattermost/types/store';
 
 import type {Dispatch} from 'src/types/store';
 
-export const useAppDispatch: () => Dispatch = useDispatch;
-export const useAppSelector: TypedUseSelectorHook<GlobalState> = useSelector;
+// The `as Dispatch` is currently required because mattermost-redux messes with the type definition for useDispatch
+export const useAppDispatch = (useDispatch as UseDispatch).withTypes<Dispatch>();
+export const useAppSelector = useSelector.withTypes<GlobalState>();
