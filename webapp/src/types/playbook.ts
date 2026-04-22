ff122986577c4a73ceb66cae7e8aa63840ffae31
Catalin I. Tomai <catalin.tomai@mattermost.com>
2026-04-23 00:39:29 +0200

    fix lint

--- webapp/src/types/playbook.ts
+++ webapp/src/types/playbook.ts
@@ -62,6 +62,7 @@ export interface PlaybookWithChecklist extends Playbook {
 
     channel_mode: string;
     channel_id: string;
+    auto_archive_channel: boolean;
 
     // Deprecated: preserved for backwards compatibility with v1.27
     broadcast_enabled: boolean;
@@ -215,6 +216,7 @@ export function emptyPlaybook(): DraftPlaybookWithChecklist {
         remove_channel_member_on_removed_participant: true,
         channel_id: '',
         channel_mode: 'create_new_channel',
+        auto_archive_channel: false,
         propertyFields: [],
     };
 }
