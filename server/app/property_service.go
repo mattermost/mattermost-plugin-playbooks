19aad9d84c770f81294a1ea75c7942c7815b6c38
Catalin I. Tomai <catalin.tomai@mattermost.com>
2026-04-25 22:43:33 +0200

    fix test

--- server/app/property_service.go
+++ server/app/property_service.go
@@ -4,6 +4,7 @@
 package app
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"sort"
@@ -89,6 +90,9 @@ func (s *propertyService) validatePropertyLimit(playbookID string) error {
 func (s *propertyService) GetPropertyField(propertyID string) (*PropertyField, error) {
 	mmPropertyField, err := s.api.Property.GetPropertyField(s.groupID, propertyID)
 	if err != nil {
+		if err == pluginapi.ErrNotFound || strings.Contains(err.Error(), sql.ErrNoRows.Error()) {
+			return nil, errors.Wrap(ErrNotFound, "property field not found")
+		}
 		return nil, errors.Wrap(err, "failed to get property field")
 	}
 
