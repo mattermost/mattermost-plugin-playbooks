ccd169d7c4bbb9e7e12caf7100edaffca005b590
Harrison Healey <harrisonmhealey@gmail.com>
2026-04-27 11:09:04 -0400

    asdf Use version of shared package that should support older versions of MM
    Obviously, I need to actually publish the matching version of the shared
    package, but this will remind me to go do that.

---
+++ webapp/update-shared-packages-pack.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+VERSION="11.7.0"
+
+npm add ./mattermost-types-${VERSION}.tgz \
+    ./mattermost-client-${VERSION}.tgz \
+    ./mattermost-redux-${VERSION}.tgz \
+    ./mattermost-shared-${VERSION}.tgz
