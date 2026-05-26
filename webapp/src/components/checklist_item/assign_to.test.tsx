34490dd9eefe3c71fd432c58ca7b2c34648ffa7d
Catalin I. Tomai <catalin.tomai@mattermost.com>
2026-05-26 16:16:41 +0200

    fix lint

--- webapp/src/components/checklist_item/assign_to.test.tsx
+++ webapp/src/components/checklist_item/assign_to.test.tsx
@@ -5,8 +5,8 @@
 
 import React from 'react';
 import renderer from 'react-test-renderer';
-
 import {OverlayTrigger} from 'react-bootstrap';
+
 import ProfileSelectorDefault from 'src/components/profile/profile_selector';
 
 import AssignTo from './assign_to';
